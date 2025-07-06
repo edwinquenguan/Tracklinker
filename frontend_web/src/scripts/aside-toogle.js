0d5b5553a1607fe519ebdcf8f0f812acafe3f714
Juan <juanesyt7@gmail.com>
2025-07-06 14:59:14 -0500

    Delete unused js file

--- frontend_web/src/scripts/aside-toogle.js
+++
@@ -1,6 +0,0 @@
-const aside = document.getElementById("aside");
-const menuButton = document.getElementById("menu-button");
-
-menuButton.addEventListener("click", () => {
-    aside.classList.toggle('show-aside');
-});
