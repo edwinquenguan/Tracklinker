83e06d23469d5ccf8194e2ebaae1e282271fa63f
edwinquenguan <edwinquenguan14@gmail.com>
2025-09-03 21:04:23 -0500

    Add 3 new multitable sql queries

--- _docs/trim04/01_Sql_Scripts/Queries(Multi Table)/queries.sql
+++ _docs/trim04/01_Sql_Scripts/Queries(Multi Table)/queries.sql
@@ -61,10 +61,41 @@ SELECT
     WHERE product_stock <= 15;
 
 /* ¿Qué proveedores abastecen productos de una marca específica? (SUPPLIERS, INPUT_ORDERS, PRODUCT_SERIALS, PRODUCTS, PRODUCT_DETAILS, PRODUCT_BRANDS) */
+SELECT
+    s.supplier_name,
+    pb.product_brand_name,
+    pd.product_detail_model,
+    io.input_order_id
+    FROM SUPPLIERS AS s
+    INNER JOIN INPUT_ORDERS AS io 
+    ON s.supplier_id = io.supplier_id
+    INNER JOIN PRODUCT_SERIALS AS ps 
+    ON io.input_order_id = ps.input_order_id
+    INNER JOIN PRODUCTS AS p 
+    ON ps.product_id = p.product_id
+    INNER JOIN PRODUCT_DETAILS AS pd 
+    ON p.product_details_id = pd.product_details_id
+    INNER JOIN PRODUCT_BRANDS AS pb 
+    ON pd.product_brand_id = pb.product_brand_id
+    WHERE pb.product_brand_name = 'Dell';
+    
+
 /* ¿Qué proveedor entregó los productos que aparecen en una orden de salida específica? (SUPPLIERS, INPUT_ORDERS, PRODUCT_SERIALS, OUTPUT_DETAILS, OUTPUT_ORDERS) */
 /* ¿Qué clientes reportaron incidentes de garantía y qué producto estaba asociado? (WARRANTY_INCIDENTS, OUTPUT_DETAILS, PRODUCT_SERIALS, PRODUCTS) */
 /* ¿Qué técnicos resolvieron garantías de productos de la marca X? (TECHNICAL, USERS, WARRANTY_INCIDENTS, OUTPUT_DETAILS, PRODUCT_SERIALS, PRODUCTS, PRODUCT_DETAILS, PRODUCT_BRANDS) */
 /* ¿Qué empleados de almacén recibieron órdenes de entrada de un proveedor en particular? (USERS, WAREHAUSEMAN, INPUT_ORDERS, SUPPLIERS) */
+SELECT
+    u.user_name,
+    s.supplier_name,
+    io.input_order_id
+    FROM USERS AS u
+    INNER JOIN WAREHAUSEMAN AS wm 
+    ON u.user_id = wm.users_id
+    INNER JOIN INPUT_ORDERS AS io 
+    ON wm.input_order_id = io.input_order_id
+    INNER JOIN SUPPLIERS AS s 
+    ON io.supplier_id = s.supplier_id;
+
 /* ¿Qué proveedor ha enviado más productos a través de órdenes de entrada? (SUPPLIERS, INPUT_ORDERS, PRODUCT_SERIALS) */
 /* ¿Qué clientes realizaron más de 3 órdenes de salida en un mismo mes? (CUSTOMERS, OUTPUT_ORDERS) */
 /* ¿Qué clientes reportaron más de 2 incidentes de garantía en un mismo año? (CUSTOMERS, OUTPUT_ORDERS, WARRANTY_INCIDENTS) */
@@ -84,7 +115,8 @@ SELECT DISTINCT u.user_name
 /* ¿Qué proveedor tiene más diversidad de productos (por categorías)? (SUPPLIERS, INPUT_ORDERS, PRODUCT_SERIALS, PRODUCTS, SUBCATEGORIES, CATEGORIES) */
 
 /* ¿Qué encargado de almacén nunca participó en una orden de salida? (WAREHAUSEMAN LEFT JOIN OUTPUT_ORDERS) */
-SELECT DISTINCT u.user_name
+SELECT DISTINCT
+    u.user_name
     FROM WAREHAUSEMAN AS wm
     INNER JOIN USERS AS u 
     ON wm.users_id = u.user_id 
@@ -97,7 +129,48 @@ SELECT DISTINCT u.user_name
     INNER JOIN OUTPUT_ORDERS AS oo 
     ON od.out_order_id = oo.out_order_id
     WHERE oo.out_order_id IS NULL;
-/* ¿Qué técnicos atendieron más de 10 incidentes en 2025? (TECHNICAL, WARRANTY_INCIDENTS) */
-/* ¿Qué proveedores no han vendido productos desde 2024? (SUPPLIERS, INPUT_ORDERS) */
-/* ¿Qué técnicos atendieron incidentes de más de 5 clientes distintos? (TECHNICAL, WARRANTY_INCIDENTS, CUSTOMERS) */
+    
+/* ¿Qué técnicos atendieron más de 10 incidentes en 2025? (TECHNICAL, WARRANTY_INCIDENTS) */ .
+/* ¿Qué proveedores no han ingresado productos desde 2024? (SUPPLIERS, INPUT_ORDERS) */
+SELECT
+    s.supplier_name,
+    io.input_order
+    FROM SUPPLIERS AS s
+    INNER JOIN INPUT_ORDERS AS io 
+    ON s.supplier_id = io.supplier_id
+    WHERE io.input_order_date < '2024-01-01';
+    
+
+
+
+
+/* ¿Qué técnicos atendieron incidentes de más de 5 clientes distintos? (TECHNICAL, WARRANTY_INCIDENTS, CUSTOMERS) */.
+
 /* ¿Qué encargados de almacén registraron más de 50 productos en un solo día? (WAREHAUSEMAN, INPUT_ORDERS, PRODUCT_SERIALS) */
+SELECT
+     u.user_name,
+     io.input_order_date,
+     ps.product_serial,
+     COUNT(ps.product_serial) AS total_products
+    FROM users AS u
+    INNER JOIN WAREHAUSEMAN AS wm
+    ON wm.users_id = u.user_id
+    INNER JOIN INPUT_ORDERS AS io
+    ON wm.input_order_id = io.input_order_id
+    INNER JOIN PRODUCT_SERIALS AS ps
+    ON io.input_order_id = ps.input_order_id
+    INNER JOIN PRODUCTS AS p
+    ON ps.product_id = p.product_id
+    HAVING COUNT(ps.product_serial) > 50;
+
+
+   
+
+   
+   
+
+
+
+
+
+
