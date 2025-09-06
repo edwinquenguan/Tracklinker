d67842fb073292c771aa96c145705972966a99f3
Juan <juanesyt7@gmail.com>
2025-09-05 20:49:03 -0500

    feat: Add icon files for aside, header, and main sections to export from everywhere

---
+++ frontend_web/react_frontend/src/assets/icons/asideIcons.js
@@ -0,0 +1,34 @@
+import avatarIcon from "./aside/avatar.svg";
+import homeIcon from "./aside/home-icon.svg";
+import dashboardIcon from "./aside/dashboard-icon.svg";
+import usersIcon from "./aside/users-icon.svg";
+import categoriesIcon from "./aside/categories-icon.svg";
+import subcategoriesIcon from "./aside/subcategories-icon.svg";
+import productsIcon from "./aside/products-icon.svg";
+import reportsIcon from "./aside/reports-icon.svg";
+import settingsIcon from "./aside/settings-icon.svg";
+import suppliersIcon from "./aside/suppliers-icon.svg";
+import transformationsIcon from "./aside/transformations-icon.svg";
+import warrantiesIcon from "./aside/warranties-icon.svg";
+import logoutIcon from "./aside/logout-icon.svg";
+import infoIcon from "./aside/more-info-icon.svg";
+import helpIcon from "./aside/help-icon.svg";
+
+// Esta constante exporta los iconos del aside para usarlos en cualquier lugar 
+export const asideIcons = {
+    avatarIcon,
+    homeIcon,
+    usersIcon,
+    dashboardIcon,
+    categoriesIcon,
+    subcategoriesIcon,
+    productsIcon,
+    reportsIcon,
+    settingsIcon,
+    suppliersIcon,
+    transformationsIcon,
+    warrantiesIcon,
+    logoutIcon,
+    helpIcon,
+    infoIcon,
+};
