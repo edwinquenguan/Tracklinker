9afa9fa7e871221885c77d5f645bc5f5248bfa62
Juan <juanesyt7@gmail.com>
2025-09-07 18:54:45 -0500

    Change the headerIcons constant "[]" to "{}"

--- frontend_web/react_frontend/src/assets/icons/headerIcons.js
+++ frontend_web/react_frontend/src/assets/icons/headerIcons.js
@@ -2,8 +2,8 @@ import bellIcon from "./header/bell.svg";
 import menubarIcon from "./header/menubar-icon.svg";
 import searchIcon from "./header/search-icon.svg";
 
-export const headerIcons = [
+export const headerIcons = {
     bellIcon,
     menubarIcon,
     searchIcon,
-];
+};
