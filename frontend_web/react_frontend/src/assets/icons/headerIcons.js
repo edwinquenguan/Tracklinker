b89b57d2399cbe81d3a46361762b39f38d63d768
Juan <juanesyt7@gmail.com>
2025-09-07 18:15:35 -0500

    Update search icon path

--- frontend_web/react_frontend/src/assets/icons/headerIcons.js
+++ frontend_web/react_frontend/src/assets/icons/headerIcons.js
@@ -1,6 +1,6 @@
 import bellIcon from "./header/bell.svg";
 import menubarIcon from "./header/menubar-icon.svg";
-import searchIcon from "/header/search-icon.svg";
+import searchIcon from "./header/search-icon.svg";
 
 export const headerIcons = [
     bellIcon,
