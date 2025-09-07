3b30c3e54a348f9a71a296e392db54472ec630ec
Juan <juanesyt7@gmail.com>
2025-09-07 15:11:11 -0500

    feat: update Layout component to render children instead of Main

--- frontend_web/react_frontend/src/components/Layout/Layout.jsx
+++ frontend_web/react_frontend/src/components/Layout/Layout.jsx
@@ -1,15 +1,16 @@
 import Header from "./Header";
 import Aside from "./Aside";
-import Main from "./Main";
 
-export default function Layout(){
+export default function Layout({children}){
     return(
         // Container 
         <div 
         className="w-[100%] h-[100%] grid grid-cols-[10%_90%] grid-rows-[15%_85%]">
             <Aside />
             <Header />
-            <Main/>
+            <main>
+                {children}
+            </main>
         </div>
     );
 }
