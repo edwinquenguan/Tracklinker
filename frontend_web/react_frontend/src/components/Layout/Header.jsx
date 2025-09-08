6feb90c3a017d58b2b153af7f278521978b9eb9d
Juan <juanesyt7@gmail.com>
2025-09-07 20:25:13 -0500

    Add search bar component and bell icon for notifications

--- frontend_web/react_frontend/src/components/Layout/Header.jsx
+++ frontend_web/react_frontend/src/components/Layout/Header.jsx
@@ -1,7 +1,13 @@
+import { headerIcons } from "../../assets/icons/headerIcons";
+import SearchBar from "../SearchBar";
+
 export default function Header(){
     return(
-        <header>
-            
+        <header className="flex justify-between content-center p-4 border-b-[1px]">
+            <SearchBar />
+            <button>
+                <img src={headerIcons.bellIcon} alt="" className="w-[23px] h-full" />
+            </button>
         </header>
     );
 }
