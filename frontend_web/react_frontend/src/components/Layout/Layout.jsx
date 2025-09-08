8b769a9d2cdca1ad69ddf65d3b9cbeaab6eb378d
Juan <juanesyt7@gmail.com>
2025-09-07 20:25:28 -0500

    remove space

--- frontend_web/react_frontend/src/components/Layout/Layout.jsx
+++ frontend_web/react_frontend/src/components/Layout/Layout.jsx
@@ -4,8 +4,7 @@ import Aside from "./Aside";
 export default function Layout({children}){
     return(
         // Container 
-        <div 
-        className="w-[100%] h-[100%] grid grid-cols-[15%_85%] grid-rows-[8%_92%]">
+        <div className="w-[100%] h-[100%] grid grid-cols-[15%_85%] grid-rows-[8%_92%]">
             <Aside />
             <Header />
             <main className="p-8">
