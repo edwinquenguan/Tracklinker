023257ce627a4ee3345653cde9911e242a015bbb
Juan <juanesyt7@gmail.com>
2025-09-07 20:22:33 -0500

    feat: add search bar component with styles

---
+++ frontend_web/react_frontend/src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+import { headerIcons } from "../assets/icons/headerIcons";
+
+export default function SearchBar(){
+    return(
+        <div className="flex px-2 py-2 w-[40%] border border-gray-400 rounded-3xl">
+            <input 
+            id="search-text"
+            type="text" 
+            placeholder="Buscar" 
+            className="w-[95%] py-2 px-5 rounded-xl outline-none
+            placeholder:text-[15px] placeholder:text-gray-500"/>
+            <button>
+                <img src={headerIcons.searchIcon} alt="Lens Icon" />
+            </button>
+        </div>
+    );
+}
