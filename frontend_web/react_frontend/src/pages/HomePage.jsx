e5ce9a3aa60b34df3b9b98c45ca90b847fb07845
Juan <juanesyt7@gmail.com>
2025-09-07 20:26:21 -0500

    feat: add layout component and add welcome on main content

---
+++ frontend_web/react_frontend/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout/Layout";
+
+export default function HomePage(){
+    return(        
+        <Layout>
+            <h1 className="p-5 text-5xl font-medium"> Bienvenido, Agustín</h1>     
+        </Layout>
+    )
+}
