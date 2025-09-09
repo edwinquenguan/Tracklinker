aa79d9713e1916ee17d25b948f01a4f8ab7b4f97
Juan <juanesyt7@gmail.com>
2025-09-08 22:31:00 -0500

    feat: update height values and add card component for main content

--- frontend_web/react_frontend/src/pages/HomePage.jsx
+++ frontend_web/react_frontend/src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
 import Layout from "../components/Layout/Layout";
+import ActionCard from "../components/ui/ActionCard";
 
 export default function HomePage(){
     return(        
         <Layout>
-            <h1 className="p-5 text-5xl font-medium"> Bienvenido, Agustín</h1>     
+            <h1 className="h-[10%] p-5 text-5xl font-medium"> Bienvenido, Agustín </h1>
+            {/* Contenedor de las cards de los modúlos */}
+            <section className="h-[90%] min-w-full">
+                <ActionCard/>
+            </section>  
         </Layout>
     )
 }
