36ab8356c458af54d77d04be9a95f535d83e20e3
Juan <juanesyt7@gmail.com>
2025-09-11 15:29:49 -0500

    feat: Add loginIcons to import everywhere

---
+++ frontend_web/react_frontend/src/assets/icons/loginIcons.js
@@ -0,0 +1,11 @@
+import userIcon from "./login/user-icon.svg";
+import hidePasswordIcon from "./login/hidepass-icon.svg";
+import showPasswordIcon from "./login/showpass-icon.svg";
+import tracklinkerIcon from "./login/logo.svg";
+
+export const loginIcons = {
+    tracklinkerIcon,
+    userIcon,
+    showPasswordIcon,
+    hidePasswordIcon,
+}
