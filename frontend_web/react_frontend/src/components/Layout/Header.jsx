71b66cf3874be903f09b5adcd750c1d38b045e59
Juan <juanesyt7@gmail.com>
2025-09-12 16:41:31 -0500

    Add scale and transition for bell icon

--- frontend_web/react_frontend/src/components/Layout/Header.jsx
+++ frontend_web/react_frontend/src/components/Layout/Header.jsx
@@ -6,7 +6,8 @@ export default function Header(){
         <header className="flex justify-between content-center p-[6px_20px_5px_150px] border-b-[1px]">
             <SearchBar />
             <button>
-                <img src={headerIcons.bellIcon} alt="" className="w-[23px] h-[23px]"/>
+                <img src={headerIcons.bellIcon} alt="" className="w-[23px] h-[23px] transition duration-300 
+            hover:scale-[1.2]"/>
             </button>
         </header>
     );
