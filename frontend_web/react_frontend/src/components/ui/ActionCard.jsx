ff51005d39e4462326ac5c4de0655ae182697090
Juan <juanesyt7@gmail.com>
2025-09-12 16:42:42 -0500

    Add hover and scale properties and transition for scale

--- frontend_web/react_frontend/src/components/ui/ActionCard.jsx
+++ frontend_web/react_frontend/src/components/ui/ActionCard.jsx
@@ -4,10 +4,12 @@ import { items } from "../../constants/homeCards";
 export default function ActionCard(){
     return(
         <section className="grid grid-cols-4 grid-rows-2 p-[100px_350px_200px_350px] min-h-full gap-[20px_12px]">
+            {/* Card de cada modulo */}
             {items.map(item => (
             <NavLink
             to={item.path}
-            className="flex flex-col h-40 w-32 place-items-center gap-2"
+            className="flex flex-col h-40 w-32 place-items-center gap-2 transition duration-300 
+            hover:scale-[1.1]"
             >
                 <section className="h-32 w-32 rounded-xl bg-stone-200 content-center justify-items-center">
                     <img src={item.icon} alt={item.alt} className="w-12"/>
