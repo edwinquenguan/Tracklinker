d5a77d95991bd23c5d1837c2da183e3b9e031770
Juan <juanesyt7@gmail.com>
2025-09-12 19:40:12 -0500

    Update width and height for layout and main

--- frontend_web/react_frontend/src/components/Layout/Layout.jsx
+++ frontend_web/react_frontend/src/components/Layout/Layout.jsx
@@ -4,10 +4,10 @@ import Aside from "./Aside";
 export default function Layout({children}){
     return(
         // Container 
-        <div className="w-[100%] h-[100%] grid grid-cols-[15%_85%] grid-rows-[6%_94%]">
+        <div className="max-w-[100%] max-h-[100%] grid grid-cols-[15%_85%] grid-rows-[6%_94%]">
             <Aside />
             <Header />
-            <main className="p-8">
+            <main className="w-full h-full p-6">
                 {children}
             </main>
         </div>
