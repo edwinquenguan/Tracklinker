ecf629d67ff125941a74e778d1164e4683d84f3b
Juan <juanesyt7@gmail.com>
2025-09-12 19:40:51 -0500

    feat: import and export all icons for main content on the pages

---
+++ frontend_web/react_frontend/src/assets/icons/mainIcons.js
@@ -0,0 +1,73 @@
+/* Dashboard Icons */
+import arrowDown from "./main/dashboard/arrow-down.svg";
+import arrowUp from "./main/dashboard/arrow-up.svg";
+import firstChart from "./main/dashboard/first-chart.svg";
+import secondChart from "./main/dashboard/second-chart.svg";
+import thirdChart from "./main/dashboard/third-chart.svg";
+import fourthChart from "./main/dashboard/fourth-chart.svg";
+import purpleCircle from "./main/dashboard/purple-circle.svg";
+import lightPurpleCircle from "./main/dashboard/light-purple-circle.svg";
+import grayCircle from "./main/dashboard/gray-circle.svg";
+
+/* Users Icons */
+import addUserIcon from "./main/users/add-user-icon.svg";
+import addressIcon from "./main/users/address-icon.svg";
+import phoneIcon from "./main/users/phone-icon.svg";
+import rolIcon from "./main/users/rol-icon.svg";
+
+/* Products Icons */
+import addProductIcon from "./main/products/add-product-icon.svg";
+import barcodeReaderIcon from "./main/products/barcode-reader-icon.svg";
+import barcodeIcon from "./main/products/barcode-scanner-icon.svg";
+
+/* Warranties Icons */
+import addWarrantyIcon from "./main/warranties/add-warranty-icon.svg";
+import incompleteIcon from "./main/warranties/incomplete-icon.svg";
+import inprocessIcon from "./main/warranties/inprocess-icon.svg";
+import completeIcon from "./main/warranties/complete-icon.svg";
+
+/* Action Icons */
+import moreInfoIcon from "./main/actions/more-info-icon.svg";
+import editInfoIcon from "./main/actions/edit-info-icon.svg";
+import deleteIcon from "./main/actions/delete-user-icon.svg";
+import filterIcon from "./main/actions/filter-icon.svg";
+
+
+export const dashboardIcons = {
+    arrowDown,
+    arrowUp,
+    firstChart,
+    secondChart,
+    thirdChart,
+    fourthChart,
+    purpleCircle,
+    lightPurpleCircle,
+    grayCircle,
+};
+
+export const usersIcons = {
+    addUserIcon,
+    addressIcon,
+    phoneIcon,
+    rolIcon,
+};
+
+export const productsIcons = {
+    addProductIcon,
+    barcodeReaderIcon,
+    barcodeIcon,
+};
+
+export const actionsIcons = {
+    moreInfoIcon,
+    editInfoIcon,
+    deleteIcon,
+    filterIcon,
+};
+
+export const warrantiesIcons = {
+    addWarrantyIcon,
+    incompleteIcon,
+    inprocessIcon,
+    completeIcon,
+};
