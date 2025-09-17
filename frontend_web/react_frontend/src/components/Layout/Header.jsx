0723ba76d5a77c7043080ef9312361e8853988ab
Juan <juanesyt7@gmail.com>
2025-09-17 18:49:35 -0500

    feat: add styles for dark mode on header component

--- frontend_web/react_frontend/src/components/Layout/Header.jsx
+++ frontend_web/react_frontend/src/components/Layout/Header.jsx
@@ -3,10 +3,11 @@ import SearchBar from "../ui/SearchBar";
 
 export default function Header(){
     return(
-        <header className="flex justify-between content-center p-[6px_20px_5px_150px] border-b-[1px]">
+        <header className="flex justify-between content-center p-[6px_20px_5px_150px] border-b-[1px] 
+        dark:bg-black dark:border-0">
             <SearchBar />
             <button>
-                <img src={headerIcons.bellIcon} alt="" className="w-[23px] h-[23px] transition duration-300 
+                <img src={headerIcons.bellIcon} alt="" className="w-[23px] h-[23px] transition duration-300
             hover:scale-[1.2]"/>
             </button>
         </header>
