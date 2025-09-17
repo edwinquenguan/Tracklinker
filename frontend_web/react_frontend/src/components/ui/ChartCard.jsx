67537b8102c2f3d724f215aadc6484b398b4ffeb
Juan <juanesyt7@gmail.com>
2025-09-17 11:27:51 -0500

    feat: add chart card  component

---
+++ frontend_web/react_frontend/src/components/ui/ChartCard.jsx
@@ -0,0 +1,11 @@
+export default function ChartCard({name, percentValue, metricValue, chart, rowSpan, colSpan, bgColor, textColor}){
+    return(
+        <section className={`row-span-${rowSpan} col-span-${colSpan} bg-${bgColor} text-${textColor}
+        p-2 shadow-xl border border-gray-200 rounded-xl`}>
+            <p className="font-medium text-base "> {name} </p>
+            <p className="text-xl font-bold">{metricValue}</p>
+            <p>{percentValue}</p>
+            <img src={chart} alt="" />
+        </section>
+    );
+}
