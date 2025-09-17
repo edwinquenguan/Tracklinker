1127ee15fd559afa39452e36a2ff9de3245651ca
Juan <juanesyt7@gmail.com>
2025-09-17 11:19:12 -0500

    Update padding for header, second section nav, third section

--- frontend_web/react_frontend/src/components/Layout/Aside.jsx
+++ frontend_web/react_frontend/src/components/Layout/Aside.jsx
@@ -6,7 +6,7 @@ export default function Aside(){
     return(
         <aside className="row-span-2 px-5 py-5">
             {/* Primera Sección */}
-            <header className="flex min-w-max gap-3 p-[1rem_1rem_0_0] justify-center items-center">
+            <header className="flex min-w-max gap-3 p-[0_1rem] justify-center items-center">
                 <button className="w-[50px] h-[50px]">
                     <img 
                     src={avatarItem.icon} 
@@ -19,13 +19,13 @@ export default function Aside(){
                 </section>
             </header>
             {/* Segunda Sección */}
-            <nav className="p-3 pl-0">
-                <ul className="flex flex-col gap-[3px] p-1 overflow-hidden">
+            <nav className="py-4 px-1">
+                <ul className="flex flex-col gap-[3px] overflow-hidden">
                     {/* Esto lo que hace es recorrer la constante y traer los datos uno a uno e ir creando un li para cada uno */}
                     {firstSectionItems.map((item, index) => (
                     <li 
                     key={index} 
-                    className="p-2.5 rounded-lg hover:bg-gray-300 transition duration-300">
+                    className="p-2.5 rounded-lg hover:bg-gray-200 transition duration-400">
                         <NavLink
                             to={item.path}
                             className="flex gap-3.5 items-center justify-center xl:justify-start"
@@ -41,11 +41,11 @@ export default function Aside(){
                 </ul>
             </nav>
             {/* Tercera Sección */}
-            <nav className="p-4 pt-6 pl-0">
+            <nav className="py-4 px-1">
                 <p className="text-base py-1 pl-3 text-gray-500">
                     Otros
                 </p>
-                <ul className="flex flex-col gap-[3px] p-1">
+                <ul className="flex flex-col gap-[3px]">
                     {secondSectionItems.map((item, index) => (
                     <li 
                     key={index} 
