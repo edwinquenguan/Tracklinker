9d2244d5c639321398901165ef836ab9fd200523
Juan <juanesyt7@gmail.com>
2025-09-17 18:48:19 -0500

    feat: add new animation for aside

--- frontend_web/react_frontend/tailwind.config.js
+++ frontend_web/react_frontend/tailwind.config.js
@@ -4,12 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        aside: {
+          from: { opacity:"0", transform: "translateX(-100%)"},
+          to: { opacity: "1", transform: "translateX(0%)"},
+        },
+      },
+      animation: {
+        aside: "aside 0.5s ease-out forwards",
+      },
+    },
   },
   plugins: [],
   safelist: [
-    'row-span-2', 'row-span-4',
-    'col-span-2', 'col-span-3', 'col-span-4', 'col-span-6',
+    'row-span-2', 
+    'row-span-4',
+    'col-span-2', 
+    'col-span-3', 
+    'col-span-4', 
+    'col-span-6',
   ],
-}
+};
 
