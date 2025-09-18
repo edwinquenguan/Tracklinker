934c7f2ca89b4cae2b822d6479ec1fd926e3f155
Juan <juanesyt7@gmail.com>
2025-09-18 13:02:30 -0500

    feat: update styles for users container

--- frontend_web/react_frontend/src/pages/UsersPage.jsx
+++ frontend_web/react_frontend/src/pages/UsersPage.jsx
@@ -13,27 +13,27 @@ export default function UsersPage(){
             addButtonText={"Agregar Usuario"}
             />
             {/* Contenedor de los usuarios */}
-            <ul className="min-h-[90%] max-w-full pt-3 overflow-x-auto overflow-y-auto">
+            <ul className="max-h-[90%] max-w-full pt-3 flex flex-col gap-1 overflow-x-auto overflow-y-auto">
                 {users.map((user) => (
                     // Usuarios   
-                        <li className="flex items-center justify-between p-5 bg-[#f3eef5] rounded-xl shadow-md">
+                        <li className="flex items-center justify-between p-5 bg-[#f3eef5] dark:bg-[#0f0f11] rounded-lg shadow-md">
                             {/* Datos del Usuario */}
                             <article>
-                                <address className="flex gap-3 not-italic font-medium">
-                                    <p className="text-2xl">{user.name} {user.firstSurname} {user.secondSurname}</p>
+                                <address className="flex gap-3 not-italic font-medium dark:text-white">
+                                    <p className="text-xl">{user.name} {user.firstSurname} {user.secondSurname}</p>
                                     <div className="flex items-center">
-                                        <img src={usersIcons.phoneIcon} alt="" className="w-5 h-5" />
+                                        <img src={usersIcons.phoneIcon} alt="" className="w-5 h-5 dark:invert" />
                                         <p>{user.phone}</p>
                                     </div>
                                     <div className="flex items-center">
-                                        <img src={usersIcons.rolIcon} alt="" className="w-5 h-5" />
+                                        <img src={usersIcons.rolIcon} alt="" className="w-5 h-5 dark:invert" />
                                         <p>{user.rol}</p>
                                     </div>
                                 </address>
                             </article>
 
                             {/* Botones para interactuar con el usuario */}
-                            <nav className="flex gap-2">
+                            <nav className="flex gap-2 dark:invert">
                                 <button> <img src={actionsIcons.moreInfoIcon} alt="" /> </button>
                                 <button> <img src={actionsIcons.editInfoIcon} alt="" /> </button>
                                 <button> <img src={actionsIcons.deleteIcon} alt="" /></button>
