61faea4745a42c2a790a024317b6b39b12f78dda
Juan <juanesyt7@gmail.com>
2025-09-17 22:46:20 -0500

    feat: add styles for dark mode to Create button component

--- frontend_web/react_frontend/src/components/ui/CreateButton.jsx
+++ frontend_web/react_frontend/src/components/ui/CreateButton.jsx
@@ -1,8 +1,10 @@
 export default function CreateButton({icon, text}){
     return(
-        <button className="flex items-center px-5 py-2.5 gap-2 bg-black rounded-3xl 
-        dark:bg-white shadow-md"> 
-            <img src={icon} alt="" className="w-6 h-6 invert brightness-0"/>
+        <button className="flex items-center px-5 py-2.5 gap-2 bg-black rounded-3xl transition duration-500
+        dark:bg-white shadow-[0px_0px_32px_-12px_#000000]
+        hover:shadow-[0px_0px_32px_-6px_#000000]
+        dark:hover:shadow-[0px_0px_32px_-11px_#ffffff]">
+            <img src={icon} alt="" className="w-6 h-6 invert brightness-0 dark:brightness-200 dark:invert-0"/>
             <p className="text-base text-white font-medium dark:text-black"> {text} </p>
         </button>
     );
