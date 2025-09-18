ebd5a7adcd6546c9e39786ac6728b22139a1b252
Juan <juanesyt7@gmail.com>
2025-09-18 15:52:31 -0500

    feat: add main content for reports page

--- frontend_web/react_frontend/src/pages/ReportsPage.jsx
+++ frontend_web/react_frontend/src/pages/ReportsPage.jsx
@@ -1,9 +1,14 @@
 import Layout from "../components/Layout/Layout";
+import ActionCard from "../components/ui/ActionCard";
 
 export default function ReportsPage(){
     return(
         <Layout>
-
+            <h1 className="px-2 py-3 font-medium dark:text-white"> Informes </h1>
+            {/* Contenedor de las cards */}
+            <section className="h-[90%] min-w-full">
+                <ActionCard/>
+            </section>
         </Layout>
     )
 }
