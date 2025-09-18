3758c18fee195c7f31c2c6299b5819577292a44b
Juan <juanesyt7@gmail.com>
2025-09-18 11:18:36 -0500

    feat: add new animation for active sections on aside

--- frontend_web/react_frontend/tailwind.config.js
+++ frontend_web/react_frontend/tailwind.config.js
@@ -13,11 +13,16 @@ module.exports = {
         activeOption: {
           from: { opacity: "0", transition:"background-color(#000000) .5s" },
           to: { opacity: "1", transition:"background-color(#FFFFFF) .5s" },
-        }
+        },
+        slideIn: {
+          '0%': { transform: 'translateX(-5px)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
       },
       animation: {
         aside: "aside 0.7s ease-out forwards",
         activeOption : "activeOption 0.5s ease-out",
+        slideIn: 'slideIn 0.5s ease-out',
       },
     },
   },
