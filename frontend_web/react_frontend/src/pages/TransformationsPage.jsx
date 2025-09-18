58cf578215e5d3d3b5612b5a14bc69aaa7bc6158
Juan <juanesyt7@gmail.com>
2025-09-18 15:35:47 -0500

    feat: update main content for transformations page

--- frontend_web/react_frontend/src/pages/TransformationsPage.jsx
+++ frontend_web/react_frontend/src/pages/TransformationsPage.jsx
@@ -1,9 +1,29 @@
+import { actionsIcons, warrantiesIcons } from "../assets/icons/mainIcons"
+import { transformations } from "../data/transformations";
 import Layout from "../components/Layout/Layout";
+import TopSection from "../components/ui/TopSection";
+import TranformationCard from "../components/ui/TransformationCard";
 
 export default function TransformationsPage(){
     return(
         <Layout>
-
+            <TopSection
+            sectionName={"Transformaciones"}
+            addButtonIcon={actionsIcons.addIcon}
+            addButtonText={"Agregar Transformación"}    
+            />
+            {/* Contendor de las transformaciones */}
+            <section className="max-h-[95%] flex flex-wrap gap-2 z-50 overflow-x-auto overflow-y-auto">
+                {transformations.map((transformation) => (
+                    <TranformationCard
+                    transformationId={transformation.transformationId}
+                    transformationRequirement={transformation.transformationRequirement}
+                    transformationCreateDate={transformation.transformationCreateDate}
+                    transformationStatusIcon={warrantiesIcons.inprocessIcon}
+                    transformationStatus={transformation.transformationStatus}
+                    />
+                ))}
+            </section>
         </Layout>
     )
 }
