d7244ad96c601f6de71b4a4553f8bf4acb45d677
Juan <juanesyt7@gmail.com>
2025-09-18 11:05:41 -0500

    feat: add styles for dark mode on welcome text

--- frontend_web/react_frontend/src/pages/HomePage.jsx
+++ frontend_web/react_frontend/src/pages/HomePage.jsx
@@ -4,11 +4,11 @@ import ActionCard from "../components/ui/ActionCard";
 export default function HomePage(){
     return(        
         <Layout>
-            <h1 className="h-[10%] p-5 text-5xl font-medium"> Bienvenido, Agustín </h1>
+            <h1 className="h-[10%] p-5 text-5xl font-medium dark:text-white"> Bienvenido, Agustín </h1>
             {/* Contenedor de las cards de los modúlos */}
             <section className="h-[90%] min-w-full">
                 <ActionCard/>
-            </section>  
+            </section>
         </Layout>
     )
 }
