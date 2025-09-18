2077f4710cc316b5cf937c2a77a0c865aa0017c7
Juan <juanesyt7@gmail.com>
2025-09-17 19:26:44 -0500

    Update colors to icon on createButton component

--- frontend_web/react_frontend/src/components/ui/CreateButton.jsx
+++ frontend_web/react_frontend/src/components/ui/CreateButton.jsx
@@ -2,7 +2,7 @@ export default function CreateButton({icon, text}){
     return(
         <button className="flex items-center px-6 py-3 gap-2 bg-black rounded-3xl 
         dark:bg-white shadow-md"> 
-            <img src={icon} alt="" className="w-6 h-6 brightness-100"/>
+            <img src={icon} alt="" className="w-6 h-6 invert brightness-0"/>
             <p className="text-base text-white font-medium dark:text-black"> {text} </p>
         </button>
     );
