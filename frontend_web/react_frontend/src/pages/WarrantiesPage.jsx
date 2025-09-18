8c96c134a7b75d34392748ce4fd56137fe22b5ac
Juan <juanesyt7@gmail.com>
2025-09-18 14:00:58 -0500

    feat: add main content on warranties page

--- frontend_web/react_frontend/src/pages/WarrantiesPage.jsx
+++ frontend_web/react_frontend/src/pages/WarrantiesPage.jsx
@@ -1,9 +1,29 @@
 import Layout from "../components/Layout/Layout";
+import WarrantyCard from "../components/ui/WarrantyCard";
+import TopSection from "../components/ui/TopSection";
+import { warranties } from "../data/warranties";
+import { warrantiesIcons} from "../assets/icons/mainIcons";
 
 export default function WarrantiesPage(){
     return(
         <Layout>
-
+            <TopSection 
+            sectionName={"Garantías"}
+            addButtonIcon={warrantiesIcons.addWarrantyIcon}
+            addButtonText={"Agregar Garantía"}
+            />
+            {/* Contenedor de las cards de garantia */}
+            <section className="max-h-[95%] flex flex-wrap gap-2 z-50 overflow-x-auto overflow-y-auto">
+                {warranties.map((warranty) => (
+                    <WarrantyCard
+                    warrantyId={warranty.warrantyId}
+                    warrantyRequirement={warranty.warrantyRequirement}
+                    warratyCreateDate={warranty.warrantyCreateDate}
+                    warrantyStatusIcon={warrantiesIcons.inprocessIcon}
+                    warrantyStatus={warranty.warrantyStatus}
+                    />
+                ))}
+            </section>
         </Layout>
     )
 }
