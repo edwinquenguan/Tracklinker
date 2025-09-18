eb0ae5efb2ad446265c5bf2e0b7a1230665eb060
Juan <juanesyt7@gmail.com>
2025-09-17 20:54:05 -0500

    Update padding for filter button

--- frontend_web/react_frontend/src/components/ui/FilterButton.jsx
+++ frontend_web/react_frontend/src/components/ui/FilterButton.jsx
@@ -2,7 +2,7 @@ import { actionsIcons } from "../../assets/icons/mainIcons";
 
 export default function FilterButton(){
     return(
-        <button className="flex items-center px-6 py-3 gap-2 bg-gray-200 shadow-md rounded-3xl">
+        <button className="flex items-center px-5 py-2.5 gap-2 bg-gray-200 shadow-md rounded-3xl">
             <img src={actionsIcons.filterIcon} alt="" className="w-6 h-6"/>
                 <p className="text-base font-medium"> Filtrar </p> 
         </button>
