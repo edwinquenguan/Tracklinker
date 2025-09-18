c3eaf57acd5b0373e665dfca545ce4d69062daa3
Juan <juanesyt7@gmail.com>
2025-09-18 12:11:30 -0500

    feat: create new component Top Section for first section on main content for all pages

---
+++ frontend_web/react_frontend/src/components/ui/TopSection.jsx
@@ -0,0 +1,17 @@
+import FilterButton from "./FilterButton";
+import CreateButton from "./CreateButton";
+
+export default function TopSection({sectionName, addButtonIcon, addButtonText}){
+    return(
+        <section className="flex items-center justify-between px-2 pb-3">
+            <h1 className="font-medium">{sectionName}</h1>
+            <section className="flex gap-4">
+                <FilterButton />
+                <CreateButton 
+                icon = {addButtonIcon}
+                text = {addButtonText}
+                />
+            </section>
+        </section>
+    );
+}
