970b78cdf1b24fce28c85833edd7ed450383abdb
Juan <juanesyt7@gmail.com>
2025-09-17 21:29:55 -0500

    Update main content padding

--- frontend_web/react_frontend/src/components/Layout/Layout.jsx
+++ frontend_web/react_frontend/src/components/Layout/Layout.jsx
@@ -5,7 +5,7 @@ export default function Layout({children}){
         // Container 
         <div className="w-full h-full grid grid-cols-[15%_85%] grid-rows-[6%_94%]">
             <Aside />
-            <main className="w-full h-full p-6 dark:bg-black">
+            <main className="w-full h-full px-1 py-4 dark:bg-black">
                 {children}
             </main>
         </div>
