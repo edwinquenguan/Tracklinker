2bb63a0edb1ccd188866461b9e91afb3ad11208e
Juan <juanesyt7@gmail.com>
2025-09-17 20:37:56 -0500

    feat: remove header section

--- frontend_web/react_frontend/src/components/Layout/Layout.jsx
+++ frontend_web/react_frontend/src/components/Layout/Layout.jsx
@@ -1,4 +1,3 @@
-import Header from "./Header";
 import Aside from "./Aside";
 
 export default function Layout({children}){
@@ -6,7 +5,6 @@ export default function Layout({children}){
         // Container 
         <div className="w-full h-full grid grid-cols-[15%_85%] grid-rows-[6%_94%]">
             <Aside />
-            <Header />
             <main className="w-full h-full p-6 dark:bg-black">
                 {children}
             </main>
