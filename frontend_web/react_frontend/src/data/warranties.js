7d72ca8e22a3727d0fac070a8a06e1b2840f8e17
Juan <juanesyt7@gmail.com>
2025-09-18 14:00:20 -0500

    feat: add new warranties constant

---
+++ frontend_web/react_frontend/src/data/warranties.js
@@ -0,0 +1,4 @@
+export const warranties = [
+    {warrantyId: "001", warrantyRequirement:"Cambio de Pantalla", warrantyCreateDate:"2025/09/20", warrantyStatus:"en Proceso"},
+    {warrantyId: "002", warrantyRequirement:"Cambio de teclado", warrantyCreateDate:"2025/09/20", warrantyStatus:"en Proceso"},
+]
