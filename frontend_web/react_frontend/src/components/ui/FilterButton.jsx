ddc78c63f577fec35b4234924dd97fdf65a6df64
Juan <juanesyt7@gmail.com>
2025-09-18 13:04:22 -0500

    Update hover for filter button on dark mode

--- frontend_web/react_frontend/src/components/ui/FilterButton.jsx
+++ frontend_web/react_frontend/src/components/ui/FilterButton.jsx
@@ -3,7 +3,7 @@ import { actionsIcons } from "../../assets/icons/mainIcons";
 export default function FilterButton(){
     return(
         <button className="flex items-center px-5 py-2.5 gap-2 shadow-[0px_0px_32px_5px_#efedf0] rounded-3xl bg-[#efedf0] transition duration-500
-        dark:bg-[#202022] dark:shadow-none dark:hover:shadow-[10px_0px_32px_-5px_#202022]
+        dark:bg-[#202022] dark:shadow-none dark:hover:bg-[#2c2c2e]
         hover:shadow-2xl">
             <img src={actionsIcons.filterIcon} alt="" className="w-6 h-6 dark:invert-[.7]"/>
                 <p className="text-base font-medium dark:text-gray-300"> Filtrar </p> 
