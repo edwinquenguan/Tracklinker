cf24bc2442a90a4f56eb8a5ec3a02e7d507be1f2
Juan <juanesyt7@gmail.com>
2025-09-18 11:04:15 -0500

    feat: update styles for dark mode on Action Card component

--- frontend_web/react_frontend/src/components/ui/ActionCard.jsx
+++ frontend_web/react_frontend/src/components/ui/ActionCard.jsx
@@ -3,18 +3,19 @@ import { items } from "../../constants/homeCards";
 
 export default function ActionCard(){
     return(
-        <section className="grid grid-cols-4 grid-rows-2 p-[100px_350px_200px_350px] min-h-full gap-[20px_12px]">
+        <section className="grid grid-cols-4 grid-rows-2 p-[100px_350px_200px_350px] min-h-full gap-[20px_12px] place-items-center">
             {/* Card de cada modulo */}
             {items.map(item => (
             <NavLink
             to={item.path}
-            className="flex flex-col h-40 w-32 place-items-center gap-2 transition duration-300 
+            className="flex flex-col h-40 w-32 place-items-center gap-2 transition duration-300
             hover:scale-[1.1]"
             >
-                <section className="h-32 w-32 rounded-xl bg-stone-200 content-center justify-items-center">
-                    <img src={item.icon} alt={item.alt} className="w-12"/>
+                <section className="h-32 w-32 rounded-xl bg-stone-200 content-center justify-items-center
+                dark:bg-[#101012]">
+                    <img src={item.icon} alt={item.alt} className="w-12 dark:brightness-[.2] dark:invert"/>
                 </section>
-                <p className="text-base font-medium text-center">{item.name}</p>
+                <p className="text-base font-medium text-center dark:text-white">{item.name}</p>
             </NavLink>
             ))}
         </section>
