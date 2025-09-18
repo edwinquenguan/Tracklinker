3ba741f469bb80219f44423f27a8f6020b5ca4bd
Juan <juanesyt7@gmail.com>
2025-09-17 20:54:17 -0500

    Update padding for create button

--- frontend_web/react_frontend/src/components/ui/CreateButton.jsx
+++ frontend_web/react_frontend/src/components/ui/CreateButton.jsx
@@ -1,6 +1,6 @@
 export default function CreateButton({icon, text}){
     return(
-        <button className="flex items-center px-6 py-3 gap-2 bg-black rounded-3xl 
+        <button className="flex items-center px-5 py-2.5 gap-2 bg-black rounded-3xl 
         dark:bg-white shadow-md"> 
             <img src={icon} alt="" className="w-6 h-6 invert brightness-0"/>
             <p className="text-base text-white font-medium dark:text-black"> {text} </p>
