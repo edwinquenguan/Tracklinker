afddbacd8cb5496d1be5bb768b206e87fc780580
Juan <juanesyt7@gmail.com>
2025-09-19 12:03:42 -0500

    feat: add new constant modal icons to export modal icons and use anywhere

---
+++ frontend_web/react_frontend/src/assets/icons/modalIcons.js
@@ -0,0 +1,13 @@
+import closeIcon from "./modal/close-icon.svg";
+import confirmIcon from "./modal/check-icon.svg";
+import errorIcon from "./modal/error-modal-image.svg";
+import githubIcon from "./modal/github_icon.svg";
+import redirectIcon from "./modal/open-link-icon.svg";
+
+export const modalIcons ={
+    closeIcon,
+    confirmIcon,
+    errorIcon,
+    githubIcon,
+    redirectIcon,
+}
