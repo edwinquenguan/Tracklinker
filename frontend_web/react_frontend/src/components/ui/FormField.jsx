4e5c43e9625dcf0c95b959385224a181704c57b7
Juan <juanesyt7@gmail.com>
2025-09-19 15:31:59 -0500

    Add new styles for form field

--- frontend_web/react_frontend/src/components/ui/FormField.jsx
+++ frontend_web/react_frontend/src/components/ui/FormField.jsx
@@ -1,7 +1,7 @@
 export default function FormField({labelText, id, type = "text", placeholder}) {
     return(
         <div className="flex flex-col items-center">
-            <label htmlFor={id}>{labelText}</label>
+            <label htmlFor={id} className="self-start">{labelText}</label>
             <input type={type} placeholder={placeholder} id={id} className="px-2 py-2 border text-sm rounded-md outline-none"/>
         </div>
     )
