c71bada676cfc8a73325a77aa12690942568cfb0
Juan <juanesyt7@gmail.com>
2025-09-19 15:56:26 -0500

    feat: add new modal for filter

---
+++ frontend_web/react_frontend/src/components/ui/FilterModal.jsx
@@ -0,0 +1,38 @@
+import { modalIcons } from "../../assets/icons/modalIcons";
+
+export default function FilterModal({title, isOpen, onClose,}){
+    return(
+        <div 
+            className={`fixed inset-0 z-50 flex items-center justify-center bg-[#0000002c]
+                ${isOpen 
+                ? "opacity-100" 
+                : "opacity-0 pointer-events-none"}
+            `}
+            onClick={onClose}>
+                {/* stopPropagation sirve para que al momento de seleccionar la modal no la cierre */}
+                <div 
+                className={`bg-white rounded-xl shadow-lg w-[90%] max-w-xl p-6 relative
+                    ${isOpen 
+                    ? "animate-modalFadeIn" 
+                    : "animate-modalFadeOut"}
+                `}
+                onClick={(e) => e.stopPropagation()}
+                >
+                    {/* Cabecera de la modal donde esta el titúlo y el icono para cerrarla */}
+                    <header className="flex justify-between items-center mb-4">
+                        <h2 className="text-lg font-medium">{title}</h2>
+                        {/* Icono "x" para cerrar la modal */}
+                        <button
+                            onClick={onClose}
+                        >
+                            <img src={modalIcons.closeIcon} alt="" className="p-1.5 rounded-3xl invert brightness-200 transition duration-300 hover:bg-[#111111]" />
+                        </button>
+                    </header>
+                {/* Contenido principal de la modal o cuerpo de la modal */}
+                <div>
+                    
+                </div>
+              </div>
+            </div>
+    );
+}
