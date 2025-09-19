7ba62a5248ffebb491abb99280237a2df7901dd9
Juan <juanesyt7@gmail.com>
2025-09-19 13:08:52 -0500

    feat: add new prop "children" to children content

--- frontend_web/react_frontend/src/components/ui/TopSection.jsx
+++ frontend_web/react_frontend/src/components/ui/TopSection.jsx
@@ -1,11 +1,12 @@
 import FilterButton from "./FilterButton";
 import CreateButton from "./CreateButton";
 
-export default function TopSection({sectionName, addButtonIcon, addButtonText}){
+export default function TopSection({sectionName, addButtonIcon, addButtonText, children}){
     return(
         <section className="flex items-center justify-between px-2 pb-3">
             <h1 className="font-medium">{sectionName}</h1>
             <section className="flex gap-4">
+                {children}
                 <FilterButton />
                 <CreateButton 
                 icon = {addButtonIcon}
