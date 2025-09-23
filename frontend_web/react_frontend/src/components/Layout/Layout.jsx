62a2191f35e386ff7cf48050849e6976d917c80e
Juan <juanesyt7@gmail.com>
2025-09-23 16:22:51 -0500

    Update styles for responsive

--- frontend_web/react_frontend/src/components/Layout/Layout.jsx
+++ frontend_web/react_frontend/src/components/Layout/Layout.jsx
@@ -3,7 +3,9 @@ import Aside from "./Aside";
 export default function Layout({children}){
     return(
         // Container 
-        <div className="w-full h-full grid grid-cols-[15%_85%] grid-rows-1">
+        <div className="w-full h-full grid grid-rows-[1fr_80px] grid-cols-1
+        xl:grid-cols-[270px_85%]
+        md:grid-cols-[130px_1fr]">
             <Aside />
             <main className="min-w-full max-h-full px-1 py-4 dark:bg-black">
                 {children}
