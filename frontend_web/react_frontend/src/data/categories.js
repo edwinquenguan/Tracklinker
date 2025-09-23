6507088ccebc85f8f1e612d0eac6863005ce082e
Juan <juanesyt7@gmail.com>
2025-09-23 16:17:59 -0500

    feat: update data for all sections

--- frontend_web/react_frontend/src/data/categories.js
+++ frontend_web/react_frontend/src/data/categories.js
@@ -1,4 +1,4 @@
 export const categories = [
-    { name: 'Computadores'},
-    { name: 'Impresoras' }
+    { name: 'Computadores', createAt: '2025/08/20' },
+    { name: 'Impresoras', createAt: '2025/08/31' }
 ]
--- frontend_web/react_frontend/src/data/subcategories.js
+++ frontend_web/react_frontend/src/data/subcategories.js
@@ -1,4 +1,4 @@
 export const subcategories = [
-    { name: 'Computadores de mesa'},
-    { name: 'Impresoras laser' }
+    { name: 'Computadores de mesa', createAt:'2025/09/08', category:'Computadores'},
+    { name: 'Impresoras laser', createAt:'2025/09/19', category:'Impresoras' }
 ]
