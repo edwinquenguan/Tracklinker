1137f1d9db666d18aef4a0d7f64f6fa3392274d1
Juan <juanesyt7@gmail.com>
2025-09-24 14:37:30 -0500

    feat: add new auth service

---
+++ frontend_web/react_frontend/src/services/authService.js
@@ -0,0 +1,18 @@
+import { supabase } from "../supabase/supabaseClient";
+
+export async function signUp(email, password) {
+  return await supabase.auth.signUp({ email, password });
+}
+
+export async function signIn(email, password) {
+  return await supabase.auth.signInWithPassword({ email, password });
+}
+
+export async function signOut() {
+  return await supabase.auth.signOut();
+}
+
+export async function getUser() {
+  const { data: { user } } = await supabase.auth.getUser();
+  return user;
+}
