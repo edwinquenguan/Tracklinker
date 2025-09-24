eb5eff3296cf466f11421f701a3317e4a157b49d
Juan <juanesyt7@gmail.com>
2025-09-24 14:38:24 -0500

    feat: add services for warranties data page

--- frontend_web/react_frontend/src/pages/WarrantiesPage.jsx
+++ frontend_web/react_frontend/src/pages/WarrantiesPage.jsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
+import { getWarranties } from "../services/warranties";
 import Layout from "../components/Layout/Layout";
 import WarrantyCard from "../components/ui/WarrantyCard";
 import TopSection from "../components/ui/TopSection";
-import { warranties } from "../data/warranties";
+// import { warranties } from "../data/warranties";
 import { warrantiesIcons} from "../assets/icons/mainIcons";
 
 export default function WarrantiesPage(){
+    const [warranties, setWarranties] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    // Esto llama a la función getWarranties y espera a obtener toda los datos y los almacena en "data"
+    useEffect(() => {
+        async function fetchWarranties() {
+            try {
+                setLoading(true)
+                const data = await getWarranties();
+                setWarranties(data);
+            } catch (error) {
+                setError(error.message);
+            }
+        }
+
+    fetchWarranties();
+    }, []);
+
+    if(error) {
+
+    }
+
     return(
         <Layout>
             <TopSection 
@@ -16,11 +41,11 @@ export default function WarrantiesPage(){
             <section className="max-h-[95%] flex flex-wrap gap-2 z-50 overflow-x-auto overflow-y-auto">
                 {warranties.map((warranty) => (
                     <WarrantyCard
-                    warrantyId={warranty.warrantyId}
-                    warrantyRequirement={warranty.warrantyRequirement}
-                    warrantyCreateDate={warranty.warrantyCreateDate}
+                    warrantyId={warranty.warranty_incidents_id}
+                    warrantyRequirement={warranty.warranty_description}
+                    warrantyCreateDate={warranty.warranty_date}
                     warrantyStatusIcon={warrantiesIcons.inprocessIcon}
-                    warrantyStatus={warranty.warrantyStatus}
+                    warrantyStatus={warranty.warranty_status}
                     />
                 ))}
             </section>
