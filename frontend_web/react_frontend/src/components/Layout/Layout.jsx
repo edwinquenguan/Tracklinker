bb527f9eafab556152eff20e402292a8dd0fa89e
Juan <juanesyt7@gmail.com>
2025-09-24 12:59:06 -0500

    feat: update reponsive styles to layout

--- frontend_web/react_frontend/src/components/Layout/Layout.jsx
+++ frontend_web/react_frontend/src/components/Layout/Layout.jsx
@@ -3,11 +3,12 @@ import Aside from "./Aside";
 export default function Layout({children}){
     return(
         // Container 
-        <div className="w-full h-full grid grid-rows-[1fr_80px] grid-cols-1
-        xl:grid-cols-[270px_85%]
-        md:grid-cols-[130px_1fr]">
+        <div className="w-full h-full grid grid-cols-1
+        xl:grid-cols-[270px_85%] xl:grid-rows-1
+        md:grid-cols-[130px_1fr] md:grid-rows-1
+        sm:grid-rows-[1fr_100px]">
             <Aside />
-            <main className="min-w-full max-h-full px-1 py-4 dark:bg-black">
+            <main className="min-w-full max-h-full px-1 py-4 dark:bg-black overflow-hidden">
                 {children}
             </main>
         </div>
