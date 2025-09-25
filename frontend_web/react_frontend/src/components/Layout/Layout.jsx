dc6bae8bee7327c6d01044fcc896bf7e56cdc644
Juan <juanesyt7@gmail.com>
2025-09-25 14:51:14 -0500

    feat: add new parameter and use it

--- frontend_web/react_frontend/src/components/Layout/Layout.jsx
+++ frontend_web/react_frontend/src/components/Layout/Layout.jsx
@@ -1,13 +1,15 @@
 import Aside from "./Aside";
 
-export default function Layout({children}){
+export default function Layout({avatarOnClick, children}){
     return(
         // Container 
         <div className="w-full h-full grid grid-cols-1
         xl:grid-cols-[270px_85%] xl:grid-rows-1
         md:grid-cols-[130px_1fr] md:grid-rows-1
         sm:grid-rows-[1fr_100px]">
-            <Aside />
+            <Aside 
+            avatarOnClick={avatarOnClick}
+            />
             <main className="min-w-full max-h-full px-1 py-4 dark:bg-black overflow-hidden">
                 {children}
             </main>
