4cfc735a8b38127291d1bf61b7545cbe609348c6
Juan <juanesyt7@gmail.com>
2025-09-25 14:37:10 -0500

    update styles for dark mode on filter modal

--- frontend_web/react_frontend/src/components/modals/FilterModal.jsx
+++ frontend_web/react_frontend/src/components/modals/FilterModal.jsx
@@ -3,19 +3,22 @@ import FormField from "../ui/FormField";
 export default function FilterModal({onClose, children}) {
     return(
         <section className="flex flex-col gap-3">
-            <section className="flex flex-col px-2">
-                <span className="text-sm font-medium">Ordenar por Nombre: </span>
-                <select name="name-option" id="filter-name-option" className="p-2 border outline-none">
+            <section className="flex flex-col px-2 gap-1">
+                <span className="text-sm font-medium dark:text-white">Ordenar por Nombre: </span>
+                <select name="name-option" id="filter-name-option" className="p-2 rounded-lg border outline-none 
+                dark:bg-[#2020226c] dark:border-[#101012] dark:text-white">
                     <option value="asc" className="">A - Z</option>
                     <option value="desc">Z - A </option>
                 </select>
             </section>
 
-            <section className="flex flex-col px-2">
-                <span className="text-sm font-medium">Ordenar por Fecha de Creación: </span>
+            <section className="flex flex-col px-2 gap-1">
+                <span className="text-sm font-medium dark:text-white">Ordenar por Fecha de Creación: </span>
                 <div className="flex gap-2 justify-center">
-                    <input type="date" className="p-2 border text-sm outline-none"/>
-                    <input type="date" className="p-2 border text-sm outline-none"/>
+                    <input type="date" className="p-3 rounded-lg border text-sm outline-none
+                    dark:bg-[#2020226c] dark:border-[#101012] dark:text-white"/>
+                    <input type="date" className="p-3 rounded-lg border text-sm outline-none
+                    dark:bg-[#2020226c] dark:border-[#101012] dark:text-white"/>
                 </div>
             </section>
 
@@ -24,8 +27,8 @@ export default function FilterModal({onClose, children}) {
         {/* Botones de aplicar y cancelar */}
             <section className="p-2 flex items-center justify-end gap-4">
                 <button 
-                className="px-3 py-3 rounded-lg text-sm bg-gray-200 font-medium
-                dark:text-white"
+                className="px-4 py-3 rounded-lg text-sm bg-gray-200 font-medium transition
+                dark:text-white dark:bg-transparent dark:hover:bg-[#101012]"
                 onClick={onClose}> 
                     Cancelar 
                 </button>
