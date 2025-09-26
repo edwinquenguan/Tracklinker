d97fec7c9cc0f2436b0bf08e32276f6641ea8b82
Juan <juanesyt7@gmail.com>
2025-09-26 15:29:01 -0500

    feat: add new prop to form field component

--- frontend_web/react_frontend/src/components/ui/FormField.jsx
+++ frontend_web/react_frontend/src/components/ui/FormField.jsx
@@ -1,8 +1,12 @@
-export default function FormField({labelText, id, type = "text", placeholder}) {
+export default function FormField({labelText, id, type = "text", placeholder, autoComplete = ""}) {
     return(
         <div className="flex flex-col items-center">
             <label htmlFor={id} className="self-start">{labelText}</label>
-            <input type={type} placeholder={placeholder} id={id} className="px-6 py-3 border text-sm rounded-md outline-none"/>
+            <input type={type} 
+            placeholder={placeholder} 
+            id={id} 
+            className="px-6 py-3 border text-sm rounded-md outline-none"
+            autoComplete={autoComplete}/>
         </div>
     )
 }
