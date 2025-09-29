7c31ce3df94dbc20dfbb413839e4d6fdb5de2c6e
Juan <juanesyt7@gmail.com>
2025-09-29 16:56:12 -0500

    chore: update padding for input and add styles for dark mode on form field

--- frontend_web/react_frontend/src/globals/components/ui/FormField.jsx
+++ frontend_web/react_frontend/src/globals/components/ui/FormField.jsx
@@ -2,13 +2,14 @@ export default function FormField({labelText, inputIcon, id, type = "text", plac
     return(
         <div className="flex flex-col items-center">
             <label htmlFor={id} className="self-start text-sm dark:text-white">{labelText}</label>
-            <div className="flex justify-between border pl-2">
-                <img src={inputIcon} alt="" className="invert brightness-200" />
+            <div className="flex justify-between border pl-1.5 
+            dark:bg-[#2020226c] dark:border-[#101012]">
+                <img src={inputIcon} alt="" className="invert brightness-200 dark:brightness-0" />
                 <input type={type}
                 placeholder={placeholder}
                 id={id} 
                 className="px-6 py-3 text-sm rounded-md outline-none
-                dark:bg-[#2020226c] dark:border-[#101012] dark:text-white"
+                dark:bg-transparent dark:border-[#101012] dark:text-white"
                 autoComplete={autoComplete}/>
             </div>
         </div>
