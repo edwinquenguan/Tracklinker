b86ab3ac737cf6a39b1c25fb97c4763738d38ae9
Juan <juanesyt7@gmail.com>
2025-09-29 16:28:24 -0500

    chore: change the button text size on confirm and cancel buttons component

--- frontend_web/react_frontend/src/globals/components/modals/ConfirmCancelButtons.jsx
+++ frontend_web/react_frontend/src/globals/components/modals/ConfirmCancelButtons.jsx
@@ -3,14 +3,14 @@ export default function ConfirmCancelButtons({confirmText = "Confirmar", cancelT
         <section className="flex items-center pt-6 gap-3">
             <button
             onClick={confirmButtonOnClick}
-            className="bg-black text-white px-4 py-3 rounded-xl
+            className="text-sm bg-black text-white px-4 py-3 rounded-xl transition duration-300
             hover:text-gray-300
             dark:bg-white dark:text-black dark:hover:text-gray-800">
                 {confirmText}
             </button>
             <button
             onClick={cancelButtonOnClick}
-            className="px-4 py-3 rounded-xl transition duration-300
+            className="px-4 py-3 rounded-xl text-sm transition duration-300
             hover:bg-gray-200
             dark:text-white dark:hover:bg-[#101012]">
                 {cancelText}
