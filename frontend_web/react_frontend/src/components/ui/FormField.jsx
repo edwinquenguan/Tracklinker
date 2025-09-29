cf763719b00eee8e146afebbac1be0eabcf4f95e
Juan <juanesyt7@gmail.com>
2025-09-29 13:26:26 -0500

    feat: add styles for dark mode on form field

--- frontend_web/react_frontend/src/components/ui/FormField.jsx
+++ frontend_web/react_frontend/src/components/ui/FormField.jsx
@@ -1,11 +1,12 @@
 export default function FormField({labelText, id, type = "text", placeholder, autoComplete = ""}) {
     return(
         <div className="flex flex-col items-center">
-            <label htmlFor={id} className="self-start">{labelText}</label>
+            <label htmlFor={id} className="self-start dark:text-white">{labelText}</label>
             <input type={type} 
             placeholder={placeholder} 
             id={id} 
-            className="px-6 py-3 border text-sm rounded-md outline-none"
+            className="px-6 py-3 border text-sm rounded-md outline-none
+            dark:bg-[#2020226c] dark:border-[#101012] dark:text-white"
             autoComplete={autoComplete}/>
         </div>
     )
