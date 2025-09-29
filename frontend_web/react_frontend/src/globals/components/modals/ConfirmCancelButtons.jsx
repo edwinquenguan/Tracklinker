fffaf0c7f659f6768e2ccc2724d10680764dfe20
Juan <juanesyt7@gmail.com>
2025-09-29 16:22:24 -0500

    feat: add new prop for confirm text and cancel text

--- frontend_web/react_frontend/src/globals/components/modals/ConfirmCancelButtons.jsx
+++ frontend_web/react_frontend/src/globals/components/modals/ConfirmCancelButtons.jsx
@@ -1,4 +1,4 @@
-export default function ConfirmCancelButtons({confirmButtonOnClick, cancelButtonOnClick}) {
+export default function ConfirmCancelButtons({confirmText = "Confirmar", cancelText = "Cancelar", confirmButtonOnClick, cancelButtonOnClick}) {
     return(
         <section className="flex items-center pt-6 gap-3">
             <button
@@ -6,14 +6,14 @@ export default function ConfirmCancelButtons({confirmButtonOnClick, cancelButton
             className="bg-black text-white px-4 py-3 rounded-xl
             hover:text-gray-300
             dark:bg-white dark:text-black dark:hover:text-gray-800">
-                Confirmar
+                {confirmText}
             </button>
             <button
             onClick={cancelButtonOnClick}
             className="px-4 py-3 rounded-xl transition duration-300
             hover:bg-gray-200
             dark:text-white dark:hover:bg-[#101012]">
-                Cancelar
+                {cancelText}
             </button>
         </section>
     );
