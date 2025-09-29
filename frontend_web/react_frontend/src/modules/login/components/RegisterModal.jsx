47b6f080331d960960597921c995ac8e2963d957
Juan <juanesyt7@gmail.com>
2025-09-29 16:29:13 -0500

    feat: add new props in form fields on register modal

--- frontend_web/react_frontend/src/modules/login/components/RegisterModal.jsx
+++ frontend_web/react_frontend/src/modules/login/components/RegisterModal.jsx
@@ -4,11 +4,15 @@ export default function RegisterModal() {
     return(
         <section className="flex flex-col items-center">
             <form action="" method="post" className="flex flex-col gap-3">
-            <FormField 
+            <FormField
+            type={"email"}
+            id={"email-input"} 
             labelText={"Email"}
             placeholder={"pepito@pepito.com"}
             />
-            <FormField 
+            <FormField
+            type={"password"}
+            id={"password-input"}
             labelText={"Contraseña"}
             placeholder={"********"}
             />
