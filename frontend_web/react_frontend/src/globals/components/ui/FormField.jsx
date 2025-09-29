df390e8d5389a1ef0c64c2e76ae881c9fe058e5e
Juan <juanesyt7@gmail.com>
2025-09-29 16:37:35 -0500

    fat: add new section for add a image inside of input on form field

--- frontend_web/react_frontend/src/globals/components/ui/FormField.jsx
+++ frontend_web/react_frontend/src/globals/components/ui/FormField.jsx
@@ -1,13 +1,16 @@
-export default function FormField({labelText, id, type = "text", placeholder, autoComplete = ""}) {
+export default function FormField({labelText, inputIcon, id, type = "text", placeholder, autoComplete = ""}) {
     return(
         <div className="flex flex-col items-center">
             <label htmlFor={id} className="self-start text-sm dark:text-white">{labelText}</label>
-            <input type={type} 
-            placeholder={placeholder} 
-            id={id} 
-            className="px-6 py-3 border text-sm rounded-md outline-none
-            dark:bg-[#2020226c] dark:border-[#101012] dark:text-white"
-            autoComplete={autoComplete}/>
+            <div className="flex justify-between border pl-2">
+                <img src={inputIcon} alt="" className="invert brightness-200" />
+                <input type={type}
+                placeholder={placeholder}
+                id={id} 
+                className="px-6 py-3 text-sm rounded-md outline-none
+                dark:bg-[#2020226c] dark:border-[#101012] dark:text-white"
+                autoComplete={autoComplete}/>
+            </div>
         </div>
     )
 }
