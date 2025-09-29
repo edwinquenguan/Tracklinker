01c5263db6fd2fc1d49b676b0fdb49a00049ad87
Juan <juanesyt7@gmail.com>
2025-09-29 16:57:56 -0500

    feat: add new props "flexDirection" and "cancelButtonWidth" on confirm cancel buttons components

--- frontend_web/react_frontend/src/globals/components/modals/ConfirmCancelButtons.jsx
+++ frontend_web/react_frontend/src/globals/components/modals/ConfirmCancelButtons.jsx
@@ -1,18 +1,18 @@
-export default function ConfirmCancelButtons({confirmText = "Confirmar", cancelText = "Cancelar", confirmButtonOnClick, cancelButtonOnClick}) {
+export default function ConfirmCancelButtons({flexDirection, cancelButtonWidth, confirmText = "Confirmar", cancelText = "Cancelar", confirmButtonOnClick, cancelButtonOnClick}) {
     return(
-        <section className="flex items-center pt-6 gap-3">
+        <section className={`flex ${flexDirection} items-center pt-6 gap-2`}>
             <button
             onClick={confirmButtonOnClick}
-            className="text-sm bg-black text-white px-4 py-3 rounded-xl transition duration-300
+            className="font-medium text-sm bg-black text-white px-4 py-3 rounded-lg transition duration-300
             hover:text-gray-300
             dark:bg-white dark:text-black dark:hover:text-gray-800">
                 {confirmText}
             </button>
             <button
             onClick={cancelButtonOnClick}
-            className="px-4 py-3 rounded-xl text-sm transition duration-300
+            className={`${cancelButtonWidth} px-4 py-3 rounded-lg text-sm transition duration-300
             hover:bg-gray-200
-            dark:text-white dark:hover:bg-[#101012]">
+            dark:text-white dark:hover:bg-[#101012]`}>
                 {cancelText}
             </button>
         </section>
