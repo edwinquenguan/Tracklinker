8361a6bd1f973619f35cadcd2ff2b2e2f62489b2
Juan <juanesyt7@gmail.com>
2025-09-29 16:38:24 -0500

    feat: add new icon and import on login icons

--- frontend_web/react_frontend/src/assets/icons/loginIcons.js
+++ frontend_web/react_frontend/src/assets/icons/loginIcons.js
@@ -2,10 +2,12 @@ import userIcon from "./login/user-icon.svg";
 import hidePasswordIcon from "./login/hidepass-icon.svg";
 import showPasswordIcon from "./login/showpass-icon.svg";
 import tracklinkerIcon from "./login/logo.svg";
+import emailIcon from "./login/email-icon.svg";
 
 export const loginIcons = {
     tracklinkerIcon,
     userIcon,
     showPasswordIcon,
     hidePasswordIcon,
+    emailIcon,
 }
