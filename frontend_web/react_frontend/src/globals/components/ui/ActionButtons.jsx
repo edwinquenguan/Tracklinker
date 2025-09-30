4dac07d7334fa7b225a7a0e9f8a9a8721ae59adb
Juan <juanesyt7@gmail.com>
2025-09-30 13:01:13 -0500

    style: refine button icon styles in ActionButtons component for better dark mode visibility

--- frontend_web/react_frontend/src/globals/components/ui/ActionButtons.jsx
+++ frontend_web/react_frontend/src/globals/components/ui/ActionButtons.jsx
@@ -2,13 +2,13 @@ import { actionsIcons } from "../../../assets/icons/mainIcons";
 
 export default function ActionButtons({children, editButtonOnClick, deleteButtonOnClick}) {
     return(
-        <section className="flex items-center justify-center gap-5">
+        <section className="flex items-center justify-center gap-5 dark:invert">
             {children}
             <button onClick={editButtonOnClick}> 
-                <img src={actionsIcons.editInfoIcon} alt="" className="dark:invert" />     
+                <img src={actionsIcons.editInfoIcon} alt="" className="dark:brightness-200" />     
             </button>
             <button onClick={deleteButtonOnClick}>  
-                <img src={actionsIcons.deleteIcon} alt="" className="dark:invert" />
+                <img src={actionsIcons.deleteIcon} alt="" className="dark:brightness-200" />
             </button>
         </section>
     );
