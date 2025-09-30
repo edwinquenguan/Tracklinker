e988e45af706b02161ce964d171e150c55347dcb
Juan <juanesyt7@gmail.com>
2025-09-30 12:17:34 -0500

    fix: add styles for dark mode on chart card component

--- frontend_web/react_frontend/src/globals/components/ui/ChartCard.jsx
+++ frontend_web/react_frontend/src/globals/components/ui/ChartCard.jsx
@@ -5,6 +5,7 @@ export default function ChartCard({name, percentValue, metricValue, chart, rowSp
         <section className={`row-span-${rowSpan} col-span-${colSpan} bg-${bgColor} text-${textColor} dark:text-white
         flex flex-col p-3 shadow-xl border border-gray-200 rounded-xl transition duration-500
         hover:bg-gray-200 hover:scale-105
+        dark:bg-[#0f0f11] dark:border-transparent dark:shadow-[0px_0px_10px_2px_#0f0f11] dark:hover:bg-[#2c2c2e]
         `}>
             <div className="flex items-start justify-between">
                 <div>
@@ -16,7 +17,9 @@ export default function ChartCard({name, percentValue, metricValue, chart, rowSp
                 </div>
                 <NavLink
                 to="/reports"
-                className="p-2 rounded-lg text-sm border transition duration-500 hover:bg-gray-300"
+                className="p-2 rounded-lg text-sm border transition duration-500 
+                hover:bg-gray-300
+                dark:shadow-[0px_0px_10px_2px_#0f0f11] dark:border-transparent dark:hover:bg-[#0f0f11]"
                 >
                     Ver Informe
                 </NavLink>
