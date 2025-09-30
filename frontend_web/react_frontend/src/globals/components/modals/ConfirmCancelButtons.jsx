cc52bfaf5ef84580454b78942bb6e5d25b34f0d0
Juan <juanesyt7@gmail.com>
2025-09-30 12:36:46 -0500

    feat: add confirmBgColor prop to ConfirmCancelButtons for customizable button background

--- frontend_web/react_frontend/src/globals/components/modals/ConfirmCancelButtons.jsx
+++ frontend_web/react_frontend/src/globals/components/modals/ConfirmCancelButtons.jsx
@@ -1,11 +1,11 @@
-export default function ConfirmCancelButtons({flexDirection, cancelButtonWidth, confirmText = "Confirmar", cancelText = "Cancelar", confirmButtonOnClick, cancelButtonOnClick}) {
+export default function ConfirmCancelButtons({flexDirection, confirmText = "Confirmar", confirmBgColor = "black", confirmButtonOnClick, cancelText = "Cancelar", cancelButtonWidth, cancelButtonOnClick}) {
     return(
         <section className={`flex ${flexDirection} items-center pt-6 gap-2`}>
             <button
             onClick={confirmButtonOnClick}
-            className="font-medium text-sm bg-black text-white px-4 py-3 rounded-lg transition duration-300
+            className={`font-medium text-sm bg-${confirmBgColor} text-white px-4 py-3 rounded-lg transition duration-300
             hover:text-gray-300
-            dark:bg-white dark:text-black dark:hover:text-gray-800">
+            dark:bg-white dark:text-black dark:hover:text-gray-800`}>
                 {confirmText}
             </button>
             <button
