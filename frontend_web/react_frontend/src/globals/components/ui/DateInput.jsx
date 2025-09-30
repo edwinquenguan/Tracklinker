bfbaf403b08499d89cda765d389696859fa6e8fe
Juan <juanesyt7@gmail.com>
2025-09-30 16:14:20 -0500

    feat: add DateInput component for date selection with customizable label

---
+++ frontend_web/react_frontend/src/globals/components/ui/DateInput.jsx
@@ -0,0 +1,12 @@
+export default function DateInput({spanText, id}) {
+    return(
+        <section className="flex flex-col gap-1">
+            <span className="text-sm dark:text-white">{spanText}</span>
+            <input
+            id={id}
+            type="date" 
+            className="p-3 rounded-lg border text-sm outline-none
+            dark:bg-[#2020226c] dark:border-[#101012] dark:text-white"/>
+        </section>
+    );
+}
