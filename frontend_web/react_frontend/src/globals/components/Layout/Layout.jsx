a4aa51f09c5b60b6192442324031bbc520c410d1
Juan <juanesyt7@gmail.com>
2025-10-01 16:09:25 -0500

    refactor: enhance Layout component's grid structure for improved responsiveness and order

--- frontend_web/react_frontend/src/globals/components/Layout/Layout.jsx
+++ frontend_web/react_frontend/src/globals/components/Layout/Layout.jsx
@@ -3,14 +3,15 @@ import Aside from "./Aside";
 export default function Layout({avatarOnClick, children}){
     return(
         // Container 
-        <div className="w-full h-full grid grid-cols-1
+        <div className="w-full h-full grid grid-cols-1 grid-rows-[1fr_100px]
+        md:grid-cols-[120px_1fr] md:grid-rows-1
         xl:grid-cols-[270px_85%] xl:grid-rows-1
-        md:grid-cols-[130px_1fr] md:grid-rows-1
-        sm:grid-rows-[1fr_100px]">
+        ">
             <Aside 
             avatarOnClick={avatarOnClick}
             />
-            <main className="min-w-full max-h-full px-1 py-4 dark:bg-black overflow-hidden">
+            <main className="min-w-full max-h-full px-1 py-4 dark:bg-black overflow-hidden order-1
+            xl:order-2 md:order-2">
                 {children}
             </main>
         </div>
