d7ed90835b37a046d193db2c098d1cc6d31ae8db
Juan <juanesyt7@gmail.com>
2025-09-30 19:16:04 -0500

    Update supabaseClient.js

--- frontend_web/react_frontend/src/supabase/supabaseClient.js
+++ frontend_web/react_frontend/src/supabase/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_API_KEY;
+const supabaseUrl = "https://ovqigziznbqcbouegafq.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92cWlneml6bmJxY2JvdWVnYWZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzMjg1MzYsImV4cCI6MjA3MzkwNDUzNn0.OTKlFhbIkz8mW4NbKMzOKWmWNvE7gCVKQIdVD0e0F2g";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
