990128997df42a2b40a6bc52a120457eb1e41aa5
Juan <juanesyt7@gmail.com>
2025-10-01 12:01:12 -0500

    feat: implement useCatalog and useModal hooks for product management

---
+++ frontend_web/react_frontend/src/modules/products/hooks/useModal.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export function useModal() {
+
+    const [modalType, setModalType] = useState(null);
+    const [selectedProduct, setSelectedProduct] = useState(null);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openModal = (product, type) => {
+        setSelectedProduct(product);
+        setModalType(type);
+        setIsOpen(true);
+    }
+
+    const closeModal = () => {
+        setSelectedProduct(null);
+        setModalType(null);
+        setIsOpen(false);
+    }
+
+    return {
+        modalType,
+        selectedProduct,
+        isOpen,
+        openModal,
+        closeModal,
+    }
+}
