a8feb840d6bbb7dbc8cc55558fd60a3570c6c278
Juan <juanesyt7@gmail.com>
2025-10-02 15:22:03 -0500

    refactor: improve formatting of report sections for better readability

--- frontend_web/react_frontend/src/modules/reports/data/reportSections.js
+++ frontend_web/react_frontend/src/modules/reports/data/reportSections.js
@@ -1,11 +1,47 @@
 import { asideIcons } from "../../../assets/icons/asideIcons";
 
 export const sections = [
-    { name:"Reporte De Usuarios", modalName: "reportUsers", icon: asideIcons.usersIcon, alt:""},
-    { name:"Reporte De Productos",  modalName: "reportProducts", icon: asideIcons.productsIcon, alt:""},
-    { name:"Reporte De Categorias", modalName: "reportCategories", icon: asideIcons.categoriesIcon, alt:""},
-    { name:"Reporte De Subcategorias", modalName: "reportSubcategories", icon: asideIcons.subcategoriesIcon, alt:""},
-    { name:"Reporte De Garantías", modalName: "reportWarranties", icon: asideIcons.warrantiesIcon, alt:""},
-    { name:"Reporte De Proveedores", modalName: "reportSuppliers", icon: asideIcons.suppliersIcon, alt:""},
-    { name:"Reporte De Transformaciones", modalName: "reportTranformations", icon: asideIcons.transformationsIcon, alt:""},
+    { 
+        name:"Reporte De Usuarios",
+        modalName: "reportUsers", 
+        icon: asideIcons.usersIcon, 
+        alt:""
+    
+    },
+    { 
+        name:"Reporte De Productos",  
+        modalName: "reportProducts", 
+        icon: asideIcons.productsIcon, 
+        alt:""
+    },
+    { 
+        name:"Reporte De Categorias", 
+        modalName: "reportCategories", 
+        icon: asideIcons.categoriesIcon,
+         alt:""
+        },
+    { 
+        name:"Reporte De Subcategorias", 
+        modalName: "reportSubcategories", 
+        icon: asideIcons.subcategoriesIcon, 
+        alt:""
+    },
+    { 
+        name:"Reporte De Garantías", 
+        modalName: "reportWarranties", 
+        icon: asideIcons.warrantiesIcon, 
+        alt:""
+    },
+    { 
+        name:"Reporte De Proveedores", 
+        modalName: "reportSuppliers", 
+        icon: asideIcons.suppliersIcon, 
+        alt:""
+    },
+    { 
+        name:"Reporte De Transformaciones", 
+        modalName: "reportTranformations", 
+        icon: asideIcons.transformationsIcon, 
+        alt:""
+    },
 ];
