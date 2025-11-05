20aae10ab77bc956d846792f44233a1c5510c888
DevJuan001 <juanesyt7@gmail.com>
2025-11-05 16:13:48 -0500

    feat: implement JWT token creation and validation functions in security module

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,15 +1,65 @@
 from datetime import datetime, timedelta
 from typing import Union
+import bcrypt
 from jose import jwt, JWTError
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from app.core.config import settings
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 
+# Función para crear el jwt con fecha de expiración
 def create_access_token(
     data: dict,
     expires_delta: Union[timedelta, None] = None
 ) -> str:
     
     to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+            to_encode, 
+            settings.SECRET_KEY, 
+            algorithm=settings.ALGORITHM
+        )
+
+    return encoded_jwt
+
+# Función para verificar el token en todas las solicitudes protegidas
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="No se pudieron validar las credenciales",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(
+            token, 
+            settings.SECRET_KEY, 
+            algorithms=[settings.ALGORITHM]
+        )
+
+        user_id: str = payload.get("sub")
+
+        if user_id is None:
+            raise credentials_exception
+
+    except JWTError:
+        raise credentials_exception
+    
+    return user_id
+
+
+def verify_credentials(user, password: str):
+    password_bytes = password.encode("utf-8")
+    hashed_bytes = user["user_password"].encode("utf-8")
+
+    if not bcrypt.checkpw(password_bytes, hashed_bytes):
+        raise HTTPException(status_code=401, detail="Contraseña Incorrecta")
+    
+    return True
