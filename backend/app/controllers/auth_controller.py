2b41b90f67c5462b4124bf7475c08e75e0c77f29
DevJuan001 <juanesyt7@gmail.com>
2025-11-06 16:10:39 -0500

    refactor: enhance AuthController with detailed docstring, update password verification function, and fix subject key from int to str and add new key named role

--- backend/app/controllers/auth_controller.py
+++ backend/app/controllers/auth_controller.py
@@ -1,11 +1,27 @@
 from fastapi import HTTPException, Depends
 from datetime import timedelta
-from app.core.security import verify_credentials
+from app.core.security import verify_password
 from app.core.security import create_access_token
 from app.repository.user_repository import UserRepository
 from app.core.config import settings
 
 class AuthController:
+    """
+    Controlador de autenticaión
+
+    Esta clase se encarga de gestionar las operaciones relacionadas con
+    la autenticación por el momento solo maneja una que sería el inicio de sesión "Login".
+
+    Metodos:
+        login(email: str, password: str):
+            Verifica las credenciales del usuario y retorna un JWT para que pueda realizar
+            o acceder a diferentes rutas.
+            
+
+    Nota:
+        Este controlador debe estar relacionado o integrarse con el repository el cual se comunica
+        con la base de datos para poder validar las credenciales del usuario.
+    """
     @staticmethod
     def login(email: str, password: str):
         user = UserRepository.find_by_email(email)
@@ -15,12 +31,17 @@ class AuthController:
             raise HTTPException(status_code=401, detail="Usuario No encontrado")
         
         # Validación de los parametros recibidos
-        verify_credentials(user, password)
+        verify_password(user, password)
 
         # Tiempo en que expira el token
         expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE)
+
         # Creación del token
-        token = create_access_token({"sub": email}, expires_delta=expires)
+        token = create_access_token({
+            "sub": str(user["user_id"]),
+            "role": user["rol_name"]
+            }, 
+            expires_delta=expires)
 
         return{
             "token_type": "Bearer",
