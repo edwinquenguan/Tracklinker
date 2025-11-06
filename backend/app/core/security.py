19eac6adaa4dbe77c3e2436c76b43e4fad41592e
DevJuan001 <juanesyt7@gmail.com>
2025-11-05 21:13:30 -0500

    fix: update the deprecated utcnow function to now(timezone.utc)

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Union
 import bcrypt
 from jose import jwt, JWTError
@@ -17,13 +17,13 @@ def create_access_token(
     to_encode = data.copy()
 
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
-            to_encode, 
-            settings.SECRET_KEY, 
+            to_encode,
+            settings.SECRET_KEY,
             algorithm=settings.ALGORITHM
         )
 
