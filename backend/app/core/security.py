29e2fc792727e6ebb5a73f373509e10f7caca759
DevJuan001 <juanesyt7@gmail.com>
2025-11-06 16:08:07 -0500

    refactor: simplify JWT token creation and update password verification function

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,11 +1,12 @@
 from datetime import datetime, timedelta, timezone
 from typing import Union
 import bcrypt
-from jose import jwt, JWTError
-from fastapi import Depends, HTTPException
+from jose import jwt
+from fastapi import HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from app.core.config import settings
 
+# Ruta en la cúal los usuarios obtienen el login
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 
 # Función para crear el jwt con fecha de expiración
@@ -14,52 +15,37 @@ def create_access_token(
     expires_delta: Union[timedelta, None] = None
 ) -> str:
     
+    # Aqui guardamos una copia de el diccionario "data" dentro de "to_encode"
     to_encode = data.copy()
 
+    # Validación
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
+
+    # Convierte la llave sub en una cadena de texto
+    to_encode["sub"] = str(to_encode["sub"])
+    
+    # Aqui guardamos todo dentro del string del jwt
     encoded_jwt = jwt.encode(
             to_encode,
             settings.SECRET_KEY,
             algorithm=settings.ALGORITHM
         )
-
+    
     return encoded_jwt
 
-# Función para verificar el token en todas las solicitudes protegidas
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=401,
-        detail="No se pudieron validar las credenciales",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
-    try:
-        payload = jwt.decode(
-            token, 
-            settings.SECRET_KEY, 
-            algorithms=[settings.ALGORITHM]
-        )
-
-        user_id: str = payload.get("sub")
-
-        if user_id is None:
-            raise credentials_exception
-
-    except JWTError:
-        raise credentials_exception
-    
-    return user_id
-
-
-def verify_credentials(user, password: str):
+# Función para verificar la contraseña del usuario
+def verify_password(user, password: str):
     password_bytes = password.encode("utf-8")
     hashed_bytes = user["user_password"].encode("utf-8")
 
     if not bcrypt.checkpw(password_bytes, hashed_bytes):
-        raise HTTPException(status_code=401, detail="Contraseña Incorrecta")
+        raise HTTPException(
+            status_code=401, 
+            detail="Contraseña Incorrecta"
+        )
     
     return True
