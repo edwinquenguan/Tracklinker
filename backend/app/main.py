98aa1ea0a69bb76173ce7df4d6354331656f0a85
DevJuan001 <juanesyt7@gmail.com>
2025-11-07 17:01:15 -0500

    feat: add CORS middleware to enable cross origin requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import get_connection
-from app.routes import user_routes
-from app.routes import auth_routes
+from app.routes import user_routes, auth_routes
 
 # Instancia principal de la app FastAPI
 app = FastAPI(
     title="API con FastAPI y MySQL",
     description="Api para tracklinker",
     version="1.0.0",
+)
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Endpoint para probar conexión a la base de datos
@@ -30,7 +39,7 @@ def root():
     return {
         "message": "API funcionando"
     }
-
+# Rutas de autenticación
+app.include_router(auth_routes.router)
 # Rutas para el modúlo de Usuarios
 app.include_router(user_routes.router)
-app.include_router(auth_routes.router)
