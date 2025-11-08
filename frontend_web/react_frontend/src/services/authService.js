06298784d756c9df87cc54a70a04468bbed48260
DevJuan001 <juanesyt7@gmail.com>
2025-11-07 20:02:26 -0500

    feat: implement login functionality with API integration and localStorage token management

--- frontend_web/react_frontend/src/services/authService.js
+++ frontend_web/react_frontend/src/services/authService.js
@@ -1,18 +1,42 @@
 import { supabase } from "../supabase/supabaseClient";
 
-export async function signUp(email, password) {
-  return await supabase.auth.signUp({ email, password });
-}
+const apiUrl = process.env.REACT_APP_API_URL
 
-export async function signIn(email, password) {
-  return await supabase.auth.signInWithPassword({ email, password });
-}
+// Función para loguearse 
+export async function login(email, password) {
+  try{
+    const res = await fetch(`${apiUrl}/api/auth/login`,{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "email": email,
+        "password": password
+      })
+    });
 
+    // Validamos si el estado de la respuesta no fue 200
+    if (!res.ok) {
+      throw new Error("Credenciales Invalidas")
+    }
+
+    // Almacenamos la respuesta y la pasamos a json
+    const data = await res.json()
+
+    // Guardamos el token en localStorage con su tipo
+    localStorage.setItem("token", `${data.token_type} ${data.access_token}`)
+    
+    return data;
+
+  } catch (error) {
+    console.error("Error", error)
+    return null
+  }
+};
+
+// Función para cerrar la sesión
+// TODO: Agregar contenido a la función y no usar supabase
 export async function signOut() {
   return await supabase.auth.signOut();
-}
-
-export async function getUser() {
-  const { data: { user } } = await supabase.auth.getUser();
-  return user;
 }
