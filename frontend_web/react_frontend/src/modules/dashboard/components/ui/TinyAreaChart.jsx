2f2e43fd0904a951e41b041db7f78ca04142a652
DevJuan001 <juanesyt7@gmail.com>
2025-11-12 14:57:47 -0500

    feat: add TinyAreaChart and TinyBarChart components

---
+++ frontend_web/react_frontend/src/modules/dashboard/components/ui/TinyAreaChart.jsx
@@ -0,0 +1,10 @@
+import { Area, AreaChart } from "recharts";
+
+export default function TinyAreaChart() {
+  return (
+    // Grafico en forma de ola 🌊
+    <AreaChart responsive>
+      <Area />
+    </AreaChart>
+  );
+}
---
+++ frontend_web/react_frontend/src/modules/dashboard/components/ui/TinyBarChart.jsx
@@ -0,0 +1,10 @@
+import { BarChart, Bar } from "recharts";
+
+// Grafico de barras 
+export default function TinyBarChart() {
+  return (
+    <BarChart>
+      <Bar />
+    </BarChart>
+  );
+}
