4d6db4dc579350aac149c5e90717f0ff12d781b0
petcard2025 <petcard.si@gmail.com>
2025-11-11 19:49:11 -0500

    feat create subcategories endpoints

--- backend/app/routes/subcategories_routes.py
+++ backend/app/routes/subcategories_routes.py
@@ -3,9 +3,9 @@ from app.controllers.subcategories_controller import SubcategoriesController
 
 router = APIRouter(
     prefix="/api/subcategories",
-    tags=["Auth"]
+    tags=["subcategories"]
 )
 
-@router.post("/")
+@router.get("/")
 def get_all_subcategories():
     return SubcategoriesController.get_all_subcategories()
