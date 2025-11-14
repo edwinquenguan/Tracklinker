452edaede9c13648d8852313b12449eb51b5a37c
DevJuan001 <juanesyt7@gmail.com>
2025-11-14 13:53:00 -0500

    feat: implement TinyBarChart component with data visualization

--- frontend_web/react_frontend/src/modules/dashboard/components/ui/TinyBarChart.jsx
+++ frontend_web/react_frontend/src/modules/dashboard/components/ui/TinyBarChart.jsx
@@ -1,10 +1,31 @@
-import { BarChart, Bar } from "recharts";
+import { BarChart, Bar, XAxis } from "recharts";
+import useBarChartData from "../hooks/useBarChartData";
+import ChartCard from "./ChartCard";
 
 // Grafico de barras 
 export default function TinyBarChart() {
+
+  const { barChartData, error} = useBarChartData();
+
   return (
-    <BarChart>
-      <Bar />
-    </BarChart>
+    // Contenedor del gráfico
+    <ChartCard
+    colSpan={4}
+    rowSpan={2}
+    name={"Entradas Mensuales de cada proveedor"}
+    >
+      {/* Gráfico */}
+      <BarChart
+      width="100%"
+      height="100%"
+      responsive
+      data={barChartData}
+      >
+        {/* Eje x donde salen los nombres */}
+        <XAxis dataKey={"supplier_name"} fontSize={"6px"} fontWeight={800}/>
+        {/* Barras del gráfico */}
+        <Bar dataKey="orders" fill="#4050e7"/>
+      </BarChart>
+    </ChartCard>
   );
 }
