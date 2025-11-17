9b1643dc63626b3fb73f7be459d9d7456c642180
DevJuan001 <juanesyt7@gmail.com>
2025-11-17 14:05:41 -0500

    feat: add monthly outputs endpoint to dashboard routes

--- backend/app/routes/dashboard_routes.py
+++ backend/app/routes/dashboard_routes.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter, Depends
 from app.controllers.dashboard_controller import DashboardController
+from app.middlewares.roles_middleware import require_roles
 
 router = APIRouter(
     prefix="/api/dashboard",
     tags=["Dashboard"]
 )
 
+# Endopoint para obtener las entradas mensuales de cada proveedor
 @router.get("/monthly-inputs")
 def get_monthly_suppliers_inputs():
     return DashboardController.get_all_monthly_supplier_inputs()
+
+# Endpoint para obtener las salidas mensuales con su mes y año
+@router.get("/monthly-outputs")
+def get_monthly_outputs():
+    return DashboardController.get_all_outputs()
