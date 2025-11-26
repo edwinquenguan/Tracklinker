9f30b27356a5943391e97911f611387ac378ce75
DevJuan001 <juanesyt7@gmail.com>
2025-11-26 15:42:05 -0500

    fix: Update pie chart data structure for compatibility with recharts

--- frontend_web/react_frontend/src/modules/dashboard/hooks/usePieChart.js
+++ frontend_web/react_frontend/src/modules/dashboard/hooks/usePieChart.js
@@ -10,7 +10,12 @@ export function usePieChart() {
     async function fetchPieChartData() {
       try {
         const data = await pieChartData();
-        setSimplePieChartData(data);
+        // Almacenamos cada item de data dentro de un array de objetos para que sea usable con recharts
+        const pieData = [
+          { name: "Sin completar", value: data[0].total, color: "#2f3ab5" },
+          { name: "En proceso", value: data[1].total, color: "#12e396" },
+        ];
+        setSimplePieChartData(pieData);
       } catch (error) {
         setError(error);
       }
