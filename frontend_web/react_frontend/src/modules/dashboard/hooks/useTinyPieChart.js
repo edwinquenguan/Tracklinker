ec44241e55b4425b9a9c13f71fa0ed53d1f69099
DevJuan001 <juanesyt7@gmail.com>
2025-11-27 15:45:18 -0500

    refactor: Simplify pie chart data handling by directly setting fetched data in useTinyPieChart hook

--- frontend_web/react_frontend/src/modules/dashboard/hooks/useTinyPieChart.js
+++ frontend_web/react_frontend/src/modules/dashboard/hooks/useTinyPieChart.js
@@ -10,12 +10,7 @@ export function useTinyPieChart() {
     async function fetchPieChartData() {
       try {
         const data = await tinyPieChartData();
-        // Almacenamos cada item de data dentro de un array de objetos para que sea usable con recharts
-        const pieData = [
-          { name: "Usuarios", value: data[0].users, color: "#2f3ab5" },
-          { name: "Nuevos usuarios", value: data[1].new_users, color: "#12e396" },
-        ];
-        setTinyPieChartInfo(pieData);
+        setTinyPieChartInfo(data);
       } catch (error) {
         setError(error);
       }
