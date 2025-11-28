7b3b874d16937e283ce0d2d105c61883f3499dcc
DevJuan001 <juanesyt7@gmail.com>
2025-11-28 15:32:59 -0500

    refactor: Remove obsolete data files for categories, products, subcategories, suppliers, users, and warranties

--- frontend_web/react_frontend/src/data/subcategories.js
+++
@@ -1,4 +0,0 @@
-export const subcategories = [
-    { name: 'Computadores de mesa', createAt:'2025/09/08', category:'Computadores'},
-    { name: 'Impresoras laser', createAt:'2025/09/19', category:'Impresoras' }
-]
