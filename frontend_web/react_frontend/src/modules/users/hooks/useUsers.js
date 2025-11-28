79a339a242954ad9ab7833f7d093b96c4af605d0
DevJuan001 <juanesyt7@gmail.com>
2025-11-28 15:33:50 -0500

    feat: Create useUsers hook for fetching and managing user data with loading and error states

---
+++ frontend_web/react_frontend/src/modules/users/hooks/useUsers.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { getUsers } from "../services/getUsers";
+
+export function useUsers() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Este effect llama al service getAllUsers y espera a obtener todos los datos y los almacena en "data"
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const data = await getUsers();
+        setUsers(data);
+        setLoading(false)
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+
+    fetchUsers();
+  }, []);
+
+  return { users, loading, error };
+}
