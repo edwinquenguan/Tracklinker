18711361b2065a5d2f9670c3e88ac8df1982b0fa
DevJuan001 <juanesyt7@gmail.com>
2025-12-01 13:45:17 -0500

    refactor: Simplify DashboardPage component by integrating useModal hook and replacing individual chart components with ChartsContainer

--- frontend_web/react_frontend/src/modules/dashboard/DashboardPage.jsx
+++ frontend_web/react_frontend/src/modules/dashboard/DashboardPage.jsx
@@ -1,118 +1,31 @@
-// States
-import { useState } from "react";
+// Hooks
+import { useModal } from "../../globals/hooks/useModal";
 // Iconos
-import { dashboardIcons } from "../../assets/icons/mainIcons";
 import { actionsIcons } from "../../assets/icons/mainIcons";
 // Componentes
 import Modal from "../../globals/components/modals/Modal";
 import Layout from "../../globals/components/Layout/Layout";
 import TopSection from "../../globals/components/ui/TopSection";
-// Gráficos
-import ChartCard from "./components/ui/ChartCard";
-import SimpleBarChart from "./components/ui/SimpleBarChart";
-import SimpleAreaChart from "./components/ui/SimpleAreaChart";
-import SimplePieChart from "./components/ui/SimplePieChart";
 // Modales
 import DownloadModal from "./components/modals/DownloadModal";
 import FilterModal from "../../globals/components/modals/FilterModal";
 import ProfileModal from "../../globals/components/modals/ProfileModal";
-import TinyPieChart from "./components/ui/TinyPieChart";
+import ChartsContainer from "./components/ui/ChartsContainer";
 
 export default function DashBoardPage() {
-  const [modalType, setModalType] = useState(null);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const openModal = (type) => {
-    setModalType(type);
-  };
-
-  const closeModal = () => {
-    setModalType(null);
-  };
+  const { modalType, isOpen, openModal, closeModal } = useModal();
 
   return (
-    <Layout
-      avatarOnClick={() => {
-        openModal("user");
-        setIsOpen(true);
-      }}
-    >
+    <Layout avatarOnClick={() => openModal("user")}>
       <TopSection
         sectionName={"Panel De Control"}
         addButtonIcon={actionsIcons.uploadIcon}
         addButtonText={"Descargar"}
-        createOnClick={() => {
-          openModal("download");
-          setIsOpen(true);
-        }}
-        filterOnClick={() => {
-          openModal("filter");
-          setIsOpen(true);
-        }}
+        createOnClick={() => openModal("download")}
+        filterOnClick={() => openModal("filter")}
       />
       {/* Container de los gráficos */}
-      <section
-        className="grid max-h-[95%] p-2 transition duration-300 ease-in-out
-            xl:grid-cols-12 xl:grid-rows-5
-            sm:grid-cols-1 sm:grid-rows-4 gap-5"
-      >
-        {/* Primera Fila de Gráficos */}
-        <TinyPieChart />
-        <ChartCard
-          rowSpan={1}
-          colSpan={3}
-          bgColor={""}
-          textColor={"black"}
-          name={"Chart"}
-          metricValue={"2.000"}
-          percentValue={"2.1%"}
-          chart={dashboardIcons.firstChart}
-        />
-        <ChartCard
-          rowSpan={1}
-          colSpan={3}
-          bgColor={""}
-          textColor={"black"}
-          name={"Chart"}
-          metricValue={"2.000"}
-          percentValue={"2.1%"}
-          chart={dashboardIcons.firstChart}
-        />
-        <ChartCard
-          rowSpan={1}
-          colSpan={3}
-          bgColor={""}
-          textColor={"black"}
-          name={"Chart"}
-          metricValue={"2.000"}
-          percentValue={"2.1%"}
-          chart={dashboardIcons.firstChart}
-        />
-        {/* Segunda Fila de Gráficos */}
-        <SimpleAreaChart />
-        <ChartCard
-          rowSpan={2}
-          colSpan={3}
-          bgColor={""}
-          name={"Chart"}
-          metricValue={"2.000"}
-          percentValue={"2.1%"}
-          chart={dashboardIcons.barChart2}
-        />
-        <SimplePieChart />
-
-        {/* Tercera fila de Gráficos */}
-        <ChartCard
-          rowSpan={2}
-          colSpan={5}
-          name={"Chart"}
-          metricValue={"2.000"}
-          percentValue={"2.1%"}
-          chart={dashboardIcons.waveChart}
-          imageSize={"w-full h-[80%]"}
-        />
-        <SimpleBarChart />
-      </section>
+      <ChartsContainer />
 
       {/* Modales */}
       {modalType && (
@@ -126,26 +39,13 @@ export default function DashBoardPage() {
           }
           type={modalType}
           isOpen={isOpen}
-          onClose={() => {
-            closeModal();
-            setIsOpen(false);
-          }}
+          onClose={() => closeModal()}
         >
           {modalType === "user" && (
-            <ProfileModal
-              onClose={() => {
-                closeModal();
-                setIsOpen(false);
-              }}
-            />
+            <ProfileModal onClose={() => closeModal()} />
           )}
           {modalType === "filter" && (
-            <FilterModal
-              onClose={() => {
-                closeModal();
-                setIsOpen(false);
-              }}
-            />
+            <FilterModal onClose={() => closeModal()} />
           )}
           {modalType === "download" && <DownloadModal />}
         </Modal>
