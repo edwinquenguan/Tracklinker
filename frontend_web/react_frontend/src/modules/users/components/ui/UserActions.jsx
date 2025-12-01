d6d81ae9cc10ba3dc41030bd9809db448b1dc62a
DevJuan001 <juanesyt7@gmail.com>
2025-12-01 14:37:57 -0500

    refactor: Update UserActions component to accept button click handlers as props for improved flexibility

--- frontend_web/react_frontend/src/modules/users/components/ui/UserActions.jsx
+++ frontend_web/react_frontend/src/modules/users/components/ui/UserActions.jsx
@@ -1,26 +1,17 @@
 import { actionsIcons } from "../../../../assets/icons/mainIcons";
 import ActionButtons from "../../../../globals/components/ui/ActionButtons";
 
-export default function UserActions() {
+export default function UserActions({editButtonOnClick, deleteButtonOnClick, moreInfoOnClick}) {
   return (
     /* Botones para interactuar con el usuario */
     <nav className="flex gap-4">
       <ActionButtons
-        editButtonOnClick={() => {
-          openModal(user, "edit");
-          setIsOpen(true);
-        }}
-        deleteButtonOnClick={() => {
-          openModal(user, "delete");
-          setIsOpen(true);
-        }}
+        editButtonOnClick={editButtonOnClick}
+        deleteButtonOnClick={deleteButtonOnClick}
       >
         {/* Botón de más información del usuario */}
         <button
-          onClick={() => {
-            openModal(user, "info");
-            setIsOpen(true);
-          }}
+          onClick={moreInfoOnClick}
         >
           <img src={actionsIcons.moreInfoIcon} alt="" />
         </button>
