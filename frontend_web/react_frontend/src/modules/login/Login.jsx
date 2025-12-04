21708a0d0a4561ed7be48bc29ff6a8574f85bd73
DevJuan001 <juanesyt7@gmail.com>
2025-12-04 14:26:22 -0500

    refactor: Simplify Login component by removing unused hooks and props from LoginForm

--- frontend_web/react_frontend/src/modules/login/Login.jsx
+++ frontend_web/react_frontend/src/modules/login/Login.jsx
@@ -1,6 +1,5 @@
 // Hooks
 import { useModal } from "../../globals/hooks/useModal";
-import { useLogin } from "./hooks/useLogin";
 // Componentes
 import LoginForm from "./components/ui/LoginForm";
 // Modales
@@ -10,19 +9,11 @@ import RecoverPasswordModal from "./components/modals/RecoverPasswordModal";
 
 export default function Login() {
   const { modalType, isOpen, openModal, closeModal } = useModal();
-  const { setEmail, setPassword, handleLogin } = useLogin(openModal);
 
   return (
     <section className="w-screen h-screen flex items-center justify-center">
       {/* Formulario */}
-      <LoginForm
-        getIntoButtonOnclick={handleLogin}
-        recoverButtonOnclick={() => {
-          openModal(null, "rememberPassword");
-        }}
-        setEmail={(e) => setEmail(e.target.value)}
-        setPassword={(e) => setPassword(e.target.value)}
-      />
+      <LoginForm openModal={openModal} />
       {modalType && (
         <Modal
           title={modalType === "rememberPassword" ? "Olvide Mi Contraseña" : ""}
