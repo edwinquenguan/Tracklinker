2a7e7426c1fe9dc9da03ffcb8fba5127610f8acf
DevJuan001 <juanesyt7@gmail.com>
2025-12-04 16:14:23 -0500

    feat: Implement AddUserModal with user creation form and role selection

--- frontend_web/react_frontend/src/modules/users/components/modals/AddUserModal.jsx
+++ frontend_web/react_frontend/src/modules/users/components/modals/AddUserModal.jsx
@@ -1,55 +1,127 @@
 import SelectMenu from "../../../../globals/components/modals/SelectMenu";
 import FormField from "../../../../globals/components/ui/FormField";
 import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
+import { useCreateUser } from "../../hooks/useCreateUser";
+import { useRoles } from "../../hooks/useRoles";
 
 export default function AddUserModal({ onClose }) {
+  const { roles } = useRoles();
+  const { form, loading, error, handleSubmit, handleChange } = useCreateUser({
+    rol_id: "",
+    user_name: "",
+    user_first_surname: "",
+    user_second_surname: "",
+    user_address: "",
+    user_city: "",
+    user_password: "",
+    user_email: "",
+    user_phone: "",
+  });
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <section className="flex flex-col items-center">
+      {/* Formulario para la informacion del nuevo usuario */}
       <form action="" className="flex flex-col gap-1">
+        {/* Menú de roles */}
         <SelectMenu
+          value={form.rol_id}
           id={"user_rol_menu"}
-          name={"user_rol_menu"}
+          name={"rol_id"}
           spanText={"Rol"}
+          onChange={handleChange}
         >
-          <option value="admin"> Administrador </option>
-          <option value="almacen"> Almacén </option>
-          <option value="tecnico"> Técnico </option>
+          {roles.map((rol) => (
+            <option value={rol.id} key={rol.id}>
+              {rol.name}
+            </option>
+          ))}
+          <option value="add-rol"> Agregar rol</option>
         </SelectMenu>
 
         <FormField
-          labelText={"Nombre Completo"}
-          placeholder={"Felipe Contreras Aguilar"}
+          value={form.user_name}
+          labelText={"Nombre"}
+          placeholder={"Felipe"}
           id={"name"}
+          name={"user_name"}
+          onChange={handleChange}
+          autoComplete="given-name"
+        />
+
+        <FormField
+          value={form.user_first_surname}
+          labelText={"Primer Apellido"}
+          placeholder={"Contreras"}
+          id={"first_surname"}
+          name={"user_first_surname"}
+          onChange={handleChange}
+          autoComplete="family-name"
+        />
+
+        <FormField
+          value={form.user_second_surname}
+          labelText={"Segundo Apellido"}
+          placeholder={"Aguilar"}
+          id={"second_surname"}
+          name={"user_second_surname"}
+          onChange={handleChange}
           autoComplete="name"
         />
 
         <FormField
+          value={form.user_city}
+          labelText={"Ciudad"}
+          placeholder={"Bogotá"}
+          id={"city"}
+          name={"user_city"}
+          onChange={handleChange}
+          autoComplete="address-level2"
+        />
+
+        <FormField
+          value={form.user_phone}
           labelText={"Número"}
           placeholder={"300012124"}
           id={"phone"}
+          name={"user_phone"}
+          onChange={handleChange}
           autoComplete="tel"
         />
 
         <FormField
+          value={form.user_email}
           labelText={"Email"}
           placeholder={"pepito@gmail.com"}
           id={"email"}
+          name={"user_email"}
+          onChange={handleChange}
           autoComplete="email"
         />
 
         <FormField
+          value={form.user_address}
           labelText={"Dirección"}
           placeholder={"KR 124 # 12-124"}
           id={"address"}
-          autoComplete="address"
+          name={"user_address"}
+          onChange={handleChange}
+          autoComplete="street-address"
         />
       </form>
 
       {/* Botones */}
       <ConfirmCancelButtons
-        confirmText={"Confirmar"}
+        confirmText={"Crear"}
         cancelText={"Cancelar"}
-        confirmButtonOnClick={onClose}
+        confirmButtonOnClick={(e) => handleSubmit(e)}
         cancelButtonOnClick={onClose}
       />
     </section>
