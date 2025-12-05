2d00d43c279759e91b783a1a7e05731793c4d8b2
DevJuan001 <juanesyt7@gmail.com>
2025-12-05 14:50:34 -0500

    feat: Integrate DisabledFormField in EditUserInfoModal for displaying user ID and enhance form field attributes

--- frontend_web/react_frontend/src/modules/users/components/modals/EditUserInfoModal.jsx
+++ frontend_web/react_frontend/src/modules/users/components/modals/EditUserInfoModal.jsx
@@ -1,7 +1,9 @@
 import FormField from "../../../../globals/components/ui/FormField";
 import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
+import DisabledFormField from "../../../../globals/components/ui/DisabledFormField";
 
 export default function EditUserInfoModal({
+  user_id,
   user_name,
   user_first_surname,
   user_second_surname,
@@ -13,22 +15,37 @@ export default function EditUserInfoModal({
   return (
     <section className="flex flex-col items-center">
       <form action="" className="flex flex-col gap-2">
-        <FormField labelText={"Nombre"} placeholder={user_name} id={"name"} />
+        {/* ID del usuario */}
+        <DisabledFormField hidden={"hidden"} id={"user_id"} value={user_id}/>
+        <FormField
+          labelText={"Nombre"}
+          placeholder={user_name}
+          id={"name"}
+          autoComplete="name"
+        />
         <FormField
           labelText={"Apellidos"}
           placeholder={`${user_first_surname} ${user_second_surname}`}
-          id={"surname"}
+          id={"first_surname"}
+          autoComplete="family-name"
         />
-        <FormField labelText={"Número"} placeholder={user_phone} id={"phone"} />
+        <FormField
+          labelText={"Número"}
+          placeholder={user_phone}
+          id={"phone"}
+          autoComplete="tel"
+        />
         <FormField
           labelText={"Correo Electrónico"}
           placeholder={user_email}
           id={"email"}
+          autoComplete="email"
         />
         <FormField
           labelText={"Dirección"}
           placeholder={user_address}
           id={"address"}
+          autoComplete="street-address"
         />
       </form>
 
