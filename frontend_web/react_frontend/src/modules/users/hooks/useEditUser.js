1ffc2e33df7f08d763efb59d74bca99dca655bd9
DevJuan001 <juanesyt7@gmail.com>
2025-12-09 20:54:48 -0500

    feat: add new function named handleSubmit to send id and formdata to edit user service in useEditUser hook

--- frontend_web/react_frontend/src/modules/users/hooks/useEditUser.js
+++ frontend_web/react_frontend/src/modules/users/hooks/useEditUser.js
@@ -1,8 +1,31 @@
-export function useEditUser() {
+import { useState } from "react";
+import { editUserService } from "../services/editUserService";
+
+export function useEditUser(userId, formData) {
+  const [id, setId] = useState(userId)
+  const [form, setForm] = useState(formData);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   function handleChange(e) {
-    setForm((prev) => ({
+    setForm(prev => ({
       ...prev,
-      [e.target.name]: e.target.value,
+      [e.target.name]: e.target.value
     }));
   }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true)
+
+    try {
+      const response = await editUserService(id, form)
+    } catch (error) {
+      setError(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return {handleChange, handleSubmit, loading, error, form}
 }
