44474f58db188180b57298492c82ef032fa763ea
DevJuan001 <juanesyt7@gmail.com>
2025-12-09 20:55:09 -0500

    feat: add new prop in error modal to text inside of this

--- frontend_web/react_frontend/src/globals/components/modals/ErrorModal.jsx
+++ frontend_web/react_frontend/src/globals/components/modals/ErrorModal.jsx
@@ -1,12 +1,12 @@
 import { modalIcons } from "../../../assets/icons/modalIcons";
 import Modal from "./Modal";
 
-export default function ErrorModal() {
+export default function ErrorModal({errorText}) {
     return(
         <Modal z_index="100">
             <img src={modalIcons.errorIcon} />
             <section className="flex">
-                <span>No se pudo elminar este usuario</span>
+                <span>{errorText}</span>
             </section>
         </Modal>
     );
