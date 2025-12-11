66168426521bd64ac4541158e44803ef32742a97
DevJuan001 <juanesyt7@gmail.com>
2025-12-10 21:02:38 -0500

    feat: implement inner modals on deleteUserModal

--- frontend_web/react_frontend/src/modules/users/components/modals/DeleteUserModal.jsx
+++ frontend_web/react_frontend/src/modules/users/components/modals/DeleteUserModal.jsx
@@ -1,16 +1,16 @@
+// Componentes
+import Loader from "../../../../globals/components/ui/Loader";
 import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
+// Modales
+import SuccessModal from "../../../../globals/components/modals/SuccessModal";
+import ErrorModal from "../../../../globals/components/modals/ErrorModal";
+// Hooks
 import { useDeleteUser } from "../../hooks/useDeleteUser";
+import { useState } from "react";
 
 export default function DeleteUserModal({ user, onClose }) {
-  const { handleSubmit, loading, error } = useDeleteUser(user.user_id);
-
-  if (loading) {
-    return <div>Cargando...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  const [innerModal, setInnerModal] = useState(null);
+  const { handleSubmit, loading} = useDeleteUser(user.user_id);
   return (
     <section className="flex flex-col justify-center items-center dark:text-white">
       <p>
@@ -23,13 +23,32 @@ export default function DeleteUserModal({ user, onClose }) {
 
       {/* Botones */}
       <ConfirmCancelButtons
-        confirmText={"Eliminar"}
+        confirmText={loading ? <Loader /> : "Eliminar"}
         confirmBgColor="red-600"
         confirmDarkBgColor=""
         cancelText={"Cancelar"}
-        confirmButtonOnClick={handleSubmit}
+        confirmButtonOnClick={(e) => handleSubmit(e, setInnerModal)}
         cancelButtonOnClick={onClose}
       />
+      {innerModal === "success" && (
+        <SuccessModal
+        isOpen={true}
+        confirmTitle={"¡Usuario eliminado correctamente!"}
+        confirmText={"Todos los datos de este usuarios han sido eliminados exitosamente."}
+        onClose={() => {
+            setInnerModal(null);
+            onClose();
+        }}
+        />
+      )}
+      {innerModal === "error" && (
+        <ErrorModal
+        isOpen={true}
+        errorTitle={"¡No se pudo borrar el usuario!"}
+        errorText={"Intenta nuevamente eliminar el usuario y si el problema persiste comunicate ccon soporte"}
+        onClose={() => setInnerModal(null)}
+        />
+      )}
     </section>
   );
 }
