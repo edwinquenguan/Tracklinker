064d2efa98c446d732e3b25dcf7f3c8a1365053a
DevJuan001 <juanesyt7@gmail.com>
2025-12-11 15:07:30 -0500

    feat: add user details to authentication response including name, surnames, and email

--- backend/app/controllers/auth_controller.py
+++ backend/app/controllers/auth_controller.py
@@ -44,6 +44,10 @@ class AuthController:
             expires_delta=expires)
 
         return{
+            "name": user["user_name"],
+            "first_surname": user["user_first_surname"],
+            "second_surname": user["user_second_surname"],
+            "email": email,
             "token_type": "Bearer",
             "access_token": token
         }
