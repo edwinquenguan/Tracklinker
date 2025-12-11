460da9d9a810166dfe201f8da8cf3eff363663d5
edwinquenguan <edwinquenguan14@gmail.com>
2025-12-10 21:05:39 -0500

    add subcategory modal

--- frontend_web/react_frontend/src/modules/subcategories/components/modals/AddSubcategoriesModal.jsx
+++ frontend_web/react_frontend/src/modules/subcategories/components/modals/AddSubcategoriesModal.jsx
@@ -1,26 +1,56 @@
-import SelectMenu from "../../../../globals/components/modals/SelectMenu";
+// Componentes
+import Loader from "../../../../globals/components/ui/Loader";
 import FormField from "../../../../globals/components/ui/FormField";
+import SelectMenu from "../../../../globals/components/modals/SelectMenu";
 import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
 
-export default function AddSubcategoriesModal({ onClose }) {
+// Hooks
+import { useState } from "react";
+import { useSubcategories } from "../../hooks/ useSubcategories ";
+import {  useCreateSubcategories  } from "../../hooks/ useCreateSubcategories ";
+// Modales
+import ErrorModal from "../../../../globals/components/modals/ErrorModal";
+import SuccessModal from "../../../../globals/components/modals/SuccessModal";
+
+export default function AddSubcategoriesModal({ onClose, fetch }) {
+  // Estado para las modales se abren encima de esta
+  const [innerModal, setInnerModal] = useState(null);
+  const { Subcategories } = useSubcategories();
+  const { form, loading, handleSubmit, handleChange } = useCreateSubcategories({
+    subcategory_id: "",
+    subcategory_name: "",
+    
+   
+  });
+
   return (
     <section className="flex flex-col items-center">
+      {/* Formulario para la informacion de la nueva subcategoria */}
       <form action="" className="flex flex-col gap-1">
-            <SelectMenu
-                id={"subcategoria_status_menu"}
-                name={"subcategory_status_menu"}
-                spanText={"Estado"}
-            >
-                <option value="active"> Activa </option>
-                <option value="inactive"> Inactiva </option>
-                
-            </SelectMenu>
+        {/* Menú de subcategoria */}
+        <SelectMenu
+          value={form.subcategory_id}
+          id={"subcategory_id_menu"}
+          name={"subcategory_id"}
+          spanText={"categoria"}
+          onChange={handleChange}
+        >
+          <option> Seleccionar </option>
+          {roles.map((subcategory) => (
+            <option value={subcategory.id} key={subcategory.id}>
+              {subcategory.name}
+            </option>
+          ))}
+          <option value="add-subcategory"> Agregar subcategoria</option>
+        </SelectMenu>
 
-            <FormField
+        
+        <FormField
                 labelText={"Nombre de la Subcategoria"}
                 placeholder={"portatiles,all in one,etc"}
                 id={"subcategory_name"}
                 autoComplete="off"
+                onChange={handleChange}
             />
 
             <FormField
@@ -28,16 +58,56 @@ export default function AddSubcategoriesModal({ onClose }) {
                 placeholder={"descripcion de la subcategoria"}
                 id={"subcategory_description"}
                 autoComplete="off"
+                onChange={handleChange}
             />
       </form>
 
+
+
+
+        
+
+        
+
+
+
+  
+
+       
+
       {/* Botones */}
       <ConfirmCancelButtons
-        confirmText={"Confirmar"}
+        confirmText={loading ? <Loader /> : "Crear"}
         cancelText={"Cancelar"}
-        confirmButtonOnClick={onClose}
+        confirmButtonOnClick={(e) => handleSubmit(e, setInnerModal)}
         cancelButtonOnClick={onClose}
       />
+
+      {/* Modales Internas */}
+      {innerModal === "success" && (
+        <SuccessModal
+          isOpen={true}
+          confirmTitle={"Usuario creado con éxito!"}
+          confirmText={
+            "Se ha creado correctamente el usuario, toca el botón de volver a la pagina para verlo, ¡Bienvenido!"
+          }
+          confirmButtonText={"Volver a la pagina"}
+          onClose={() => {
+            setInnerModal(null);
+            onClose();
+            fetch();
+          }}
+        />
+      )}
+      {innerModal === "error" && (
+        <ErrorModal
+          isOpen={true}
+          errorTitle="No se puedo completar el registro!"
+          errorText="Verfica que todos los campos esten completos y que el correo electronico no este registrado"
+          confirmButtonText="Volver a intentarlo"
+          onClose={() => setInnerModal(null)}
+        />
+      )}
     </section>
   );
 }
--- frontend_web/react_frontend/src/modules/subcategories/components/modals/DeleteSubcategoriesModal.jsx
+++ frontend_web/react_frontend/src/modules/subcategories/components/modals/DeleteSubcategoriesModal.jsx
@@ -1,16 +1,22 @@
 import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
+import { useDeleteSubcategories } from "../../hooks/useDeleteSubcategories";
 
-export default function DeleteSubcategoriesModal({
-  subcategories_name,
-  subcategories_first_surname,
-  onClose,
-}) {
+export default function DeleteSubcategoriesModal({ user, onClose }) {
+  const { handleSubmit, loading, error } = useDeleteSubcategories(subcategories.subcategories_id);
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
   return (
     <section className="flex flex-col justify-center items-center dark:text-white">
       <p>
         ¿Seguro que deseas eliminar a{" "}
         <span className="font-medium">
-          {subcategories_name} {subcategories_first_surname}
+          {subcategories.subcategories_name} {subcategories.subcategories_first_surname}
         </span>
         ?
       </p>
@@ -21,7 +27,7 @@ export default function DeleteSubcategoriesModal({
         confirmBgColor="red-600"
         confirmDarkBgColor=""
         cancelText={"Cancelar"}
-        confirmButtonOnClick={onClose}
+        confirmButtonOnClick={handleSubmit}
         cancelButtonOnClick={onClose}
       />
     </section>
