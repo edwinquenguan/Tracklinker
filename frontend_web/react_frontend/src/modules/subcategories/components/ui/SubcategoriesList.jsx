a610f4afd90816837aaeb10d874cb15e50da7505
DevJuan001 <juanesyt7@gmail.com>
2025-12-12 14:25:34 -0500

    refactor: update SubcategoriesList component to use subcategory object directly and improve code readability

--- frontend_web/react_frontend/src/modules/subcategories/components/ui/SubcategoriesList.jsx
+++ frontend_web/react_frontend/src/modules/subcategories/components/ui/SubcategoriesList.jsx
@@ -1,29 +1,26 @@
-import { useSubcategories } from "../../hooks/useUsers";
+import { useSubcategories } from "../../hooks/useSubcategories";
 import SubcategoriesItem from "./SubcategoriesItem";
 
-export default function SubcategoriesList({ openModal }) {
+export default function SubcategoriesList({openModal}) {
   const { subcategories, loading, error } = useSubcategories();
-    if (error) {
+  if (error) {
     return <div>Error: {error}</div>;
-    }
-    if (loading) {
+  }
+  if (loading) {
     return <div>Cargando...</div>;
-    }
-    return (
+  }
+  return (
     /* Contenedor de las subcategorías */
     <section className="max-h-[95%] max-w-full overflow-x-auto overflow-y-auto overflow-hidden">
       <ul className="pt-3 flex flex-col gap-1">
         {subcategories.map((subcategory) => (
-            // Subcategorías
-            <SubcategoriesItem
-            subcategory_id={subcategory.subcategory_id}
-
-            subcategory_name={subcategory.subcategory_name}
-            subcategory_description={subcategory.subcategory_description}
+          // Subcategorías
+          <SubcategoriesItem
+            subcategory={subcategory}
             moreInfoOnClick={() => openModal(subcategory, "info")}
             editButtonOnClick={() => openModal(subcategory, "edit")}
             deleteButtonOnClick={() => openModal(subcategory, "delete")}
-            />
+          />
         ))}
       </ul>
     </section>
