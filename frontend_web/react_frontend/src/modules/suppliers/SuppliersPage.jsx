56a3099385acb5315bd6eda53d84e8b24f7a6fbd
DevJuan001 <juanesyt7@gmail.com>
2025-12-13 18:42:59 -0500

    feat: refactor SuppliersPage to utilize custom hooks and modularize modal components for improved maintainability

--- frontend_web/react_frontend/src/modules/suppliers/SuppliersPage.jsx
+++ frontend_web/react_frontend/src/modules/suppliers/SuppliersPage.jsx
@@ -1,58 +1,25 @@
-import { useState, useEffect } from "react";
-import { getAllSuppliers } from "../../services/getAllSuppliers";
-import { actionsIcons } from "../../assets/icons/mainIcons";
+// Hooks
+import { useModal } from "../../globals/hooks/useModal";
+import { useSuppliers } from "./hooks/useSuppliers";
+// Iconos
 import { usersIcons } from "../../assets/icons/mainIcons";
-//import { suppliers } from "../data/suppliers";
-import Modal from "../../globals/components/modals/Modal";
-import FilterModal from "../../globals/components/modals/FilterModal";
+import { actionsIcons } from "../../assets/icons/mainIcons";
+// Componentes
 import Layout from "../../globals/components/Layout/Layout";
 import FormField from "../../globals/components/ui/FormField";
 import TopSection from "../../globals/components/ui/TopSection";
+// Modales
+import Modal from "../../globals/components/modals/Modal";
+import FilterModal from "../../globals/components/modals/FilterModal";
+import SuppliersList from "./components/ui/SuppliersList";
+import AddSupplierModal from "./components/modals/AddSupplierModal";
+import MoreInfoSupplierModal from "./components/modals/MoreInfoSupplierModal";
+import EditSupplierInfoModal from "./components/modals/EditSupplierInfoModal";
+import DeleteSupplierModal from "./components/modals/DeleteSupplierModal";
 
 export default function SuppliersPage() {
-  // Definir los estados y sus valores por defecto
-  const [suppliers, setSuppliers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [modalType, setModalType] = useState(null);
-  const [selectedSupplier, setSelectedSupplier] = useState(null);
-  const [isOpen, setIsOpen] = useState(false);
-
-  // Esto llama a la función getAllSuppliers y espera a obtener data y los almacena en "data"
-  useEffect(() => {
-    async function fetchSuppliers() {
-      try {
-        setLoading(true);
-        const data = await getAllSuppliers();
-        setSuppliers(data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchSuppliers();
-  }, []);
-
-  if (loading) {
-    //const supplierField = document.getElementById("user-field");
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  // Al momento de clickear un botón esto guarda la información del Proveedor y abre la modal que pertenece a ese botón
-  const openModal = (supplier, type) => {
-    setSelectedSupplier(supplier);
-    setModalType(type);
-  };
-  // Y esto cierra la modal y quita los datos del Proveedor seleccionado
-  const closeModal = () => {
-    setSelectedSupplier(null);
-    setModalType(null);
-  };
+  const { modalType, isOpen, modalData, openModal, closeModal } = useModal();
+  const { suppliers, loading, error, fetchSuppliers } = useSuppliers();
 
   return (
     <Layout>
@@ -60,74 +27,17 @@ export default function SuppliersPage() {
         sectionName={"Proveedores"}
         addButtonIcon={actionsIcons.addIcon}
         addButtonText={"Agregar Proveedor"}
-        createOnClick={() => {
-          openModal(null, "add");
-          setIsOpen(true);
-        }}
-        filterOnClick={() => {
-          openModal(null, "filter");
-          setIsOpen(true);
-        }}
+        createOnClick={() => openModal(null, "add", fetchSuppliers)}
+        filterOnClick={() => openModal(null, "filter", fetchSuppliers)}
       />
       {/* Listado de proveedores */}
-      <ul className="max-h-[95%] max-w-full pt-3 flex flex-col gap-1 overflow-x-auto overflow-y-auto">
-        {suppliers.map((supplier) => (
-          <li
-            className="flex items-center justify-between p-4 bg-[#f3eef5] rounded-xl shadow-md"
-            hey={supplier.supplier_id}
-            id="user-field"
-          >
-            <article className="flex">
-              <address className="flex gap-5 not-italic font-medium">
-                <p className="text-[22px]">{supplier.supplier_name}</p>
-                <div className="flex items-center">
-                  <img src={usersIcons.phoneIcon} alt="" className="w-5 h-5" />
-                  <p>{supplier.supplier_phone}</p>
-                </div>
-                <div className="flex items-center">
-                  <img src={usersIcons.rolIcon} alt="" className="w-5 h-5" />
-                  <p>{supplier.supplier_address}</p>
-                </div>
-                <div className="flex items-center">
-                  <img
-                    src={usersIcons.cityIcon}
-                    alt=""
-                    className="invert brightness-200"
-                  />
-                  <p>{supplier.supplier_city}</p>
-                </div>
-              </address>
-            </article>
-            {/* Botones para interactuar */}
-            <nav className="flex gap-4">
-              <button
-                onClick={() => {
-                  openModal(supplier, "info");
-                  setIsOpen(true);
-                }}
-              >
-                <img src={actionsIcons.moreInfoIcon} alt="" />
-              </button>
-              <button
-                onClick={() => {
-                  openModal(supplier, "edit");
-                  setIsOpen(true);
-                }}
-              >
-                <img src={actionsIcons.editInfoIcon} alt="" />
-              </button>
-              <button
-                onClick={() => {
-                  openModal(supplier, "delete");
-                  setIsOpen(true);
-                }}
-              >
-                <img src={actionsIcons.deleteIcon} alt="" />
-              </button>
-            </nav>
-          </li>
-        ))}
-      </ul>
+      <SuppliersList
+        suppliers={suppliers}
+        loading={loading}
+        error={error}
+        refetch={fetchSuppliers}
+        openModal={openModal}
+      />
 
       {/* Modales */}
       {modalType && (
@@ -145,180 +55,33 @@ export default function SuppliersPage() {
           }
           type={modalType}
           isOpen={isOpen}
-          onClose={() => {
-            closeModal();
-            setIsOpen(false);
-          }}
+          onClose={() => closeModal()}
         >
           {modalType === "filter" && (
-            <FilterModal
-              onClose={() => {
-                closeModal();
-                setIsOpen(false);
-              }}
-            ></FilterModal>
+            <FilterModal onClose={() => closeModal()}></FilterModal>
           )}
           {modalType === "add" && (
-            <div className="flex flex-col items-center">
-              <form action="" className="flex flex-col gap-1">
-                <FormField
-                  labelText={"Nombre"}
-                  placeholder={"Lenovo"}
-                  id={"name"}
-                />
-
-                <FormField
-                  labelText={"Ciudad"}
-                  placeholder={"Miami"}
-                  id={"city"}
-                />
-
-                <FormField
-                  labelText={"Número"}
-                  placeholder={"300012124"}
-                  id={"phone"}
-                />
-
-                <FormField
-                  labelText={"Dirección"}
-                  placeholder={"KR 124 # 12-124"}
-                  id={"address"}
-                />
-              </form>
-
-              {/* Botones */}
-              <div className="flex gap-2 pt-5">
-                <button
-                  className="bg-black text-white px-5 py-2 rounded-xl shadow-xl text-sm transition duration-300 hover:text-gray-400"
-                  onClick={() => {
-                    closeModal();
-                    setIsOpen(false);
-                  }}
-                >
-                  Confirmar
-                </button>
-                <button
-                  className="px-5 py-2 border rounded-xl shadow-xl text-sm transition duration-300 hover:bg-gray-200"
-                  onClick={() => {
-                    closeModal();
-                    setIsOpen(false);
-                  }}
-                >
-                  Cancelar
-                </button>
-              </div>
-            </div>
+            <AddSupplierModal onClose={() => closeModal()} />
           )}
           {/* Modal para mas información del Proveedor */}
           {modalType === "info" && (
-            <div className="flex flex-col justify-center">
-              <p>
-                <strong>Nombre:</strong> {selectedSupplier.supplier_name}
-              </p>
-              <p>
-                <strong>Ciudad:</strong> {selectedSupplier.supplier_city}
-              </p>
-              <p>
-                <strong>Teléfono:</strong> {selectedSupplier.supplier_phone}
-              </p>
-              <p>
-                <strong>Dirección:</strong> {selectedSupplier.supplier_address}
-              </p>
-              <p>
-                <strong>Fecha De Creación:</strong>{" "}
-                {selectedSupplier.supplier_date}
-              </p>
-            </div>
+            <MoreInfoSupplierModal supplier={modalData} />
           )}
 
           {/* Modal para editar el Proveedor */}
           {modalType === "edit" && (
-            <div className="flex flex-col items-center">
-              <form action="" className="flex flex-col gap-2">
-                <FormField
-                  labelText={"Nombre"}
-                  placeholder={selectedSupplier.supplier_name}
-                  id={"name"}
-                />
-                <FormField
-                  labelText={"Ciudad"}
-                  placeholder={selectedSupplier.supplier_city}
-                  id={"city"}
-                />
-                <FormField
-                  labelText={"Número"}
-                  placeholder={selectedSupplier.supplier_phone}
-                  id={"phone"}
-                />
-                <FormField
-                  labelText={"Dirección"}
-                  placeholder={selectedSupplier.supplier_address}
-                  id={"address"}
-                />
-              </form>
-
-              {/* Botones */}
-              <div className="flex gap-2 pt-5">
-                <button
-                  className="bg-black text-white px-5 py-2 rounded-xl shadow-xl text-sm transition duration-300 hover:text-gray-400"
-                  onClick={() => {
-                    closeModal();
-                    setIsOpen(false);
-                  }}
-                >
-                  Confirmar
-                </button>
-                <button
-                  className="px-5 py-2 border rounded-xl shadow-xl text-sm transition duration-300 hover:bg-gray-200"
-                  onClick={() => {
-                    closeModal();
-                    setIsOpen(false);
-                  }}
-                >
-                  Cancelar
-                </button>
-              </div>
-            </div>
+            <EditSupplierInfoModal
+              supplier={modalData}
+              onClose={() => closeModal()}
+            />
           )}
 
           {/* Modal para eliminar el Proveedor */}
           {modalType === "delete" && (
-            <div className="flex flex-col justify-center items-center">
-              <p>
-                ¿Seguro que deseas eliminar a{" "}
-                <span className="font-medium">
-                  {selectedSupplier.supplier_name}
-                </span>
-                ?
-              </p>
-
-              {/* Botones */}
-              <div className="flex pt-4 gap-5">
-                <button
-                  className="flex items-center gap-2 px-5 py-2 rounded-xl shadow-xl text-sm bg-red-600 text-white transition duration-300 hover:bg-red-700"
-                  onClick={() => {
-                    closeModal();
-                    setIsOpen(false);
-                  }}
-                >
-                  <img
-                    src={actionsIcons.deleteIcon}
-                    alt=""
-                    className="w-[20px] h-[20px] invert"
-                  />
-                  Eliminar
-                </button>
-                <button
-                  className="px-5 py-2 border rounded-xl shadow-xl text-sm transition duration-300 hover:bg-gray-200"
-                  onClick={() => {
-                    closeModal();
-                    setIsOpen(false);
-                  }}
-                >
-                  Cancelar
-                </button>
-              </div>
-            </div>
+            <DeleteSupplierModal
+              supplier={modalData}
+              onClose={() => closeModal()}
+            />
           )}
         </Modal>
       )}
