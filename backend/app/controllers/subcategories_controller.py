376d9541f21ec4eaeab230c40f0c2c9619049a39
DevJuan001 <juanesyt7@gmail.com>
2025-12-13 15:13:52 -0500

    refactor: improve code readability in SubcategoriesController by formatting function calls and removing unnecessary whitespace

--- backend/app/controllers/subcategories_controller.py
+++ backend/app/controllers/subcategories_controller.py
@@ -3,22 +3,23 @@ from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from app.core.security import create_access_token
 
+
 class SubcategoriesController:
 
     @staticmethod
     def get_all_subcategories():
         error, subcategories = SubcategoriesRepository.find_all_subcategories()
-        
+
         if error:
             raise HTTPException(status_code=404, detail=error)
         return {
             "data": subcategories
-        }     
+        }
 
     @staticmethod
     def get_subcategory_by_id(subcategory_id: int):
         error, subcategory = SubcategoriesRepository.find_by_id(subcategory_id)
-        
+
         if error:
             raise HTTPException(status_code=404, detail=error)
         return {
@@ -27,7 +28,8 @@ class SubcategoriesController:
 
     @staticmethod
     def create_subcategory(subcategory_data: dict):
-        error, message = SubcategoriesRepository.create_subcategory(subcategory_data)
+        error, message = SubcategoriesRepository.create_subcategory(
+            subcategory_data)
         if error:
             raise HTTPException(status_code=400, detail=error)
         return {
@@ -37,7 +39,8 @@ class SubcategoriesController:
 
     @staticmethod
     def update_subcategory(subcategory_id: int, subcategory_data: dict):
-        error, message, subcategory = SubcategoriesRepository.update_subcategory(subcategory_id, subcategory_data)
+        error, message, subcategory = SubcategoriesRepository.update_subcategory(
+            subcategory_id, subcategory_data)
         if error:
             raise HTTPException(status_code=400, detail=error)
         return {
@@ -48,12 +51,11 @@ class SubcategoriesController:
 
     @staticmethod
     def delete_subcategory(subcategory_id: int):
-        error, message = SubcategoriesRepository.delete_subcategory(subcategory_id)
+        error, message = SubcategoriesRepository.delete_subcategory(
+            subcategory_id)
         if error:
             raise HTTPException(status_code=400, detail=error)
         return {
             "success": True,
             "message": message
         }
-
-          
