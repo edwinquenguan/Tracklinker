d499b8b94684bf7bb82027b69618b7ea20acae10
DevJuan001 <juanesyt7@gmail.com>
2025-12-13 13:51:09 -0500

    refactor: streamline CategoriesPage component by integrating useModal hook and optimizing modal handling

--- frontend_web/react_frontend/src/modules/categories/CategoriesPage.jsx
+++ frontend_web/react_frontend/src/modules/categories/CategoriesPage.jsx
@@ -1,114 +1,35 @@
-import { useState, useEffect } from "react";
-import { getAllCategories } from "../../services/getAllCategories";
-// import { categories } from "../data/categories";
+// Hooks
+import { useModal } from "../../globals/hooks/useModal";
+// Iconos
 import { actionsIcons } from "../../assets/icons/mainIcons";
-import Modal from "../../globals/components/modals/Modal";
-import ProfileModal from "../../globals/components/modals/ProfileModal";
-import ActionButtons from "../../globals/components/ui/ActionButtons";
-import ConfirmCancelButtons from "../../globals/components/modals/ConfirmCancelButtons";
-import FilterModal from "../../globals/components/modals/FilterModal";
+// Componentes
 import Layout from "../../globals/components/Layout/Layout";
-import FormField from "../../globals/components/ui/FormField";
 import TopSection from "../../globals/components/ui/TopSection";
+import CategoriesList from "./components/ui/CategoriesList";
+// Modales
+import Modal from "../../globals/components/modals/Modal";
+import MoreInfoModal from "./components/modals/MoreInfoModal";
+import AddCategoryModal from "./components/modals/AddCategoryModal";
+import FilterModal from "../../globals/components/modals/FilterModal";
+import ProfileModal from "../../globals/components/modals/ProfileModal";
+import DeleteCategoryModal from "./components/modals/DeleteCategoryModal";
+import EditCategoryInfoModal from "./components/modals/EditCategoryInfoModal";
 
 export default function CategoriesPage() {
-  // Definir los estados y sus valores por defecto
-  const [categories, setCategories] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [modalType, setModalType] = useState(null);
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  const [isOpen, setIsOpen] = useState(false);
-
-  useEffect(() => {
-    async function fetchCategories() {
-      try {
-        setLoading(true);
-        const data = await getAllCategories();
-        setCategories(data);
-      } catch (error) {
-        setError(error.message);
-      }
-    }
-
-    fetchCategories();
-  }, []);
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  // Al momento de clickear un botón esto guarda la información del usuario y abre la modal que pertenece a ese botón
-  const openModal = (category, type) => {
-    setSelectedCategory(category);
-    setModalType(type);
-  };
-  // Y esto cierra la modal y quita los datos del usuario seleccionado
-  const closeModal = () => {
-    setSelectedCategory(null);
-    setModalType(null);
-  };
+  const { modalType, isOpen, modalData, refetch, openModal, closeModal } =
+    useModal();
 
   return (
-    <Layout
-      avatarOnClick={() => {
-        openModal(null, "user");
-        setIsOpen(true);
-      }}
-    >
+    <Layout avatarOnClick={() => openModal(null, "user")}>
       <TopSection
         sectionName={"Categorias"}
         addButtonIcon={actionsIcons.addIcon}
         addButtonText={"Agregar Categoria"}
-        createOnClick={() => {
-          openModal(null, "add");
-          setIsOpen(true);
-        }}
-        filterOnClick={() => {
-          openModal(null, "filter");
-          setIsOpen(true);
-        }}
+        createOnClick={() => openModal(null, "add")}
+        filterOnClick={() => openModal(null, "filter")}
       />
       {/* Listado de categorias */}
-      <section className="max-h-[95%] max-w-full overflow-x-auto overflow-y-auto overflow-hidden">
-        <ul className="pt-3 flex flex-col gap-1 overflow-x-auto overflow-y-auto">
-          {categories.map((category) => (
-            // Categorias
-            <li
-              className="flex items-center justify-between p-4 bg-[#f3eef5] rounded-lg shadow-md transition duration-300
-                        dark:bg-[#0f0f11] dark:hover:bg-[#212125] dark:text-white"
-              key={category.category_id}
-            >
-              <span className="text-2xl font-medium">
-                {category.category_name}
-              </span>
-              {/* Botones para interactuar */}
-              <nav className="flex gap-4">
-                <ActionButtons
-                  editButtonOnClick={() => {
-                    openModal(category, "edit");
-                    setIsOpen(true);
-                  }}
-                  deleteButtonOnClick={() => {
-                    openModal(category, "delete");
-                    setIsOpen(true);
-                  }}
-                >
-                  {/* Botón de más información del usuario */}
-                  <button
-                    onClick={() => {
-                      openModal(category, "info");
-                      setIsOpen(true);
-                    }}
-                  >
-                    <img src={actionsIcons.moreInfoIcon} alt="" />
-                  </button>
-                </ActionButtons>
-              </nav>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <CategoriesList openModal={openModal} />
 
       {/* Modales */}
       {modalType && (
@@ -128,103 +49,29 @@ export default function CategoriesPage() {
           }
           type={modalType}
           isOpen={isOpen}
-          onClose={() => {
-            closeModal();
-            setIsOpen(false);
-          }}
+          onClose={() => closeModal()}
         >
           {modalType === "user" && (
-            <ProfileModal
-              onClose={() => {
-                closeModal();
-                setIsOpen(false);
-              }}
-            />
+            <ProfileModal onClose={() => closeModal()} />
           )}
           {modalType === "filter" && (
-            <FilterModal
-              onClose={() => {
-                closeModal();
-                setIsOpen(false);
-              }}
-            ></FilterModal>
+            <FilterModal onClose={() => closeModal()}></FilterModal>
           )}
           {modalType === "add" && (
-            <section className="flex flex-col items-center">
-              <form action="" className="flex flex-col gap-1">
-                <FormField
-                  labelText={"Nombre"}
-                  placeholder={"Routers"}
-                  id={"name"}
-                />
-              </form>
-
-              {/* Botones */}
-              <ConfirmCancelButtons
-                confirmText={"Confirmar"}
-                confirmBgColor="black"
-                cancelText={"Cancelar"}
-                confirmButtonOnClick={() => {
-                  closeModal();
-                  setIsOpen(false);
-                }}
-              />
-            </section>
+            <AddCategoryModal onClose={() => closeModal()} />
           )}
           {/* Modal para mas información de la categoria */}
-          {modalType === "info" && (
-            <section className="flex flex-col justify-center dark:text-white">
-              <p>
-                <strong>Nombre:</strong> {selectedCategory.category_name}{" "}
-              </p>
-              <p>
-                <strong>Creada:</strong> {selectedCategory.category_date}
-              </p>
-            </section>
-          )}
+          {modalType === "info" && <MoreInfoModal category={modalData} />}
           {/* Modal para editar la categoria */}
           {modalType === "edit" && (
-            <section className="flex flex-col items-center">
-              <form action="" className="flex flex-col gap-2">
-                <FormField
-                  labelText={"Nombre"}
-                  placeholder={selectedCategory.category_name}
-                  id={"name"}
-                />
-              </form>
-
-              {/* Botones */}
-              <ConfirmCancelButtons
-                confirmText={"Confirmar"}
-                confirmBgColor="black"
-                cancelText={"Cancelar"}
-                confirmButtonOnClick={() => {
-                  closeModal();
-                  setIsOpen(false);
-                }}
-              />
-            </section>
+            <EditCategoryInfoModal
+              category={modalData}
+              onClose={() => closeModal()}
+            />
           )}
-
           {/* Modal para eliminar la categoria */}
           {modalType === "delete" && (
-            <section className="flex flex-col justify-center items-center dark:text-white">
-              <p>
-                ¿Seguro que deseas eliminar la Categoria{" "}
-                <strong>{selectedCategory.category_name}</strong>?
-              </p>
-
-              {/* Botones */}
-              <ConfirmCancelButtons
-                confirmText={"Eliminar"}
-                confirmBgColor="red-600"
-                cancelText={"Cancelar"}
-                confirmButtonOnClick={() => {
-                  closeModal();
-                  setIsOpen(false);
-                }}
-              />
-            </section>
+            <DeleteCategoryModal category={modalData} onClose={() => closeModal()}/>
           )}
         </Modal>
       )}
