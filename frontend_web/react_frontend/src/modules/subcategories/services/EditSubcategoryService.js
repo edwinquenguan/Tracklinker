e6e4132619602e203928125bf3a8bbd881f3fc56
DevJuan001 <juanesyt7@gmail.com>
2025-12-13 12:25:15 -0500

    refactor: update editSubcategoryService to use PUT method and include subcategory_id in the API endpoint

--- frontend_web/react_frontend/src/modules/subcategories/services/EditSubcategoryService.js
+++ frontend_web/react_frontend/src/modules/subcategories/services/EditSubcategoryService.js
@@ -1,16 +1,18 @@
 import { apiRoutes } from "../../../config/apiRoutes";
 import { getToken } from "../../../utils/auth";
 
-export async function editSubcategoryService(subcategory_data,subcategory_data) {
-  console.log(JSON.stringify(subcategory_data))
-  const res = await fetch(`${apiRoutes.apiUrl}${apiRoutes.users}/update`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: getToken(),
-    },
-    body: JSON.stringify(subcategory_data),
-  });
+export async function editSubcategoryService(subcategory_id, subcategory_data) {
+  const res = await fetch(
+    `${apiRoutes.apiUrl}${apiRoutes.users}/update/${subcategory_id}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: getToken(),
+      },
+      body: JSON.stringify(subcategory_data),
+    }
+  );
 
   // Validamos si la respuesta no fue OK
   if (!res.ok) {
