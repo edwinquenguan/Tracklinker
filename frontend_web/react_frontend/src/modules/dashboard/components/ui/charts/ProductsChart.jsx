f5d56f1de0a7774a0025e84561ebffde5af432ab
DevJuan001 <juanesyt7@gmail.com>
2025-12-14 11:51:45 -0500

    feat: refactor chart components to include titles directly in TopChartsCard for improved readability

--- frontend_web/react_frontend/src/modules/dashboard/components/ui/charts/ProductsChart.jsx
+++ frontend_web/react_frontend/src/modules/dashboard/components/ui/charts/ProductsChart.jsx
@@ -14,23 +14,17 @@ export default function ProductsChart() {
   }
 
   return (
-    <TopChartsCard background={"products-background"}>
-      <section>
-        <section className="flex items-center justify-between">
-          <span className="font-medium text-xl">Productos Totales</span>
-          <SeeReportButton />
-        </section>
-        {productChartInfo.map((item) => (
-          <div className="flex items-center gap-1" key={item.products}>
-            {/* Productos Totales */}
-            <span className="font-semibold text-2xl">{item.products}</span>
-            {/* Products que entraton en el mes actual */}
-            <span className="font-medium text-base text-[#00a86b]">
-              +{item.new_products}
-            </span>
-          </div>
-        ))}
-      </section>
+    <TopChartsCard background={"products-background"} title={"Productos"}>
+      {productChartInfo.map((item) => (
+        <div className="flex items-center gap-1" key={item.products}>
+          {/* Productos Totales */}
+          <span className="font-semibold text-2xl">{item.products}</span>
+          {/* Products que entraton en el mes actual */}
+          <span className="font-medium text-base text-[#00a86b]">
+            +{item.new_products}
+          </span>
+        </div>
+      ))}
     </TopChartsCard>
   );
 }
--- frontend_web/react_frontend/src/modules/dashboard/components/ui/charts/UsersChart.jsx
+++ frontend_web/react_frontend/src/modules/dashboard/components/ui/charts/UsersChart.jsx
@@ -14,24 +14,17 @@ export default function UsersChart() {
   }
 
   return (
-    <TopChartsCard background={"users-background"}>
-      <section>
-        <section className="flex items-center justify-between">
-          {/* Nombre del Grafico */}
-          <span className="font-medium text-xl">Usuarios</span>
-          <SeeReportButton />
-        </section>
-        {usersChartInfo.map((item) => (
-          <div className="flex items-center gap-1" key={item.users}>
-            {/* Usuarios existentes */}
-            <span className="font-semibold text-2xl">{item.users}</span>
-            {/* Usuarios creados en el mes actual */}
-            <span className="font-medium text-base text-[#00a86b]">
-              +{item.new_users}
-            </span>
-          </div>
-        ))}
-      </section>
+    <TopChartsCard background={"users-background"} title={"Usuarios"}>
+      {usersChartInfo.map((item) => (
+        <div className="flex items-center gap-1" key={item.users}>
+          {/* Usuarios existentes */}
+          <span className="font-semibold text-2xl">{item.users}</span>
+          {/* Usuarios creados en el mes actual */}
+          <span className="font-medium text-base text-[#00a86b]">
+            +{item.new_users}
+          </span>
+        </div>
+      ))}
     </TopChartsCard>
   );
 }
