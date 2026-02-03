182d601dd17bd852050965d9477493e8fe57a026
DevJuan001 <juanesyt7@gmail.com>
2026-02-03 15:35:16 -0500

    feat: add initial transformations data for customer requests

---
+++ frontend_web/react+vite/src/modules/transformations/data/transformations.js
@@ -0,0 +1,25 @@
+export const transformations = [
+  {
+    transformationId: "001",
+    transformationCustomer: "Miguel Perez",
+    transformationSerial: "10285KANEF",
+    transformationRequirement: "Agregar mas memoría RAM",
+    transformationCreateDate: "2025/09/20",
+    transformationPhone: "30148515112",
+    transformationEmail: "miguel@yahoo.com",
+    transformationCity: "Medellin",
+    transformationStatus: "En Proceso",
+  },
+
+  {
+    transformationId: "002",
+    transformationCustomer: "Fernando Miguel",
+    transformationSerial: "1014125KF",
+    transformationRequirement: "Agregar más almacenamiento",
+    transformationCreateDate: "2025/09/20",
+    transformationPhone: "30148515112",
+    transformationEmail: "fernando@yahoo.com",
+    transformationCity: "Bogotá",
+    transformationStatus: "En Proceso",
+  },
+];
