7dd012740f2186394463d1a3484a508ef282b58d
DevJuan001 <juanesyt7@gmail.com>
2026-02-03 15:21:32 -0500

    feat: add apiRoutes configuration for API endpoints

---
+++ frontend_web/react+vite/src/config/apiRoutes.js
@@ -0,0 +1,13 @@
+export const apiRoutes = {
+    apiUrl: import.meta.env.VITE_API_URL,
+    users: "/users" ,
+    categories: "/categories" ,
+    subcategories: "/subcategories" ,
+    reports: "/reports" ,
+    dashboard: "/dashboard" ,
+    auth: "/auth" ,
+    suppliers: "/suppliers" ,
+    warranties: "/warranty_incidents" ,
+    transformations: "/output_details" ,
+    products: "/products"
+}
