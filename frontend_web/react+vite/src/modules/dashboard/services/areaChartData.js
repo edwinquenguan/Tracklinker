2e00864b8f03e5ba19f210728d50ec3ad9079899
DevJuan001 <juanesyt7@gmail.com>
2026-02-03 15:26:11 -0500

    feat: add data fetching functions for various dashboard charts including area, bar, brands, categories, line, output orders, pie, products, subcategories, and users

---
+++ frontend_web/react+vite/src/modules/dashboard/services/areaChartData.js
@@ -0,0 +1,22 @@
+import { apiRoutes } from "../../../config/apiRoutes";
+import { getToken } from "../../../utils/auth";
+
+export async function areaChartData() {
+  const res = await fetch(`${apiRoutes.apiUrl}${apiRoutes.dashboard}/monthly-outputs`, {
+    method: "GET",
+    headers: {
+      Authorization: getToken(),
+    },
+  });
+
+  // Validamos si la respuesta fue OK
+  if (!res.ok) {
+    throw new Error("Error en la petición");
+  }
+
+  // Convertimos la respuesta a json y la almacenamos en data
+  const data = await res.json();
+
+  // Retornamos la información para el gráfico
+  return data.data;
+}
