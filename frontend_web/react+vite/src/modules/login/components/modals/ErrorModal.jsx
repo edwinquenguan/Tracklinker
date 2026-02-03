6007d5bcfde78995a44c1ad92b84cb8ff3610aa4
DevJuan001 <juanesyt7@gmail.com>
2026-02-03 15:27:45 -0500

    feat: add ErrorModal and RecoverPasswordModal components for user authentication feedback

---
+++ frontend_web/react+vite/src/modules/login/components/modals/ErrorModal.jsx
@@ -0,0 +1,24 @@
+import { modalIcons } from "../../../../assets/icons/modalIcons";
+
+export default function ErrorModal({ onClose }) {
+  return (
+    <section className="flex flex-col items-center">
+      <img src={modalIcons.errorIcon} alt="" className="h-36 w-36" />
+      <div className="flex flex-col items-center gap-2 dark:text-white">
+        <span className="text-xl font-medium">
+          Usuario o Contraseña Incorrectos
+        </span>
+        <p>Verifique su usuario o contraseña</p>
+      </div>
+      <div className="pt-6">
+        <button
+          onClick={onClose}
+          className="px-6 py-3 bg-black rounded-xl text-white
+                dark:bg-white dark:text-black dark:hover:text-gray-800"
+        >
+          Ok
+        </button>
+      </div>
+    </section>
+  );
+}
---
+++ frontend_web/react+vite/src/modules/login/components/modals/RecoverPasswordModal.jsx
@@ -0,0 +1,24 @@
+import FormField from "../../../../globals/components/ui/FormField";
+import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
+import { loginIcons } from "../../../../assets/icons/loginIcons";
+
+export default function RecoverPasswordModal({cancelButtonOnClick, confirmButtonOnClick}) {
+  return (
+    <section className="flex flex-col items-center">
+      <FormField
+        labelText={"Email"}
+        inputIcon={loginIcons.emailIcon}
+        type="email"
+        placeholder={"Escribe tu correo aquí"}
+      />
+      <ConfirmCancelButtons
+        flexDirection={"flex-col"}
+        cancelText="Volver al login"
+        cancelButtonWidth={"w-52"}
+        cancelButtonOnClick={cancelButtonOnClick}
+        confirmText="Restablecer Contraseña"
+        confirmButtonOnClick={confirmButtonOnClick}
+      />
+    </section>
+  );
+}
