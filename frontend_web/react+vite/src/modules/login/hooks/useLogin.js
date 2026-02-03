9aa6fcef83320820d193c6168ab4b8199892fa95
DevJuan001 <juanesyt7@gmail.com>
2026-02-03 15:27:59 -0500

    feat: implement authentication service with login and signOut functions

---
+++ frontend_web/react+vite/src/modules/login/hooks/useLogin.js
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { login } from "../services/authService";
+
+export function useLogin(openModal) {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    // Prevenir el envio del formulario
+    e.preventDefault();
+    
+    const response = await login(email, password);
+    // Validacion de si existe algún error
+    if (!response.success) {
+      openModal(null, "error");
+    } else {
+      navigate("/home");
+    }
+  };
+  return { setEmail, setPassword, handleLogin };
+}
---
+++ frontend_web/react+vite/src/modules/login/services/authService.js
@@ -0,0 +1,56 @@
+import { apiRoutes } from "../../../config/apiRoutes";
+
+// Función para loguearse
+export async function login(email, password) {
+  try {
+    const res = await fetch(`${apiRoutes.apiUrl}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+
+    // Validamos si el estado de la respuesta no fue 200
+    if (!res.ok) {
+      throw new Error("Credenciales Invalidas");
+    }
+
+    // Almacenamos la respuesta y la pasamos a json
+    const data = await res.json();
+
+    // Guardamos el token en localStorage con su tipo
+    localStorage.setItem("token", `${data.token_type} ${data.access_token}`);
+
+    // Guardamos la información del usuario en el localStorage
+    localStorage.setItem("user", JSON.stringify(data.user))
+    
+    return data;
+  } catch (error) {
+    return {
+      error: error.message,
+    };
+  }
+}
+
+// Función para cerrar sesión
+export async function signOut() {
+  try {
+    // Trae el token del localStorage
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      throw new Error("No existe un token");
+    }
+    // Remueve todo lo que este almacenado en el localStorage
+    localStorage.clear();
+  } catch (error) {
+    console.error(error);
+    return {
+      error: error,
+    };
+  }
+}
