8dc719ca8e048ef2b07706123634019f2e9bea80
DevJuan001 <juanesyt7@gmail.com>
2026-02-03 15:20:08 -0500

    feat: add icon files for aside, header, login, main, and modal components

---
+++ frontend_web/react+vite/src/assets/icons/headerIcons.js
@@ -0,0 +1,9 @@
+import bellIcon from "./header/bell.svg";
+import menubarIcon from "./header/menubar-icon.svg";
+import searchIcon from "./header/search-icon.svg";
+
+export const headerIcons = {
+  bellIcon,
+  menubarIcon,
+  searchIcon,
+};
---
+++ frontend_web/react+vite/src/assets/icons/mainIcons.js
@@ -0,0 +1,89 @@
+/* Dashboard Icons */
+import arrowDown from "./main/dashboard/arrow-down.svg";
+import arrowUp from "./main/dashboard/arrow-up.svg";
+import firstChart from "./main/dashboard/first-chart.svg";
+import secondChart from "./main/dashboard/second-chart.svg";
+import thirdChart from "./main/dashboard/third-chart.svg";
+import fourthChart from "./main/dashboard/fourth-chart.svg";
+import barChart from "./main/dashboard/bar-chart.svg";
+import barChart2 from "./main/dashboard/bar-chart-2.png";
+import waveChart from "./main/dashboard/wave-chart.svg";
+import waveChart2 from "./main/dashboard/wave-chart.png";
+import purpleCircle from "./main/dashboard/purple-circle.svg";
+import lightPurpleCircle from "./main/dashboard/light-purple-circle.svg";
+import grayCircle from "./main/dashboard/gray-circle.svg";
+
+/* Users Icons */
+import addUserIcon from "./main/users/add-user-icon.svg";
+import addressIcon from "./main/users/address-icon.svg";
+import phoneIcon from "./main/users/phone-icon.svg";
+import rolIcon from "./main/users/rol-icon.svg";
+import cityIcon from "./main/users/city-icon.svg";
+
+/* Products Icons */
+import addProductIcon from "./main/products/add-product-icon.svg";
+import barcodeReaderIcon from "./main/products/barcode-reader-icon.svg";
+import barcodeIcon from "./main/products/barcode-scanner-icon.svg";
+
+/* Warranties Icons */
+import addWarrantyIcon from "./main/warranties/add-warranty-icon.svg";
+import incompleteIcon from "./main/warranties/incomplete-icon.svg";
+import inprocessIcon from "./main/warranties/inprocess-icon.svg";
+import completeIcon from "./main/warranties/complete-icon.svg";
+import editIcon from "./main/warranties/edit-info-icon.svg";
+
+
+/* Action Icons */
+import moreInfoIcon from "./main/actions/more-info-icon.svg";
+import editInfoIcon from "./main/actions/edit-info-icon.svg";
+import deleteIcon from "./main/actions/delete-user-icon.svg";
+import filterIcon from "./main/actions/filter-icon.svg";
+import addIcon from "./main/actions/add-icon.svg";
+import uploadIcon from "./main/actions/upload-icon.svg";
+
+export const dashboardIcons = {
+  arrowDown,
+  arrowUp,
+  firstChart,
+  secondChart,
+  thirdChart,
+  fourthChart,
+  barChart,
+  barChart2,
+  waveChart,
+  waveChart2,
+  purpleCircle,
+  lightPurpleCircle,
+  grayCircle,
+};
+
+export const usersIcons = {
+  addUserIcon,
+  addressIcon,
+  phoneIcon,
+  rolIcon,
+  cityIcon,
+};
+
+export const productsIcons = {
+  addProductIcon,
+  barcodeReaderIcon,
+  barcodeIcon,
+};
+
+export const actionsIcons = {
+  moreInfoIcon,
+  editInfoIcon,
+  deleteIcon,
+  filterIcon,
+  addIcon,
+  uploadIcon,
+};
+
+export const warrantiesIcons = {
+  addWarrantyIcon,
+  incompleteIcon,
+  inprocessIcon,
+  completeIcon,
+  editIcon,
+};
