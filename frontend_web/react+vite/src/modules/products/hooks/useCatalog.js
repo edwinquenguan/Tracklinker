bbe1157697bda319d0c6f948debf0f3acf01570c
DevJuan001 <juanesyt7@gmail.com>
2026-02-03 15:30:25 -0500

    feat: add useCatalog, useCreateProduct hooks for product management

---
+++ frontend_web/react+vite/src/modules/products/hooks/useCatalog.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import { getProducts } from "../services/getProducts";
+import { getCategoriesService } from "../../categories/services/getCategoriesService"
+import { getSubcategories } from "../../subcategories/services/getSubcategoriesService";
+
+export function useCatalog() {
+  // Definir los estados y sus valores por defecto
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [subcategories, setSubcategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        setLoading(true);
+        const data = await getProducts();
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProducts();
+
+    async function fetchCategories() {
+      try {
+        const categoryData = await getCategoriesService();
+        setCategories(categoryData);
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+    fetchCategories();
+
+    async function fetchSubcategories() {
+      try {
+        const subcategoryData = await getSubcategories();
+        setSubcategories(subcategoryData);
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+    fetchSubcategories();
+  }, []);
+
+  return { products, categories, subcategories, loading, error };
+}
---
+++ frontend_web/react+vite/src/modules/products/hooks/useCreateProduct.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { createProductService } from "../services/createProductService";
+
+export function useCreateProduct(formData) {
+  const [form, setForm] = useState(formData);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  console.log(form);
+
+  function handleChange(e) {
+    setForm((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  async function handleSubmit(e, setInnerModal) {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await createProductService(form);
+      if (response.sucess) {
+        setInnerModal("success");
+      }
+    } catch (error) {
+      setInnerModal("error");
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { form, loading, error, handleChange, handleSubmit };
+}
