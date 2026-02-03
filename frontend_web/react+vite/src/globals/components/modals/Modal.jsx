5ffd1a847d8df07917c1af009adc12012dbac9c0
DevJuan001 <juanesyt7@gmail.com>
2026-02-03 15:22:39 -0500

    feat: add modal components including ConfirmCancelButtons, ErrorModal, FilterModal, ProfileModal, SelectMenu, and SuccessModal

---
+++ frontend_web/react+vite/src/globals/components/modals/Modal.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import { modalIcons } from "../../../assets/icons/modalIcons";
+
+export default function Modal({ isOpen, title, children, onClose, type, z_index = "50" }) {
+  const [visible, setVisible] = useState(false);
+  const [closing, setClosing] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      setVisible(true);
+      setClosing(false);
+    }
+  }, [isOpen]);
+
+  // Validación de si la modal no está visible
+  if (!visible) return null;
+  
+  // Manejador para cuando la modal cierre
+  const handleClose = () => {
+    setClosing(true);
+    setTimeout(() => {
+      setVisible(false);
+      onClose();
+    }, 500);
+  };
+
+  return (
+    /* Container de la modal */
+    <section
+      className={`fixed z-${z_index} inset-0 bg-[#0000002c]
+            ${
+              type === "filter"
+                ? "flex justify-end items-start pr-[260px] pt-4 bg-[#00000013]"
+                : type === "download"
+                  ? "flex justify-end items-end p-4 bg-[#0000001e]"
+                  : "flex items-center justify-center"
+            }
+        `}
+      onClick={handleClose}
+    >
+      {/* Card blanca o modal */}
+      {/* stopPropagation sirve para que al momento de seleccionar la modal no la cierre */}
+      <section
+        className={`bg-white rounded-xl shadow-lg w-[90%] p-6 relative
+            dark:bg-black dark:shadow-[0px_0px_0px_1px_#101012]
+            ${closing ? "animate-modalFadeOut" : "animate-modalFadeIn"}
+            ${
+              type === "filter"
+                ? "max-w-sm"
+                : type === "user"
+                  ? "max-w-2xl"
+                  : type === "download"
+                    ? "max-w-sm"
+                    : "max-w-xl"
+            }
+        `}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {/* Cabecera de la modal donde esta el titúlo y el icono para cerrarla */}
+        <header className="flex justify-between items-center mb-4">
+          <h2 className="text-lg font-medium dark:text-white">{title}</h2>
+          {/* Icono "x" para cerrar la modal */}
+          <button
+            onClick={handleClose}
+            className="p-1.5 rounded-3xl transition
+                    hover:bg-[#efedf0]
+                     dark:hover:bg-[#c5c6ce27]"
+          >
+            <img
+              src={modalIcons.closeIcon}
+              alt=""
+              className="invert brightness-200 transition duration-300
+                    dark:brightness-0 dark:hover:bg-transparent"
+            />
+          </button>
+        </header>
+        {/* Contenido principal de la modal o cuerpo de la modal */}
+        <div>{children}</div>
+      </section>
+    </section>
+  );
+}
---
+++ frontend_web/react+vite/src/globals/components/modals/ProfileModal.jsx
@@ -0,0 +1,102 @@
+import { asideIcons } from "../../../assets/icons/asideIcons";
+import { useUser } from "../../hooks/useUser";
+
+export default function ProfileModal({ onClose }) {
+  const { user } = useUser()
+  return (
+    <section className="flex gap-5">
+      <aside className="w-40 pr-3 border-r border-gray-300 dark:border-[#3a3d43]">
+        {/* Lista de opciones */}
+        <ul className="flex flex-col gap-2">
+          <li>
+            <button
+              className="flex items-center px-5 py-3 rounded-xl bg-black text-white gap-3
+                        hover:text-gray-200
+                        dark:bg-white dark:text-black dark:hover:text-gray-700"
+            >
+              <img
+                src={asideIcons.usersIcon}
+                alt=""
+                className="invert brightness-0 dark:brightness-200"
+              />
+              <span> Usuario </span>
+            </button>
+          </li>
+          <li>
+            <button
+              className="flex py-3 pl-4 pr-7 gap-3 rounded-xl transition duration-300
+                        hover:bg-[#efedf0]
+                        dark:text-white dark:hover:bg-[#202022]"
+            >
+              <img
+                src={asideIcons.themesIcon}
+                alt=""
+                className="brightness-200 dark:invert"
+              />
+              <span> Temas </span>
+            </button>
+          </li>
+          <li>
+            <button
+              className="flex py-3 px-4 gap-3 rounded-xl transition duration-300
+                        hover:bg-[#efedf0]
+                        dark:text-white dark:hover:bg-[#202022]"
+            >
+              <img
+                src={asideIcons.helpIcon}
+                alt=""
+                className="invert brightness-200 dark:brightness-0"
+              />
+              <span> Creditos </span>
+            </button>
+          </li>
+        </ul>
+      </aside>
+      <main className="flex flex-col w-96 items-center justify-center">
+        {/* Información del usuario */}
+        <section className="flex flex-col items-center justify-center">
+          <img src={asideIcons.avatarIcon} alt="" className="h-20 w-20" />
+          <article className="flex flex-col items-center pt-5 gap-2">
+            <section className="flex flex-col items-center justify-center gap-1">
+              <span className="dark:text-white"> Nombre </span>
+              <span
+                className="py-5 px-10 rounded-xl bg-[#efedf0] font-light
+                            dark:bg-[#202022] dark:text-white"
+              >
+                {user.name} {user.first_surname} {user.second_surname}
+              </span>
+            </section>
+            <section className="flex flex-col items-center justify-center gap-1">
+              <span className="dark:text-white"> Correo Electrónico </span>
+              <span
+                className="p-5 rounded-xl bg-[#efedf0] font-light
+                            dark:bg-[#202022] dark:text-white"
+              >
+                {user.email}
+              </span>
+            </section>
+          </article>
+        </section>
+        {/* Botones */}
+        <section className="flex flex-col pt-8 gap-3">
+          <button
+            onClick={onClose}
+            className="px-5 py-3 rounded-lg text-sm bg-black text-white transition
+                    hover:text-gray-200
+                    dark:bg-[#2020226c] dark:text-white dark:hover:text-gray-800"
+          >
+            Cambiar Contraseña
+          </button>
+          <button
+            onClick={onClose}
+            className="px-5 py-3 rounded-lg text-red-500 text-sm transition
+                    hover:bg-[#efedf0]
+                    dark:hover:bg-[#2020226c]"
+          >
+            Cerrar Sesión
+          </button>
+        </section>
+      </main>
+    </section>
+  );
+}
