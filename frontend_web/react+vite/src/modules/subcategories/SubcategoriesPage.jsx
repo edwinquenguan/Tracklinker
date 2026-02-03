466a08a5fc83018b14029b4a94497b07324034ec
DevJuan001 <juanesyt7@gmail.com>
2026-02-03 15:32:14 -0500

    feat: add SubcategoriesPage, SubcategoriesList, SubcategoriesItem, and SubcategoriesActions components for subcategory management

---
+++ frontend_web/react+vite/src/modules/subcategories/SubcategoriesPage.jsx
@@ -0,0 +1,96 @@
+// Hooks
+import { useModal } from "../../globals/hooks/useModal";
+import { useSubcategories } from "./hooks/useSubcategories";
+// Iconos
+import { actionsIcons } from "../../assets/icons/mainIcons";
+// Modales
+import Modal from "../../globals/components/modals/Modal";
+import FilterModal from "../../globals/components/modals/FilterModal";
+import ProfileModal from "../../globals/components/modals/ProfileModal";
+import AddSubcategoryModal from "./components/modals/AddSubcategoryModal";
+import EditSubcategoryModal from "./components/modals/EditSubcategoryModal";
+import DeleteSubcategoryModal from "./components/modals/DeleteSubcategoryModal";
+// Componentes
+import Layout from "../../globals/components/Layout/Layout";
+import TopSection from "../../globals/components/ui/TopSection";
+import SubcategoriesList from "./components/ui/SubcategoriesList";
+import MoreSubcategoryInfoModal from "./components/modals/MoreSubcategoryInfoModal";
+
+export default function SubcategoriesPage() {
+  const { subcategories, loading, error, fetchSubcategories } =
+    useSubcategories();
+  const { modalType, isOpen, modalData, openModal, closeModal } = useModal();
+
+  return (
+    <Layout avatarOnClick={() => openModal(null, "user")}>
+      <TopSection
+        sectionName={"Subcategorias"}
+        addButtonIcon={actionsIcons.addIcon}
+        addButtonText={"Agregar Subcategoria"}
+        createOnClick={() => openModal(null, "add", fetchSubcategories)}
+        filterOnClick={() => openModal(null, "filter", fetchSubcategories)}
+      />
+      {/* Listado de las subcategorias */}
+      <SubcategoriesList
+        subcategories={subcategories}
+        loading={loading}
+        error={error}
+        refetch={fetchSubcategories}
+        openModal={openModal}
+      />
+      {/* Modales */}
+      {modalType && (
+        <Modal
+          title={
+            modalType === "user"
+              ? "Configuración"
+              : modalType === "filter"
+                ? "Filtrar"
+                : modalType === "add"
+                  ? "Agregar Subcategoria"
+                  : modalType === "info"
+                    ? "Información de la subcategoría"
+                    : modalType === "edit"
+                      ? "Editar Subcategoria"
+                      : "Eliminar Subcategoria"
+          }
+          type={modalType}
+          isOpen={isOpen}
+          onClose={() => closeModal()}
+        >
+          {modalType === "user" && (
+            <ProfileModal onClose={() => closeModal()} />
+          )}
+          {modalType === "filter" && (
+            <FilterModal onClose={() => closeModal()}></FilterModal>
+          )}
+          {/* Modal para agregar una subcategoria */}
+          {modalType === "add" && (
+            <AddSubcategoryModal onClose={() => closeModal()} />
+          )}
+          {/* Modal para mas información de la subcategoria */}       
+          {modalType === "info" && (
+            <MoreSubcategoryInfoModal
+              subcategory={modalData}
+              onClose={() => closeModal()}
+            />
+          )}
+          {/* Modal para editar la información de la subcategoria */}
+          {modalType === "edit" && (
+            <EditSubcategoryModal
+              subcategory={modalData}
+              onClose={() => closeModal()}
+            />
+          )}
+          {/* Modal para eliminar la subcategoria */}
+          {modalType === "delete" && (
+            <DeleteSubcategoryModal
+              subcategory={modalData}
+              onClose={() => closeModal()}
+            />
+          )}
+        </Modal>
+      )}
+    </Layout>
+  );
+}
