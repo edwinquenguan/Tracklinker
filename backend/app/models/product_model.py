e720f8e44c7330b15dde49c2255472de8f3d8f70
DevJuan001 <juanesyt7@gmail.com>
2026-03-05 20:43:02 -0500

    refactor: remove redundant product_model field and adjust product_brand type in Product model

--- backend/app/models/product_model.py
+++ backend/app/models/product_model.py
@@ -4,9 +4,7 @@ from datetime import datetime
 class Product(BaseModel):
     input_order_id: int
     subcategory_id: int
-    product_model: str
     product_serial: str
-    product_brand: str
-    product_model: str
-    product_stock: int
+    product_brand: int
+    product_details_id: int
     product_garanty_input: datetime
