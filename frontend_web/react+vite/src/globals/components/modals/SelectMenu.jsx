f47f2586656ea613a005d6404cd2f60380cffa2a
DevJuan001 <juanesyt7@gmail.com>
2026-03-05 20:44:57 -0500

    feat: enhance SelectMenu component by adding an icon button for additional functionality and improving layout

--- frontend_web/react+vite/src/globals/components/modals/SelectMenu.jsx
+++ frontend_web/react+vite/src/globals/components/modals/SelectMenu.jsx
@@ -3,25 +3,40 @@ export default function SelectMenu({
   name,
   onChange,
   value,
-  width,
   defaultValue,
   spanText,
   children,
+  addIcon,
+  addIconFunction,
+  addButtonInvisible = true
 }) {
   return (
-    <section className={`flex flex-col px-2 gap-1 w-${width}`}>
+    <section className={`flex flex-col gap-1 w-full`}>
       <span className="text-sm dark:text-white">{spanText}</span>
-      <select
-        defaultValue={defaultValue}
-        value={value}
-        onChange={onChange}
-        name={name}
-        id={id}
-        className="h-12 p-2 border outline-none
-            dark:bg-[#2020226c] dark:border-[#101012] dark:text-white"
-      >
-        {children}
-      </select>
+      <div className="w-full flex items-center gap-1.5">
+        <select
+          defaultValue={defaultValue}
+          value={value}
+          onChange={onChange}
+          name={name}
+          id={id}
+          className="h-12 w-full px-4 border outline-none rounded-xl bg-slate-50
+              dark:bg-[#2020226c] dark:border-[#101012] dark:text-white"
+        >
+          {children}
+        </select>
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            if (addIconFunction) addIconFunction(e);
+          }}
+          disabled={addButtonInvisible}
+          type="button"
+          className={`w-6 h-6 invert dark:invert-0 ${addButtonInvisible ? "hidden" : "opacity-100"}`}
+        >
+          <img src={addIcon} />
+        </button>
+      </div>
     </section>
   );
 }
