a066379b2ab9d444ced034e3eaed9206b1831533
DevJuan001 <juanesyt7@gmail.com>
2026-03-13 16:19:36 -0500

    refactor: update ReportSectionCard component to use icon components and adjust layout for improved styling

--- frontend_web/react+vite/src/modules/reports/components/ui/ReportSectionCard.jsx
+++ frontend_web/react+vite/src/modules/reports/components/ui/ReportSectionCard.jsx
@@ -2,28 +2,27 @@ export default function ReportSectionCard({
   sectionOnClick,
   sectionKey,
   sectionName,
-  sectionIcon,
+  sectionIcon: Icon,
   sectionIconAlt,
 }) {
   return (
     <button
       onClick={sectionOnClick}
       key={sectionKey}
-      className="flex flex-col h-32 w-32 place-items-center gap-2 transition duration-300
+      className="flex flex-col h-36 w-36 place-items-center gap-2 transition duration-300
         hover:scale-[1.1]
         xl:h-48 xl:w-32"
     >
       <section
         className="h-20 w-20 rounded-xl bg-[#eae8eb] content-center justify-items-center
             dark:bg-[#101012]
-            xl:h-28 xl:w-28 
+            xl:h-32 xl:w-32
             md:h-28 md:w-28
             sm:h-28 sm:w-28"
       >
-        <img
-          src={sectionIcon}
+        <Icon
           alt={sectionIconAlt}
-          className="w-12 dark:brightness-[.2] dark:invert"
+          className="w-10 h-10 fill-[#75777E] dark:invert"
         />
       </section>
       <p className="text-sm font-medium text-center dark:text-white">
