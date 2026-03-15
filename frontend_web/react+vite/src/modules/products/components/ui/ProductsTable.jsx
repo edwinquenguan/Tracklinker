0af3eb7d892b3aa7ec648feb4a87d2c2f6ffd190
DevJuan001 <juanesyt7@gmail.com>
2026-03-15 14:17:48 -0500

    refactor: update ProductsTable component styles for improved readability and dark mode support

--- frontend_web/react+vite/src/modules/products/components/ui/ProductsTable.jsx
+++ frontend_web/react+vite/src/modules/products/components/ui/ProductsTable.jsx
@@ -14,11 +14,11 @@ export default function ProductsTable({products, openModal, refetch}) {
       >
         {/* Cabecera de la tabla */}
         <thead
-          className="sticky top-0
-                  dark:bg-black dark:text-gray-300"
+          className="sticky top-0 bg-white
+                   dark:text-white"
         >
           <tr
-            className="h-[50px] border-b border-gray-200 text-base
+            className="h-[40px] border-b border-gray-200 text-sm
                       dark:border-[#303033]"
           >
             <th className="font-medium text-start pl-4"> Fecha de Ingreso </th>
@@ -28,7 +28,6 @@ export default function ProductsTable({products, openModal, refetch}) {
             <th className="font-medium text-start pl-4"> Modelo </th>
             <th className="font-medium text-start pl-2"> Descripción </th>
             <th className="font-medium text-start pl-4"> Marca </th>
-            <th className="font-medium text-start pl-2"> Stock </th>
             <th className="font-medium text-start pl-4"> Tiempo de Garantia </th>
             <th className="font-medium text-start pr-4"> Acciones </th>
           </tr>
@@ -40,7 +39,7 @@ export default function ProductsTable({products, openModal, refetch}) {
             {/* Productos */}
               <tr
                 key={product.product_serial}
-                className="h-12 text-base overflow-x-auto overflow-y-auto transition duration-500 text-[#45474d] 
+                className="h-12 text-base overflow-x-auto overflow-y-auto transition duration-500 text-[#45474d] dark:text-white
                           hover:bg-[#e3e2e4] hover:shadow-md
                           dark:hover:bg-[#101012]"
               >
@@ -89,13 +88,6 @@ export default function ProductsTable({products, openModal, refetch}) {
                   className="font-normal text-start pl-4 text-sm"
                 >
                   {product.brand}
-                </th>
-
-                {/* Stock */}
-                <th
-                  className="font-normal text-start pl-2 text-sm"
-                >
-                  {product.stock}
                 </th>
 
                 {/* Tiempo de garantía */}
