7454b3899d0254ec962d0bfd92a15c3c18764f3e
DevJuan001 <juanesyt7@gmail.com>
2026-03-15 19:02:09 -0500

    refactor: enhance FilterButton and TopSection components to support dynamic visibility and improved layout

--- frontend_web/react+vite/src/globals/components/ui/TopSection.jsx
+++ frontend_web/react+vite/src/globals/components/ui/TopSection.jsx
@@ -3,18 +3,28 @@ import CreateButton from "./CreateButton";
 
 export default function TopSection({
   sectionName,
+  rangeDate = false,
   addButtonIcon,
   addButtonText,
   children,
   filterOnClick,
+  filterButton = true,
   createOnClick,
 }) {
   return (
     <section className="flex items-center justify-between px-2 pb-3">
-      <h1 className="font-medium dark:text-white">{sectionName}</h1>
+      <div className="flex flex-col">
+        <h1 className="font-medium dark:text-white">{sectionName}</h1>
+        <span className={`${rangeDate ? "block" : "hidden"} text-xs animate-blurUp`}>
+          {rangeDate}
+        </span>
+      </div>
       <section className="flex gap-4">
         {children}
-        <FilterButton onClick={filterOnClick} />
+        <FilterButton
+          onClick={filterOnClick}
+          filterButtonVisibility={filterButton}
+        />
         <CreateButton
           icon={addButtonIcon}
           text={addButtonText}
