420767220050fc6ff3e7fdd9729ac25cb22357dc
DevJuan001 <juanesyt7@gmail.com>
2026-03-17 17:31:46 -0500

    style: refactor SelectMenu component for improved layout and styling, including adjustments to select and button elements

--- frontend_web/react+vite/src/globals/components/modals/SelectMenu.jsx
+++ frontend_web/react+vite/src/globals/components/modals/SelectMenu.jsx
@@ -8,23 +8,27 @@ export default function SelectMenu({
   children,
   addIcon,
   addIconFunction,
-  addButtonInvisible = true
+  addButtonInvisible = true,
 }) {
   return (
-    <section className={`flex flex-col gap-1 w-full`}>
+    <section className={`w-full flex flex-col gap-1`}>
       <span className="text-sm dark:text-white">{spanText}</span>
       <div className="w-full flex items-center gap-1.5">
-        <select
-          defaultValue={defaultValue}
-          value={value}
-          onChange={onChange}
-          name={name}
-          id={id}
-          className="h-12 w-full px-4 border outline-none rounded-xl bg-slate-50
-              dark:bg-[#2020226c] dark:border-[#101012] dark:text-white"
+        <div
+          className="w-full h-11 px-2 border border-[#a1a1a131] bg-[#e5e5e527] rounded-lg
+        dark:border-[#ffffff15] dark:bg-[#ffffff10] dark:text-[#a1a1a1]"
         >
-          {children}
-        </select>
+          <select
+            defaultValue={defaultValue}
+            value={value}
+            onChange={onChange}
+            name={name}
+            id={id}
+            className="h-full w-full outline-none bg-transparent"
+          >
+            {children}
+          </select>
+        </div>
         <button
           onClick={(e) => {
             e.stopPropagation();
@@ -32,9 +36,10 @@ export default function SelectMenu({
           }}
           disabled={addButtonInvisible}
           type="button"
-          className={`w-6 h-6 dark:invert ${addButtonInvisible ? "hidden" : "opacity-100"}`}
+          className={`${addButtonInvisible ? "hidden" : "opacity-100"} w-12 h-11 flex items-center justify-center border rounded-xl
+              dark:invert`}
         >
-          <img src={addIcon} />
+          <img src={addIcon} className="h-5 w-5" />
         </button>
       </div>
     </section>
