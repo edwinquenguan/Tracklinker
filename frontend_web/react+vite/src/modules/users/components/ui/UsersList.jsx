d832dd982285b419b65659cbc3b05680f066edad
DevJuan001 <juanesyt7@gmail.com>
2026-03-17 18:59:21 -0500

    feat: add loading skeleton to UsersList component for improved user experience during data fetching

--- frontend_web/react+vite/src/modules/users/components/ui/UsersList.jsx
+++ frontend_web/react+vite/src/modules/users/components/ui/UsersList.jsx
@@ -1,4 +1,6 @@
 import UserItem from "./UserItem";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function UsersList({
   users,
@@ -7,30 +9,37 @@ export default function UsersList({
   refetch,
   openModal,
 }) {
-
   return (
     /* Contenedor de los usuarios */
     <section className="max-h-[95%] max-w-full overflow-x-auto overflow-y-auto overflow-hidden">
       <ul className="flex flex-col gap-1">
-        {users.map((user) => (
-          // Usuarios
-          <UserItem
-            key={user.user_id}
-            user={user}
-            moreInfoOnClick={(e) => {
-              e.stopPropagation();
-              openModal(user, "info");
-            }}
-            editButtonOnClick={(e) => {
-              e.stopPropagation();
-              openModal(user, "edit", refetch);
-            }}
-            deleteButtonOnClick={(e) => {
-              e.stopPropagation();
-              openModal(user, "delete", refetch);
-            }}
-          />
-        ))}
+        {loading ? (
+          <SkeletonTheme baseColor="#f3eef5" highlightColor="#848185">
+            <li>
+              <Skeleton height={"68px"} count={13} borderRadius={"8px"} />
+            </li>
+          </SkeletonTheme>
+        ) : (
+          users.map((user) => (
+            // Usuarios
+            <UserItem
+              key={user.id}
+              user={user}
+              moreInfoOnClick={(e) => {
+                e.stopPropagation();
+                openModal(user, "info");
+              }}
+              editButtonOnClick={(e) => {
+                e.stopPropagation();
+                openModal(user, "edit", refetch);
+              }}
+              deleteButtonOnClick={(e) => {
+                e.stopPropagation();
+                openModal(user, "delete", refetch);
+              }}
+            />
+          ))
+        )}
       </ul>
     </section>
   );
