5effe66abd43e0b4644e84125b9a9abcc8cd3978
DevJuan001 <juanesyt7@gmail.com>
2026-03-17 15:16:09 -0500

    style: refactor ProductsTable component for improved readability and consistent formatting

--- frontend_web/react+vite/src/modules/products/components/ui/ProductsTable.jsx
+++ frontend_web/react+vite/src/modules/products/components/ui/ProductsTable.jsx
@@ -1,13 +1,11 @@
 import ActionButtons from "../../../../globals/components/ui/ActionButtons";
 
-export default function ProductsTable({products, openModal, refetch}) {
-
+export default function ProductsTable({ products, openModal, refetch }) {
   return (
     <section
-        className="max-h-[95%] max-w-full border border-gray-200 rounded-xl shadow-md overflow-y-auto overflow-x-auto overflow-hidden
+      className="max-h-[95%] max-w-full border border-gray-200 rounded-xl shadow-md overflow-y-auto overflow-x-auto overflow-hidden
             dark:border-[#303033]"
-      >
-
+    >
       <table
         className="min-w-full min-h-full appearance-none border-collapse
               dark:bg-black"
@@ -15,7 +13,7 @@ export default function ProductsTable({products, openModal, refetch}) {
         {/* Cabecera de la tabla */}
         <thead
           className="sticky top-0 bg-white
-                   dark:text-white"
+                   dark:text-white dark:bg-black"
         >
           <tr
             className="h-[40px] border-b border-gray-200 text-sm
@@ -37,74 +35,60 @@ export default function ProductsTable({products, openModal, refetch}) {
         {products.map((product) => (
           <tbody className="font-normal dark:text-gray-300">
             {/* Productos */}
-              <tr
-                key={product.product_serial}
-                className="h-12 text-base overflow-x-auto overflow-y-auto transition duration-500 text-[#45474d] dark:text-white
+            <tr
+              key={product.product_serial}
+              className="text-base overflow-x-auto overflow-y-auto transition duration-500 text-[#45474d] dark:text-white
                           hover:bg-[#e3e2e4] hover:shadow-md
                           dark:hover:bg-[#101012]"
-              >
-                {/* Fecha de ingreso */}
-                <th className="font-normal text-start pl-4 text-sm">
-                  {product.input_date}
-                </th>
+            >
+              {/* Fecha de ingreso */}
+              <th className="font-normal text-start pl-4 text-sm">
+                {product.input_date}
+              </th>
 
-                {/* Orden de Entrada */}
-                <th
-                  className="font-normal text-start pl-4 text-sm"
-                >
-                  {product.input_order}
-                </th>
+              {/* Orden de Entrada */}
+              <th className="font-normal text-start pl-4 text-sm">
+                {product.input_order}
+              </th>
 
-                {/* Subcategoria */}
-                <th
-                  className="font-normal text-start pl-4 text-sm"
-                >
-                  {product.subcategory}
-                </th>
+              {/* Subcategoria */}
+              <th className="font-normal text-start pl-4 text-sm">
+                {product.subcategory}
+              </th>
 
-                {/* Serial */}
-                <th
-                  className="font-normal text-start pl-4 text-sm"
-                >
-                  {product.product_serial}
-                </th>
+              {/* Serial */}
+              <th className="font-normal text-start pl-4 text-sm">
+                {product.product_serial}
+              </th>
 
-                {/* Modelo */}
-                <th
-                  className="font-normal text-start pl-4 text-sm"
-                >
-                  {product.model}
-                </th>
+              {/* Modelo */}
+              <th className="font-normal text-start pl-4 text-sm">
+                {product.model}
+              </th>
 
-                {/* Descripción */}
-                <th
-                  className="font-normal text-start pl-2 text-sm"
-                >
-                  {product.description}
-                </th>
+              {/* Descripción */}
+              <th className="font-normal text-start pl-2 text-sm">
+                {product.description}
+              </th>
 
-                {/* Marca */}
-                <th
-                  className="font-normal text-start pl-4 text-sm"
-                >
-                  {product.brand}
-                </th>
+              {/* Marca */}
+              <th className="font-normal text-start pl-4 text-sm">
+                {product.brand}
+              </th>
 
-                {/* Tiempo de garantía */}
-                <th
-                  className="font-normal text-start pl-4 text-sm"
-                >
-                  {product.warranty_time}
-                </th>
+              {/* Tiempo de garantía */}
+              <th className="font-normal text-start pl-4 text-sm">
+                {product.warranty_time}
+              </th>
 
-                {/* Botones */}
-                <th className="flex items-center justify-center h-14">
-                  <ActionButtons
-                    editButtonOnClick={() => openModal(product, "edit", refetch)}
-                    deleteButtonVisible={false}
-                  />
-                </th>
-              </tr>
+              {/* Botones */}
+              <th className="flex items-center justify-center h-14">
+                <ActionButtons
+                  editButtonOnClick={() => openModal(product, "edit", refetch)}
+                  deleteButtonVisible={false}
+                />
+              </th>
+            </tr>
           </tbody>
         ))}
       </table>
