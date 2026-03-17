5d72ef58f53e8ff7930a96f70f401d615d123350
DevJuan001 <juanesyt7@gmail.com>
2026-03-17 18:59:27 -0500

    refactor: improve code formatting and error handling in user service functions for better readability and consistency

--- frontend_web/react+vite/src/modules/users/services/deleteUserService.js
+++ frontend_web/react+vite/src/modules/users/services/deleteUserService.js
@@ -2,16 +2,19 @@ import { apiRoutes } from "../../../config/apiRoutes";
 import { getToken } from "../../../utils/auth";
 
 export async function deleteUserService(userId) {
-    const res = await fetch(`${apiRoutes.apiUrl}${apiRoutes.users}/delete/${userId}`, {
-        method: "DELETE",
-        headers: {
-            Authorization: getToken()
-        }
-    });
+  const res = await fetch(
+    `${apiRoutes.apiUrl}${apiRoutes.users}/delete/${userId}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: getToken(),
+      },
+    },
+  );
 
-    if (!res.ok) {
-        throw new Error("Error al intentar eliminar el usuario");
-    }
+  if (!res.ok) {
+    throw new Error("Error al intentar eliminar el usuario");
+  }
 
-    return await res.json()
+  return await res.json();
 }
--- frontend_web/react+vite/src/modules/users/services/editUserService.js
+++ frontend_web/react+vite/src/modules/users/services/editUserService.js
@@ -2,18 +2,21 @@ import { apiRoutes } from "../../../config/apiRoutes";
 import { getToken } from "../../../utils/auth";
 
 export async function editUserService(user_id, user_data) {
-    const res = await fetch(`${apiRoutes.apiUrl}${apiRoutes.users}/update/${user_id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: getToken()
-        },
-        body: JSON.stringify(user_data),
-    })
-        // Validamos si la respuesta no fue OK
-    if (!res.ok) {
-        throw new Error(await res.json());
-    }
+  const res = await fetch(
+    `${apiRoutes.apiUrl}${apiRoutes.users}/update/${user_id}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: getToken(),
+      },
+      body: JSON.stringify(user_data),
+    },
+  );
+  // Validamos si la respuesta no fue OK
+  if (!res.ok) {
+    throw new Error("Error al intentar actualizar el usuario");
+  }
 
-    return await res.json();
+  return await res.json();
 }
