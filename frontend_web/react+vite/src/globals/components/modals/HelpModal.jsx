c3ec1824ba8bfe5a27bbdf5eff222517ed221df2
DevJuan001 <juanesyt7@gmail.com>
2026-03-17 17:31:26 -0500

    style: enhance HelpModal component with improved textarea styling and responsive layout adjustments

--- frontend_web/react+vite/src/globals/components/modals/HelpModal.jsx
+++ frontend_web/react+vite/src/globals/components/modals/HelpModal.jsx
@@ -4,18 +4,21 @@ import ConfirmCancelButtons from "./ConfirmCancelButtons";
 export default function HelpModal({ onClose }) {
   return (
     <section className="flex flex-col items-center justify-center gap-4">
-      <span className="text-[#75777E]">
+      <span className="text-[#75777E] text-sm">
         Si te encontraste con algún error, necesitas ayuda con un registro o
         presentas un problema con la aplicación, puedes comunicarte con nosotros
         y te ayudaremos.
       </span>
-      <textarea
-        name=""
-        id=""
-        className="w-full h-40 px-3 py-4 border border-gray-500 text-black rounded-xl
-        placeholder:text-gray-800"
-        placeholder="Escribe aquí tu problema o sugerencia"
-      />
+      <div className="w-full pr-1 pl-2 pt-2 border border-gray-500 text-black rounded-xl">
+        <textarea
+          name=""
+          id=""
+          className="w-full h-40 outline-none
+          placeholder:text-gray-800
+          dark:bg-transparent dark:placeholder:text-gray-300"
+          placeholder="Escribe aquí tu problema o sugerencia"
+        />
+      </div>
       <ConfirmCancelButtons
         itemsPosition="end"
         confirmText="Enviar"
