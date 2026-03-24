66368668be9d7fafdcdcab49d4ecd2eeecf70a11
DevJuan001 <juanesyt7@gmail.com>
2026-03-24 17:57:45 -0500

    refactor: streamline output details routes by removing unnecessary role dependencies and adding new endpoints for enabling and disabling output orders

--- backend/app/routes/output_details_routes.py
+++ backend/app/routes/output_details_routes.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
-# Pendiente crear los archivos relacionados
 from app.controllers.output_details_controller import OutputDetailsController
 from fastapi import Depends
-# Pendiente crear los archivos relacionados
 from app.models.output_details_model import OutputDetails
 from app.middlewares.roles_middleware import require_roles
 
@@ -12,50 +10,40 @@ router = APIRouter(
 )
 
 # Endpoint para obtener todos los detalles de salida
-
-
 @router.get("/")
-def get_all_outputDetails(
-    payload: dict = Depends(require_roles(["Admin"]))
-):
+def get_all_outputDetails():
     return OutputDetailsController.get_all_outputDetails()
 
 # Endpoint para obtener los detalles de salida por id
-
-
 @router.get("/{output_details_id}")
-def get_outputDetails_by_id(output_details_id: int,
-                            payload: dict = Depends(require_roles(["Admin"]))
-                            ):
+def get_outputDetails_by_id(
+    output_details_id: int
+):
     return OutputDetailsController.get_outputDetails_by_id(output_details_id)
 
-
 # Endpoint para crear un detalle de salida
-
 @router.post("/create")
 def create_outputDetails(
-    outputDetails_data: OutputDetails,
-    payload: dict= Depends(require_roles(["Admin"]))
+    outputDetails_data: OutputDetails
 ):
     return OutputDetailsController.create_outputDetails(outputDetails_data)
 
 # Endpoint para actualizar detalles de salida
-
-
-@router.put("/update{output_details_id}")
+@router.put("/update/{output_details_id}")
 def update_outputDetails(
     output_details_id: int,
-    outputDetails_data: dict,
-    payload: dict = Depends(require_roles(["Admin"]))
+    outputDetails_data: dict
 ):
     return OutputDetailsController.update_outputDetails(output_details_id, outputDetails_data)
 
-# Endpoint para eliminar registro de detalles de salida
-
-
-@router.delete("/delete{output_details_id}")
-def delete_outputDetails(
-    output_details_id: int,
-    payload: dict = Depends(require_roles(["Admin"]))
+# Endpoint para deshabilitar la orden de salida
+@router.put("/disable/{out_order_id}")
+def disable_output_order(
+    out_order_id: int    
 ):
-    return OutputDetailsController.delete_outputDetails(output_details_id)
+    return OutputDetailsController.disable_output(out_order_id)
+
+# Endpoint para habilitar la orden de salida
+@router.put("/enable/{out_order_id}")
+def enable_output_order(out_order_id: int ):
+    return OutputDetailsController.enable_output(out_order_id)
