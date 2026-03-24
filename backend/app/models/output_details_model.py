78f7f1cd2cf4c6fef9e4871132311232e028352d
DevJuan001 <juanesyt7@gmail.com>
2026-03-23 20:47:52 -0500

    refactor: update OutputDetails model by changing out_product_garanty type to str and adding product_details_id for improved data structure

--- backend/app/models/output_details_model.py
+++ backend/app/models/output_details_model.py
@@ -3,11 +3,9 @@ from typing import Optional
 from datetime import datetime
 
 class OutputDetails(BaseModel):
-    out_order_id: int
     product_serial: str 
     output_details_id: Optional[int] = None
-    out_product_garanty: datetime
-    product_transformation: str 	
-
-
+    out_product_garanty: str
+    product_transformation: str
+    product_details_id : int
    
