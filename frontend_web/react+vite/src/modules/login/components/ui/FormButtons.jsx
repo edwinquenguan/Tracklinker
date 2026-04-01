67de1086b6e949ab689e562e5f7aa21b3e52c5e5
DevJuan001 <juanesyt7@gmail.com>
2026-04-01 18:50:48 -0500

    feat: update FormButtons component to accept dynamic button text and improve styling for better user experience

--- frontend_web/react+vite/src/modules/login/components/ui/FormButtons.jsx
+++ frontend_web/react+vite/src/modules/login/components/ui/FormButtons.jsx
@@ -1,23 +1,27 @@
-export default function FormButtons({getIntoButtonOnclick, recoverButtonOnclick}){
-    return(
-        <section className="flex flex-col p-3 gap-2">
-            <button
-                onClick={getIntoButtonOnclick}
-                className="flex justify-center px-8 py-4 text-base text-white bg-blue-700 rounded-xl transition duration-300
-                        hover:shadow-2xl
-                        dark:hover:shadow-[0px_0px_20px_-10px_#1d4ed8]"
-            >
-                Ingresar
-            </button>
-            <button
-                type="button"
-                onClick={recoverButtonOnclick}
-                className="flex justify-center px-8 py-4 text-base rounded-xl transition-all duration-300
-                hover:bg-stone-300
-                dark:text-white dark:hover:bg-[#202022a6]"
-            >
-                ¿Olvidaste tu Contraseña?
-            </button>
-        </section>
-    );
+export default function FormButtons({
+  getIntoButtonOnclick,
+  getIntoButtonText,
+  recoverButtonOnclick,
+}) {
+  return (
+    <section className="flex flex-col pt-5 gap-2">
+      <button
+        onClick={getIntoButtonOnclick}
+        className="flex justify-center py-3.5 text-white bg-blue-700 rounded-xl transition duration-300
+        hover:text-gray-300
+        dark:hover:shadow-[0px_0px_20px_-10px_#1d4ed8]"
+      >
+        {getIntoButtonText}
+      </button>
+      <button
+        type="button"
+        onClick={recoverButtonOnclick}
+        className="flex justify-center px-8 py-3.5 text-base rounded-xl transition-all duration-300
+        hover:bg-gray-100
+        dark:text-white dark:hover:bg-[#202022a6]"
+      >
+        Olvidaste tu contraseña?
+      </button>
+    </section>
+  );
 }
