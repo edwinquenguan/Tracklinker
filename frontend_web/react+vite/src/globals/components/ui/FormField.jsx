bb921660b352acc3b66138a7a7b2dbfa1d7987a1
DevJuan001 <juanesyt7@gmail.com>
2026-04-01 17:18:29 -0500

    refactor: update FormField component to support children and improve input styling for better user experience

--- frontend_web/react+vite/src/globals/components/ui/FormField.jsx
+++ frontend_web/react+vite/src/globals/components/ui/FormField.jsx
@@ -1,31 +1,32 @@
 export default function FormField({
   value,
   labelText,
-  defaultValue,
   id,
   type = "text",
   placeholder,
   onChange,
   name,
   autoComplete = "",
+  children
 }) {
   return (
     <div className="flex flex-col items-center">
       <label htmlFor={id} className="self-start text-sm dark:text-white">
         {labelText}
       </label>
-      <input
-        name={name}
-        onChange={onChange}
-        value={value}
-        type={type}
-        defaultValue={defaultValue}
-        placeholder={placeholder}
-        id={id}
-        className="w-full px-6 py-3 text-sm rounded-xl outline-none border bg-[#e5e5e527] placeholder:text-[#8a8a8a]
-              dark:bg-[#ffffff10] dark:border-[#ffffff15] dark:text-[#a1a1a1]"
-        autoComplete={autoComplete}
-      />
+      <div className="flex rounded-xl outline-none border bg-[#e5e5e527] placeholder:text-[#8a8a8a] pr-2">
+        <input
+          name={name}
+          onChange={onChange}
+          value={value}
+          type={type}
+          placeholder={placeholder}
+          id={id}
+          className="w-full px-6 py-3 text-sm rounded-xl outline-none bg-[#e5e5e527] dark:bg-[#ffffff10] dark:border-[#ffffff15] dark:text-white"
+          autoComplete={autoComplete}
+        />
+        {children}
+      </div>
     </div>
   );
 }
