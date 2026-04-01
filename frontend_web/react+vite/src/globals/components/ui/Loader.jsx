45026c6740ec1f24edeb1bc81eba9a488dce646f
DevJuan001 <juanesyt7@gmail.com>
2026-04-01 18:49:08 -0500

    feat: update Loader component to include rotation animation and improved styling with Tailwind CSS

--- frontend_web/react+vite/src/globals/components/ui/Loader.jsx
+++ frontend_web/react+vite/src/globals/components/ui/Loader.jsx
@@ -1,5 +1,8 @@
 export default function Loader() {
-    return(
-        <span className="loader"></span>
-    );
+  return (
+    <span
+      className="inline-block w-5 h-5 rounded-[50%] border-2 border-black border-b-transparent animate-rotation 
+    dark:border-white dark:border-b-transparent"
+    />
+  );
 }
