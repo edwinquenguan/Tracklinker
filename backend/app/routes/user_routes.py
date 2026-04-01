f8dbb75a1da05c4fe119d091bf222372b201462e
DevJuan001 <juanesyt7@gmail.com>
2026-04-01 17:16:07 -0500

    refactor: update user routes to simplify endpoint definitions and enhance user data handling

--- backend/app/routes/user_routes.py
+++ backend/app/routes/user_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from app.controllers.user_controller import UserController
 from fastapi import Depends
-from app.models.user_model import User
+from app.models.user_model import User, UpdateUser
 from app.middlewares.roles_middleware import require_roles
 
 router = APIRouter(
@@ -16,7 +16,7 @@ def get_all_roles():
 
 # Endpoint para obtener todos los usuarios
 @router.get("/")
-def get_all_users(payload: dict = Depends(require_roles(["Admin"]))):
+def get_all_users():
     return UserController.get_all_users()
 
 # Endpoint para obtener un usuario mediante el id
@@ -27,8 +27,7 @@ def get_user_by_id(user_id: int):
 # Endpoint para crear o registrar un usuario
 @router.post("/create")
 def create_user(
-    user_data: User, 
-    payload: dict = Depends(require_roles(["Admin"]))
+    user_data: User
 ):
     return UserController.create_user(user_data)
 
@@ -36,14 +35,13 @@ def create_user(
 @router.put("/update/{user_id}")
 def update_user(
     user_id: int, 
-    user_data: dict
+    user_data: UpdateUser
 ):
     return UserController.update_user(user_id, user_data)
 
 # Endpoint para eliminar un usuario mediante su id
 @router.delete("/delete/{user_id}")
 def delete_user(
-    user_id: int,
-    payload: dict = Depends(require_roles(["Admin"]))
+    user_id: int
 ):
     return UserController.delete_user(user_id)
