7fa6d617392bcc638ab5addb14c67159926d648d
DevJuan001 <juanesyt7@gmail.com>
2026-04-03 16:50:13 -0500

    fix: update EmailSentModal message for clarity on email confirmation

--- frontend_web/react+vite/src/modules/login/components/modals/EmailSentModal.jsx
+++ frontend_web/react+vite/src/modules/login/components/modals/EmailSentModal.jsx
@@ -17,7 +17,8 @@ export default function EmailSentModal({ isOpen, onClose }) {
           </span>
           <span className="text-sm">
             Se ha enviado un enlace de confirmacion a tu correo. Por favor,
-            revisa tu bandeja de entrada o en el apartado de spam
+            revisa tu bandeja de entrada o en el apartado de spam, rectifica que
+            tu correo sea el correcto si no te aparece
           </span>
         </section>
         <ConfirmCancelButtons
