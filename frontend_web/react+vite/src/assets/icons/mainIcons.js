c20091b5a73584912cabb0d693590ba5dbce1537
DevJuan001 <juanesyt7@gmail.com>
2026-04-04 17:05:15 -0500

    feat: add active and inactive circle icons for user status representation

--- frontend_web/react+vite/src/assets/icons/mainIcons.js
+++ frontend_web/react+vite/src/assets/icons/mainIcons.js
@@ -19,6 +19,8 @@ import addressIcon from "./main/users/address-icon.svg";
 import phoneIcon from "./main/users/phone-icon.svg";
 import rolIcon from "./main/users/rol-icon.svg";
 import cityIcon from "./main/users/city-icon.svg";
+import activeCircle from "./main/users/active-circle.svg"
+import inactiveCircle from "./main/users/inactive-circle.svg"
 
 /* Products Icons */
 import addProductIcon from "./main/products/add-product-icon.svg";
@@ -67,6 +69,8 @@ export const usersIcons = {
   phoneIcon,
   rolIcon,
   cityIcon,
+  activeCircle,
+  inactiveCircle,
 };
 
 export const productsIcons = {
