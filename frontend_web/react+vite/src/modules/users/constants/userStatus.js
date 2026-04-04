0b9e6c8cee792356c39aa021baca178489e25f5f
DevJuan001 <juanesyt7@gmail.com>
2026-04-04 17:42:32 -0500

    feat: enhance userStatus constants with modalType and visibilityIcon for improved user interaction

--- frontend_web/react+vite/src/modules/users/constants/userStatus.js
+++ frontend_web/react+vite/src/modules/users/constants/userStatus.js
@@ -3,12 +3,15 @@ import { usersIcons } from "../../../assets/icons/mainIcons";
 export const userStatus = {
   0: {
     text: "Deshabilitado",
+    modalType: "enable",
+    visibilityIcon: false,
     icon: usersIcons.inactiveCircle,
-    styles:
-      "bg-gray-50 text-gray-400 border-gray-400 dark:bg-[#1f2937]",
+    styles: "bg-gray-50 text-gray-400 border-gray-400 dark:bg-[#1f2937]",
   },
   1: {
     text: "Activo",
+    modalType: "disable",
+    visibilityIcon: true,
     icon: usersIcons.activeCircle,
     styles:
       "bg-green-200 text-green-600 border-green-500 dark:text-[#00ff3779] dark:bg-[#00ff151f]",
