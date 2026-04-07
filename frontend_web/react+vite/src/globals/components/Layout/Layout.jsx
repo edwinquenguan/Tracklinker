ae9372e1cd41c1e303b05729e6f7d25b6349d33d
DevJuan001 <juanesyt7@gmail.com>
2026-04-07 12:44:47 -0500

    style: update Layout component to use full screen dimensions and adjust order classes for better layout consistency

--- frontend_web/react+vite/src/globals/components/Layout/Layout.jsx
+++ frontend_web/react+vite/src/globals/components/Layout/Layout.jsx
@@ -4,7 +4,7 @@ export default function Layout({ avatarOnClick, helpOnClick, children }) {
   return (
     // Container
     <div
-      className="w-full h-full grid grid-cols-1 grid-rows-[1fr_80px]
+      className="w-screen h-screen grid grid-cols-1 grid-rows-[1fr_80px]
         md:grid-cols-[120px_1fr] md:grid-rows-1
         xl:grid-cols-[270px_85%] xl:grid-rows-1
         "
@@ -12,7 +12,8 @@ export default function Layout({ avatarOnClick, helpOnClick, children }) {
       <Aside avatarOnClick={avatarOnClick} helpOnClick={helpOnClick} />
       <main
         className="min-w-full max-h-full px-1 py-4 dark:bg-black overflow-hidden order-1
-            xl:order-2 md:order-2"
+        md:order-2
+        xl:order-2"
       >
         {children}
       </main>
