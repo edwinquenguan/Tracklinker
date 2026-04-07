d8440de638b5d9468c0ab0339dacf92edbc4935e
DevJuan001 <juanesyt7@gmail.com>
2026-04-07 16:07:31 -0500

    refactor: streamline UsersReport component by integrating ReportsTopSection and KpisContainer, and removing unused elements

--- frontend_web/react+vite/src/modules/reports/components/ui/reports/users/UsersReport.jsx
+++ frontend_web/react+vite/src/modules/reports/components/ui/reports/users/UsersReport.jsx
@@ -1,76 +1,36 @@
 // Hooks
 import { useUsersData } from "../../../../hooks/users/useUsersData";
-// Icons
-import { actionsIcons } from "../../../../../../assets/icons/mainIcons";
 // Components
-import KpiCard from "../../KpiCard";
-import ReportCard from "../../ReportCard";
-import TableCard from "../../TableCard";
 import UsersTable from "./UsersTable";
+import TableCard from "../../TableCard";
+import ReportCard from "../../ReportCard";
 import UsersPieChart from "./UsersPieChart";
 import UsersAreaChart from "./UsersAreaChart";
-import ReturnButton from "../../ReturnButton";
+import KpisContainer from "../../KpisContainer";
+import ReportsContainer from "../../ReportsContainer";
+import ReportsTopSection from "../../ReportsTopSection";
 
-export default function UsersReport({ setReport, setTopSectionVisiblity }) {
+export default function UsersReport({ setReport }) {
   const { usersData } = useUsersData();
-  setTopSectionVisiblity(false);
   return (
     <section className="w-full h-full flex flex-col gap-2 animate-blurUp">
-      <section className="flex items-center justify-between pl-3 dark:text-white">
-        <ReturnButton onClick={() => setReport("home")} />
-        <div className="flex items-center justify-end gap-1.5 pr-3">
-          <div className="flex gap-1 py-0.5 border rounded-xl text-sm font-medium bg-gray-200 
-          dark:bg-gray-950 dark:border-neutral-800 ">
-            <button className="px-4 py-1.5 rounded-lg">7d</button>
-            <button className="px-4 py-1.5 rounded-xl bg-white shadow-md dark:text-black">
-              30d
-            </button>
-            <button className="px-4 py-1.5 rounded-lg">6m</button>
-            <button className="px-4 py-1.5 rounded-lg">1a</button>
-          </div>
-          <button
-            className="flex items-center px-4 py-2 gap-2 border rounded-xl shadow-md text-sm 
-          dark:text-white dark:border-neutral-800"
-          >
-            <img
-              src={actionsIcons.uploadIcon}
-              alt=""
-              className="w-5 h-5 dark:invert"
-            />
-            <span>Exportar</span>
-          </button>
-        </div>
-      </section>
+      <ReportsTopSection setReport={setReport} />
+
       {usersData.map((item) => (
-        <section
-          className="h-full w-full grid p-3 pt-2
-              xl:grid-cols-[repeat(16,_1fr)] xl:grid-rows-7
-              gap-3"
+        <ReportsContainer
+          reportsName={"usuarios"}
+          reportsDate={"16 De Marzo - 23 De Marzo 2025"}
         >
-          <section className="col-span-4 row-span-1 flex flex-col justify-center items-start dark:text-white">
-            <span className="text-2xl font-medium">Reporte de Usuarios</span>
-            <span className="text-sm">16 De Marzo - 23 De Marzo 2025</span>
-          </section>
           {/* Cards o KPIs principales */}
-          <KpiCard
-            name={"Total usuarios"}
-            metricValue={item.total_users}
-            percentValue={"+12%"}
-          />
-          <KpiCard
-            name={"Activos"}
-            metricValue={item.active_users}
-            percentValue={"+12%"}
-          />
-          <KpiCard
-            name={"Deshabilitados"}
-            metricValue={item.inactive_users}
-            percentValue={"+12%"}
-          />
-          <KpiCard
-            name={"Nuevos este mes"}
-            metricValue={item.recent_users}
-            percentValue={"+12%"}
+          <KpisContainer
+            firstKpiName={"Total usuarios"}
+            firstKpiValue={item.total_users}
+            secondKpiName={"Activos"}
+            secondKpiValue={item.active_users}
+            thirdKpiName={"Deshabilitados"}
+            thirdKpiValue={item.inactive_users}
+            fourthKpiName={"Nuevos este mes"}
+            fourthKpiValue={item.recent_users}
           />
 
           <ReportCard name={"Crecimiento Mensual"} colSpan={12}>
@@ -84,7 +44,7 @@ export default function UsersReport({ setReport, setTopSectionVisiblity }) {
           <TableCard tableTitle={"Usuarios recientes"}>
             <UsersTable />
           </TableCard>
-        </section>
+        </ReportsContainer>
       ))}
     </section>
   );
