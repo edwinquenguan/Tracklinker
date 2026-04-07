95faedd84aadaf5d8cb42a13162a9749fddaa590
DevJuan001 <juanesyt7@gmail.com>
2026-04-07 16:08:32 -0500

    style: enhance ReturnButton with hover effect for improved user interaction

--- frontend_web/react+vite/src/modules/reports/components/ui/ReturnButton.jsx
+++ frontend_web/react+vite/src/modules/reports/components/ui/ReturnButton.jsx
@@ -3,8 +3,9 @@ import { actionsIcons } from "../../../../assets/icons/mainIcons";
 export default function ReturnButton({ onClick }) {
   return (
     <button
-      className="flex items-center gap-1 px-3.5 py-1.5 border rounded-md shadow-md
-      dark:text-white dark:border-[#9490902d]"
+      className="flex items-center gap-1 px-3.5 py-1.5 border rounded-md shadow-md transition-all duration-300
+      hover:bg-gray-200
+      dark:text-white dark:border-[#9490902d] dark:hover:bg-[#2c2c2e]"
       onClick={onClick}
     >
       <img
