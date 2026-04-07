ac7e2bdc6984908a43e9ba6bcd18abb3312ad256
DevJuan001 <juanesyt7@gmail.com>
2026-04-06 23:20:09 -0500

    style: update userStatus styles for improved layout and consistency

--- frontend_web/react+vite/src/modules/users/constants/userStatus.js
+++ frontend_web/react+vite/src/modules/users/constants/userStatus.js
@@ -6,7 +6,7 @@ export const userStatus = {
     modalType: "enable",
     visibilityIcon: false,
     icon: usersIcons.inactiveCircle,
-    styles: "bg-gray-50 text-gray-400 border-gray-400 dark:bg-[#1f2937]",
+    styles: "w-[115px] bg-gray-50 text-gray-400 border-gray-400 dark:bg-[#1f2937]",
   },
   1: {
     text: "Activo",
@@ -14,6 +14,6 @@ export const userStatus = {
     visibilityIcon: true,
     icon: usersIcons.activeCircle,
     styles:
-      "bg-green-200 text-green-600 border-green-500 dark:text-[#00ff3779] dark:bg-[#00ff151f]",
+      "w-20 bg-green-200 text-green-600 border-green-500 dark:text-[#00ff3779] dark:bg-[#00ff151f]",
   },
 };
