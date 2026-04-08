d5467b9656da21f567e6e766a5853e64183db3bb
DevJuan001 <juanesyt7@gmail.com>
2026-04-07 19:55:39 -0500

    refactor: rename report methods for clarity and add period parameter to enhance data retrieval

--- backend/app/controllers/reports_controller.py
+++ backend/app/controllers/reports_controller.py
@@ -5,8 +5,8 @@ class ReportsController:
 
 #   ------------ REPORTES DE USUARIOS ------------
     @staticmethod
-    def get_users():
-        error, user = ReportsRepository.find_users()
+    def get_recent_users():
+        error, user = ReportsRepository.find_recent_users()
         if error:
             raise HTTPException(status_code=404, detail=error)
         return {
@@ -14,8 +14,8 @@ class ReportsController:
         }
     
     @staticmethod
-    def get_users_by_rol():
-        error, user = ReportsRepository.find_by_rol()
+    def get_users_by_rol(period: str):
+        error, user = ReportsRepository.find_users_by_rol(period)
         if error:
             raise HTTPException(status_code=404, detail=error)
         return {
@@ -23,17 +23,17 @@ class ReportsController:
         }
     
     @staticmethod
-    def get_users_by_month():
-        error, data = ReportsRepository.find_by_month()
+    def get_users_by_month(period: str):
+        error, data = ReportsRepository.find_users_by_month(period)
         if error:
             raise HTTPException(status_code=404, detail=error)
         return {
-            "data": data 
+            "data": data
         }
     
     @staticmethod
-    def get_monthly_user_growth():
-        error, data = ReportsRepository.find_monthly_user_growth()
+    def get_monthly_user_growth(period: str):
+        error, data = ReportsRepository.find_monthly_users_growth(period)
         if error:
             raise HTTPException(status_code=404, detail=error)
         return {
@@ -51,16 +51,17 @@ class ReportsController:
 
 #   ------------ REPORTES DE PRODUCTOS ------------
     @staticmethod
-    def get_products():
-        error, products = ReportsRepository.find_products()
+    def get_recent_products():
+        error, products = ReportsRepository.find_recent_products()
         if error:
             raise HTTPException(status_code=404, detail=error)
         return {
             "data": products
         }
+
     @staticmethod
-    def get_monthly_products_growth():
-        error, data = ReportsRepository.find_monthly_products_growth()
+    def get_monthly_products_growth(period: str):
+        error, data = ReportsRepository.find_monthly_products_growth(period)
         if error:
             raise HTTPException(status_code=404, detail=error)
         return {
@@ -68,8 +69,17 @@ class ReportsController:
         }
     
     @staticmethod
-    def get_count_of_all():
-        error, data = ReportsRepository.get_count_of_all()
+    def get_products_by_brand(period: str):
+        error, data = ReportsRepository.find_products_by_brand(period)
+        if error:
+            raise HTTPException(status_code=404, detail=error)
+        return {
+            "data": data 
+        }
+    
+    @staticmethod
+    def get_products_by_status():
+        error, data = ReportsRepository.find_products_by_status()
         if error:
             raise HTTPException(status_code=404, detail=error)
         return {
