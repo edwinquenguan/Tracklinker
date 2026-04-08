f81f515b4eb14de3dcc78cf71029d94d37452c3b
DevJuan001 <juanesyt7@gmail.com>
2026-04-07 23:41:46 -0500

    feat: implement data fetching hooks and UI component for subcategories report

--- frontend_web/react+vite/src/modules/reports/components/ui/reports/subcategories/SubcategoriesReport.jsx
+++ frontend_web/react+vite/src/modules/reports/components/ui/reports/subcategories/SubcategoriesReport.jsx
@@ -1,38 +1,57 @@
 // Hooks
+import { useState } from "react";
+import { useSubcategoriesData } from "../../../../hooks/subcategories/useSubcategoriesData";
 // Components
 import KpisContainer from "../../KpisContainer";
 import ReportsContainer from "../../ReportsContainer";
 import ReportsTopSection from "../../ReportsTopSection";
 import TableCard from "../../TableCard";
 import ReportCard from "../../ReportCard";
+import SubcategoriesAreaChart from "./SubcategoriesAreaChart";
+import SubcategoriesTable from "./SubcategoriesTable";
+import SubcategoriesPieChart from "./SubcategoriesPieChart";
 
 export default function SubcategoriesReport({ setReport }) {
+  const { subcategoriesData } = useSubcategoriesData();
+  const [period, setPeriod] = useState("30d");
   return (
     <section className="w-full h-full flex flex-col gap-2 animate-blurUp">
-      <ReportsTopSection setReport={setReport} />
+      <ReportsTopSection
+        setReport={setReport}
+        periods={["7d", "30d", "6m", "1a"]}
+        setPeriod={setPeriod}
+        currentPeriod={period}
+      />
+      {subcategoriesData.map((item) => (
+        <ReportsContainer
+          reportsName={"Subcategorias"}
+          reportsDate={"16 De Marzo - 23 De Marzo 2025"}
+        >
+          {/* Cards o KPIs principales */}
+          <KpisContainer
+            firstKpiName={"Totales"}
+            firstKpiValue={item.total_subcategories}
+            secondKpiName={"Recientes"}
+            secondKpiValue={item.recent_subcategories}
+            thirdKpiName={"Inactivas"}
+            thirdKpiValue={item.inactive_subcategories}
+            fourthKpiName={"Activas"}
+            fourthKpiValue={item.active_subcategories}
+          />
 
-      <ReportsContainer
-        reportsName={"Productos"}
-        reportsDate={"16 De Marzo - 23 De Marzo 2025"}
-      >
-        {/* Cards o KPIs principales */}
-        <KpisContainer
-          firstKpiName={""}
-          firstKpiValue={""}
-          secondKpiName={""} 
-          secondKpiValue={""}
-          thirdKpiName={""}
-          thirdKpiValue={""}
-          fourthKpiName={""}
-          fourthKpiValue={""}
-        />
+          <ReportCard name={"Crecimiento"} colSpan={12}>
+            <SubcategoriesAreaChart period={period} />
+          </ReportCard>
 
-        <ReportCard name={"Crecimiento Mensual"} colSpan={12}></ReportCard>
+          <ReportCard name={"Distribución"} colSpan={4}>
+            <SubcategoriesPieChart period={period} />
+          </ReportCard>
 
-        <ReportCard name={"Distribución"} colSpan={4}></ReportCard>
-
-        <TableCard tableTitle={"Productos recientes"}></TableCard>
-      </ReportsContainer>
+          <TableCard tableTitle={"Subcategorias recientes"}>
+            <SubcategoriesTable />
+          </TableCard>
+        </ReportsContainer>
+      ))}
     </section>
   );
 }
