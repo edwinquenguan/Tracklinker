cdbc37da2e99341a9bea9163e0698d8ada6098fc
DevJuan001 <juanesyt7@gmail.com>
2026-04-07 20:44:26 -0500

    feat: update SearchBar component to accept value and onChange props for controlled input

--- frontend_web/react+vite/src/globals/components/ui/SearchBar.jsx
+++ frontend_web/react+vite/src/globals/components/ui/SearchBar.jsx
@@ -1,15 +1,17 @@
 import { headerIcons } from "../../../assets/icons/headerIcons";
 
-export default function SearchBar() {
+export default function SearchBar({ value, onChange }) {
   return (
-    <div className="flex px-2 w-[40%] border border-gray-300 rounded-3xl dark:border-gray-800">
+    <div className="flex px-2 w-[40%] border border-gray-300 rounded-3xl dark:border-[#37415173]">
       <input
         id="search-text"
         type="text"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
         placeholder="Buscar"
         className="w-[95%] py-2.5 px-5 rounded-xl outline-none
             placeholder:text-[15px] placeholder:text-gray-500
-            dark:bg-black dark:placeholder-white dark:text-white"
+            dark:bg-black dark:placeholder-text-gray-700 dark:text-white"
       />
       <button>
         <img
