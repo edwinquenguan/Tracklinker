6348eaff1ff2d335e8e420130f5c0d84c6940ae2
DevJuan001 <juanesyt7@gmail.com>
2026-04-07 19:12:21 -0500

    refactor: update ReportsTopSection to use dynamic period buttons and improve code readability

--- frontend_web/react+vite/src/modules/reports/components/ui/ReportsTopSection.jsx
+++ frontend_web/react+vite/src/modules/reports/components/ui/ReportsTopSection.jsx
@@ -3,43 +3,32 @@ import ExportButton from "./ExportButton";
 
 export default function ReportsTopSection({
   setReport,
-  sevenDaysOnclick,
-  thirtyDaysOnclick,
-  sixMonthsOnClick,
-  oneYearOnClick,
+  setPeriod,
+  periods,
+  currentPeriod,
 }) {
   return (
     <section className="flex items-center justify-between pl-3 dark:text-white">
       <ReturnButton onClick={() => setReport("home")} />
       <div className="flex items-center justify-end gap-1.5 pr-3">
         <div
-          className="flex gap-1 py-0.5 px-1 border rounded-xl text-sm font-medium bg-gray-200 
-                  dark:bg-gray-950 dark:border-neutral-800 "
+          className="flex gap-1 py-0.5 px-1 border rounded-xl text-sm font-medium bg-gray-200
+          dark:bg-gray-950 dark:border-neutral-800"
         >
-          <button
-            onClick={sevenDaysOnclick}
-            className="px-4 py-1.5 rounded-lg hover:bg-gray-300 transition duration-300"
-          >
-            7d
-          </button>
-          <button
-            onClick={thirtyDaysOnclick}
-            className="px-4 py-1.5 rounded-xl bg-white shadow-md dark:text-black"
-          >
-            30d
-          </button>
-          <button
-            onClick={sixMonthsOnClick}
-            className="px-4 py-1.5 rounded-lg hover:bg-gray-300 transition duration-300"
-          >
-            6m
-          </button>
-          <button
-            onClick={oneYearOnClick}
-            className="px-4 py-1.5 rounded-lg hover:bg-gray-300 transition duration-300"
-          >
-            1a
-          </button>
+          {periods.map((period) => (
+            <button
+              key={period}
+              onClick={() => setPeriod(period)}
+              className={`px-4 py-1.5 rounded-lg 
+              ${
+                currentPeriod === period
+                  ? "bg-white shadow-md dark:text-black"
+                  : "hover:bg-gray-300 transition duration-300 dark:hover:bg-[#2c2c2e]"
+              }`}
+            >
+              {period}
+            </button>
+          ))}
         </div>
         <ExportButton />
       </div>
