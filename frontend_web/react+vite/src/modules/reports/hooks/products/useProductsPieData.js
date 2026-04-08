69a5f860b113157f9adca4b14ff98b4d71caf225
DevJuan001 <juanesyt7@gmail.com>
2026-04-07 19:10:55 -0500

    feat: add ProductsPieChart component with custom hook for fetching and displaying product data in a pie chart

---
+++ frontend_web/react+vite/src/modules/reports/hooks/products/useProductsPieData.js
@@ -0,0 +1,38 @@
+import { useEffect, useRef, useState } from "react";
+import { getProductsPieDataService } from "../../services/products/getProductsPieDataService";
+
+export function useProductsPieData(period) {
+  const [productsData, setProductsData] = useState([]);
+  const [error, setError] = useState(false);
+  const controllerRef = useRef(null);
+
+  useEffect(() => {
+    async function fetchProductsData() {
+      controllerRef.current?.abort();
+      controllerRef.current = new AbortController();
+
+      try {
+        const data = await getProductsPieDataService(
+          period,
+          controllerRef.current.signal,
+        );
+
+        const colors = ["#a5acfa", "#5769ff", "#4f5ff1", "#2f3ab5"];
+
+        const pieData = data.map((item, index) => ({
+          ...item,
+          color: colors[index % colors.length],
+        }));
+        setProductsData(pieData);
+      } catch (error) {
+        if (error.name === "AbortError") return;
+        setError(error.message);
+      }
+    }
+
+    fetchProductsData();
+    return () => controllerRef.current?.abort();
+  }, [period]);
+
+  return { productsData, error };
+}
