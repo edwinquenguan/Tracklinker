683c59ea65c8e562e66d6831f70d1ab8beb54262
DevJuan001 <juanesyt7@gmail.com>
2026-04-07 20:17:17 -0500

    feat: add endpoints for retrieving recent categories, monthly growth, and category status reports

--- backend/app/routes/reports_routes.py
+++ backend/app/routes/reports_routes.py
@@ -50,3 +50,19 @@ def get_products_by_brand(period: str = "30d"):
 @router.get("/get_products_by_status")
 def get_products_by_status():
     return ReportsController.get_products_by_status()
+
+#   ------------ REPORTES DE CATEGORIAS ------------
+# Endpoint para obtener reporte de las ultimas 6 categorias agregados
+@router.get("/get_recent_categories")
+def get_recent_categories():
+    return ReportsController.get_recent_categories()
+
+# Endpoint para obtener el crecimiento de categorias
+@router.get("/get_monthly_categories_growth/{period}")
+def get_monthly_categories_growth(period: str = "30d"):
+    return ReportsController.get_monthly_categories_growth(period)
+
+# Endpoint para obtener cateegorias por estado
+@router.get("/get_categories_by_status")
+def get_categories_by_status():
+    return ReportsController.get_categories_by_status()
