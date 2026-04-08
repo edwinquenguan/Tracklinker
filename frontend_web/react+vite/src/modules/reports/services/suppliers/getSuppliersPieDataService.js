072adfcbcf568cd57d90346666492ab621efdfb3
DevJuan001 <juanesyt7@gmail.com>
2026-04-08 14:54:47 -0500

    feat: add SuppliersPieChart component with data fetching and visualization logic

---
+++ frontend_web/react+vite/src/modules/reports/services/suppliers/getSuppliersPieDataService.js
@@ -0,0 +1,23 @@
+import { apiRoutes } from "../../../../config/apiRoutes";
+import { getToken } from "../../../../utils/auth";
+
+export async function getSuppliersPieDataService(period, signal) {
+  const response = await fetch(
+    `${apiRoutes.apiUrl}${apiRoutes.reports}/get_suppliers_by_brand/${period}`,
+    {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Authorization: getToken(),
+      },
+      signal,
+    },
+  );
+
+  if (!response.ok) {
+    throw new Error("Error en la petición");
+  }
+  const data = await response.json();
+
+  return data.data;
+}
